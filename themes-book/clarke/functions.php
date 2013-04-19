42def53167711168b56709d42530b9d6b0737aae
Dac Chartrand <dac.chartrand@gmail.com>
2013-04-19 13:34:58 -0400

    CSS override the word "part" based on book language

---
+++ themes-book/clarke/functions.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author  PressBooks <code@pressbooks.org>
+ * @license GPLv2 (or any later version)
+ */
+
+
+/**
+ * Clarke features we inject ourselves, (not user options)
+ *
+ * @param $css
+ *
+ * @return string
+ */
+function donham_theme_pdf_css_override( $css ) {
+
+	// Translate "Part" to whatever language this book is in
+	$css .= '#toc .part a::before { content: "' . __( 'part', 'pressbooks' ) . ' "counter(part) ". "; }' . "\n";
+
+	return $css;
+}
+add_filter( 'pb_pdf_css_override', 'donham_theme_pdf_css_override' );
+
+
+/**
+ * Clarke features we inject ourselves, (not user options)
+ *
+ * @param $css
+ *
+ * @return string
+ */
+function donham_theme_ebook_css_override( $css ) {
+
+	return $css;
+}
+add_filter( 'pb_epub_css_override', 'donham_theme_ebook_css_override' );
--- themes-book/donham/functions.php
+++ themes-book/donham/functions.php
@@ -4,12 +4,46 @@
  * @license GPLv2 (or any later version)
  */
 
-/* ------------------------------------------------------------------------ *
+
+/**
  * Google Webfonts
- * ------------------------------------------------------------------------ */
- 
+ */
 function harvard_enqueue_styles() {
-   		 wp_enqueue_style( 'harvard-fonts', 'http://fonts.googleapis.com/css?family=Galdeano|Tinos:400,400italic,700,700italic');  		   		   		       		           
-}     
-add_action('wp_print_styles', 'harvard_enqueue_styles');
-?>
+	wp_enqueue_style( 'harvard-fonts', 'http://fonts.googleapis.com/css?family=Galdeano|Tinos:400,400italic,700,700italic' );
+}
+add_action( 'wp_print_styles', 'harvard_enqueue_styles' );
+
+
+/**
+ * Donham features we inject ourselves, (not user options)
+ *
+ * @param $css
+ *
+ * @return string
+ */
+function donham_theme_pdf_css_override( $css ) {
+
+	// Translate "Part" to whatever language this book is in
+	$css .= '#toc .part a::before { content: "' . __( 'part', 'pressbooks' ) . ' "counter(part) ". "; }' . "\n";
+	$css .= 'div.part-title-wrap > h3.part-number:before { content: "' . __( 'part', 'pressbooks' ) . ' "; }' . "\n";
+
+	return $css;
+}
+add_filter( 'pb_pdf_css_override', 'donham_theme_pdf_css_override' );
+
+
+/**
+ * Donham features we inject ourselves, (not user options)
+ *
+ * @param $css
+ *
+ * @return string
+ */
+function donham_theme_ebook_css_override( $css ) {
+
+	// Translate "Part" to whatever language this book is in
+	$css .= 'div.part-title-wrap > h3.part-number:before { content: "' . __( 'part', 'pressbooks' ) . ' "; }' . "\n";
+
+	return $css;
+}
+add_filter( 'pb_epub_css_override', 'donham_theme_ebook_css_override' );
