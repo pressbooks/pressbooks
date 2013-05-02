79e656c16d483b6503bdb1ec726ba7a23b62ccd1
Dac Chartrand <dac.chartrand@gmail.com>
2013-05-02 14:08:51 -0400

    Coding standards

--- includes/modules/import/epub/class-pb-chapter.php
+++ includes/modules/import/epub/class-pb-chapter.php
@@ -7,159 +7,183 @@
 
 namespace PressBooks\Import\Epub;
 
+
 class Chapter {
 
-  /**
-   * DOM object
-   * @var \SimpleXMLElement
-   */
-  private $xml;
-  private $title = null;
-  private $content = null;
-  private $parent_id = null;
-  private $slug = null;
-  private $imagefiles = array();
-  private $pb_type;
+	/**
+	 * DOM object
+	 *
+	 * @var \SimpleXMLElement
+	 */
+	private $xml;
 
-  /**
-   * 
-   * @param type $id_file
-   * @param \SimpleXMLElement $xml
-   * @param $pb_type string - one of either front-matter, back-matter, chapter
-   */
-  function __construct( $id_file, \SimpleXMLElement $xml, $pb_type = 'chapter' ) {
-    $this->xml = $xml;
-    $this->xml->registerXPathNamespace ( "n", "http://www.w3.org/1999/xhtml" );
-    $this->pb_type = $pb_type;
-    //$this->getContent();
-    //echo "<pre>" . $this->getContent() . "</pre><br />";die();
-  }
+	private $title = null;
 
-  /**
-   * 
-   * @param array $imageFiles
-   */
-  function setImageFiles( array $imageFiles ) {
-    $this->imagefiles = $imageFiles;
-  }
+	private $content = null;
 
-  /**
-   * 
-   * @return type
-   */
-  function getContent() {
+	private $parent_id = null;
 
-    if ( is_null ( $this->content ) ) {
+	private $slug = null;
 
-      // remove first h1 headline
-      $headlines = $this->xml->xpath ( '//n:h1' );
-      if ( empty ( $headlines ) ) {
-        $this->title = 'title-missing';
-      } else {
-        $headline = $headlines[0];
-        $this->title = (string) $headline;
-      }
-      unset ( $headlines[0][0] );
+	private $imagefiles = array();
 
-      $bodies = $this->xml->xpath ( '//n:body' );
-      $body = $bodies[0];
+	private $pb_type;
 
-      $this->parseImages ( $body );
 
-      $this->content = str_replace ( "\n", ' ', $body->asXML () );
-    }
-    return $this->content;
-  }
+	/**
+	 *
+	 * @param type $id_file
+	 * @param \SimpleXMLElement $xml
+	 * @param $pb_type string - one of either front-matter, back-matter, chapter
+	 */
+	function __construct( $id_file, \SimpleXMLElement $xml, $pb_type = 'chapter' ) {
+		$this->xml = $xml;
+		$this->xml->registerXPathNamespace( "n", "http://www.w3.org/1999/xhtml" );
+		$this->pb_type = $pb_type;
+		//$this->getContent();
+		//echo "<pre>" . $this->getContent() . "</pre><br />";die();
+	}
 
-  /**
-   * 
-   * @param \SimpleXMLElement $body
-   */
-  private function parseImages( \SimpleXMLElement $body ) {
-    $body->registerXPathNamespace ( "n", "http://www.w3.org/1999/xhtml" );
-    $imageTags = $body->xpath ( '//n:img' );
-    foreach ( $imageTags AS $imageTag ) {
-      $imageTag['src'] = $this->getImageUrl ( (string) $imageTag['src'] );
-    }
-    //\var_dump($this->imagefiles);
-  }
 
-  /**
-   * 
-   * @param type $path
-   * @return type
-   * @throws \Exception
-   */
-  private function getImageUrl( $path ) {
-    //borks on relative paths
-    $path = str_replace ( '../', '', $path );
+	/**
+	 *
+	 * @param array $imageFiles
+	 */
+	function setImageFiles( array $imageFiles ) {
+		$this->imagefiles = $imageFiles;
+	}
 
-    if ( ! array_key_exists ( $path, $this->imagefiles ) ) {
-      throw new \Exception ( 'missing image: ' . $path );
-    }
-    $post_id = $this->imagefiles[$path];
-    $post = \get_post ( $post_id );
-    return $post->guid;
-  }
 
-  /**
-   * 
-   * @return type
-   */
-  function getTitle() {
-    if ( is_null ( $this->title ) ) {
-      $this->getContent ();
-    }
-    return $this->title;
-  }
+	/**
+	 *
+	 * @return type
+	 */
+	function getContent() {
 
-  /**
-   * 
-   * @return string
-   */
-  function getSlug() {
-    // return 'chapter';
-    $slug = 'chapter';
-    if ( ! empty ( $this->title ) ) {
-      $slug = strtolower ( $this->title );
-      $slug = str_replace ( ' ', '-', $slug );
-    }
-    return $slug;
-  }
+		if ( is_null( $this->content ) ) {
 
-  /**
-   * 
-   * @global type $wpdb
-   * @return type
-   * @throws Exception
-   */
-  function getParent() {
-    if ( is_null ( $this->parent_id ) ) {
-      GLOBAL $wpdb;
-      $query = "SELECT ID FROM " . $wpdb->posts . " WHERE post_type = 'part' AND post_name = 'main-body'";
-      $parents = $wpdb->get_results ( $query );
-      if ( empty ( $parents ) ) {
-        throw new Exception ( 'missing main body' );
-      }
-      $this->parent_id = $parents[0]->ID;
-    }
-    return $this->parent_id;
-  }
+			// remove first h1 headline
+			$headlines = $this->xml->xpath( '//n:h1' );
+			if ( empty ( $headlines ) ) {
+				$this->title = 'title-missing';
+			} else {
+				$headline = $headlines[0];
+				$this->title = (string) $headline;
+			}
+			unset ( $headlines[0][0] );
 
-  /**
-   * 
-   * @return string
-   */
-  function getExcerpt() {
-    return '';
-  }
+			$bodies = $this->xml->xpath( '//n:body' );
+			$body = $bodies[0];
 
-  /**
-   * 
-   * @return type
-   */
-  function getPbType() {
-    return $this->pb_type;
-  }
+			$this->parseImages( $body );
+
+			$this->content = str_replace( "\n", ' ', $body->asXML() );
+		}
+
+		return $this->content;
+	}
+
+
+	/**
+	 *
+	 * @param \SimpleXMLElement $body
+	 */
+	private function parseImages( \SimpleXMLElement $body ) {
+		$body->registerXPathNamespace( "n", "http://www.w3.org/1999/xhtml" );
+		$imageTags = $body->xpath( '//n:img' );
+		foreach ( $imageTags AS $imageTag ) {
+			$imageTag['src'] = $this->getImageUrl( (string) $imageTag['src'] );
+		}
+		//\var_dump($this->imagefiles);
+	}
+
+
+	/**
+	 *
+	 * @param type $path
+	 *
+	 * @return type
+	 * @throws \Exception
+	 */
+	private function getImageUrl( $path ) {
+		//borks on relative paths
+		$path = str_replace( '../', '', $path );
+
+		if ( ! array_key_exists( $path, $this->imagefiles ) ) {
+			throw new \Exception ( 'missing image: ' . $path );
+		}
+		$post_id = $this->imagefiles[$path];
+		$post = \get_post( $post_id );
+
+		return $post->guid;
+	}
+
+
+	/**
+	 *
+	 * @return type
+	 */
+	function getTitle() {
+		if ( is_null( $this->title ) ) {
+			$this->getContent();
+		}
+
+		return $this->title;
+	}
+
+
+	/**
+	 *
+	 * @return string
+	 */
+	function getSlug() {
+		// return 'chapter';
+		$slug = 'chapter';
+		if ( ! empty ( $this->title ) ) {
+			$slug = strtolower( $this->title );
+			$slug = str_replace( ' ', '-', $slug );
+		}
+
+		return $slug;
+	}
+
+
+	/**
+	 *
+	 * @global type $wpdb
+	 * @return type
+	 * @throws Exception
+	 */
+	function getParent() {
+		if ( is_null( $this->parent_id ) ) {
+			GLOBAL $wpdb;
+			$query = "SELECT ID FROM " . $wpdb->posts . " WHERE post_type = 'part' AND post_name = 'main-body'";
+			$parents = $wpdb->get_results( $query );
+			if ( empty ( $parents ) ) {
+				throw new Exception ( 'missing main body' );
+			}
+			$this->parent_id = $parents[0]->ID;
+		}
+
+		return $this->parent_id;
+	}
+
+
+	/**
+	 *
+	 * @return string
+	 */
+	function getExcerpt() {
+		return '';
+	}
+
+
+	/**
+	 *
+	 * @return type
+	 */
+	function getPbType() {
+		return $this->pb_type;
+	}
 
 }
