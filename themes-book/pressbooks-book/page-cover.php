adcf293efabbad986d68fc66a8fec66e3512560f
Sonia <sonia@nudge.ca>
2013-05-17 16:16:21 -0400

    test commit
    Signed-off-by: Sonia <sonia@nudge.ca>

--- themes-book/pressbooks-book/page-cover.php
+++ themes-book/pressbooks-book/page-cover.php
@@ -4,6 +4,7 @@ $metadata = pb_get_book_information();
 if (get_option('blog_public') == '1' || (get_option('blog_public') == '0' && current_user_can_for_blog($blog_id, 'read'))):
 if (have_posts()) the_post();
 ?>
+<h1>here</h1>
 <?php pb_get_links(false); ?>
 <?php global $first_chapter; ?>
 			<div id="post-<?php the_ID(); ?>" <?php post_class('home-post'); ?>>
