8d1af8f32a7e7b0e7ff2e68c7567c59e4f477db9
Sonia <sonia@nudge.ca>
2013-06-13 10:53:43 -0400

    Reordering top block html for responsive element order. (cook cover below book description). Also added retina version of PB branding.
    Signed-off-by: Sonia <sonia@nudge.ca>

--- themes-book/pressbooks-book/page-cover-top-block.php
+++ themes-book/pressbooks-book/page-cover-top-block.php
@@ -4,13 +4,7 @@
 			<?php pb_get_links(false); ?>
 			<?php $metadata = pb_get_book_information();?>
 			<div class="right-block">
-				<a href="http://pressbooks.com" class="pressbooks-brand"><img src="<?php bloginfo('template_url'); ?>/images/pressbooks-branding.png" alt="pressbooks-branding" width="186" height="123" /></a>
-				<?php if ( ! empty( $metadata['pb_cover_image'] ) ): ?>
-					<div class="book-cover">
-						<img src="<?php echo $metadata['pb_cover_image']; ?>" alt="book-cover" title="<?php bloginfo( 'name' ); ?> book cover" />
-					
-					</div>	
-				<?php endif; ?>		
+				<a href="http://pressbooks.com" class="pressbooks-brand"><img src="<?php bloginfo('template_url'); ?>/images/pressbooks-branding-2x.png" alt="pressbooks-branding" width="186" height="123" /></a>
 			</div>	
 						
 			<div class="book-info">
@@ -32,20 +26,32 @@
 				<?php if ( ! empty( $metadata['pb_about_50'] ) ): ?>
 					<p><?php echo pb_decode( $metadata['pb_about_50'] ); ?></p>
 				<?php endif; ?>
-				
-				<?php global $first_chapter; ?>
-				<div class="call-to-action">
-					<a class="btn red" href="<?php global $first_chapter; echo $first_chapter; ?>"><span class="read-icon"></span><?php _e('Read', 'pressbooks'); ?></a>
-					
-					<?php if ( @array_filter( get_option( 'pressbooks_ecommerce_links' ) ) ) : ?>
-					 <!-- Buy -->
-						 <a class="btn black" href="<?php echo get_option('home'); ?>/buy"><span class="buy-icon"></span><?php _e('Buy', 'pressbooks'); ?></a>				
-					 <?php endif; ?>	
-					 
-					
-				</div> <!-- .call-to-action -->		
+		
+			</div> <!-- end .book-info -->
 			
-			</div>
+				<?php if ( ! empty( $metadata['pb_cover_image'] ) ): ?>
+				<div class="book-cover">
+				
+						<img src="<?php echo $metadata['pb_cover_image']; ?>" alt="book-cover" title="<?php bloginfo( 'name' ); ?> book cover" />
+					
+				</div>	
+				<?php endif; ?>
+				
+				<div class="call-to-action-wrap">
+					<?php global $first_chapter; ?>
+					<div class="call-to-action">
+						<a class="btn red" href="<?php global $first_chapter; echo $first_chapter; ?>"><span class="read-icon"></span><?php _e('Read', 'pressbooks'); ?></a>
+						
+						<?php if ( @array_filter( get_option( 'pressbooks_ecommerce_links' ) ) ) : ?>
+						 <!-- Buy -->
+							 <a class="btn black" href="<?php echo get_option('home'); ?>/buy"><span class="buy-icon"></span><?php _e('Buy', 'pressbooks'); ?></a>				
+						 <?php endif; ?>	
+						 
+						
+					</div> <!-- end .call-to-action -->		
+				</div><!--  end .call-to-action-wrap -->
+				
+			
 			
 			
 	</section> <!-- end .top-block -->
