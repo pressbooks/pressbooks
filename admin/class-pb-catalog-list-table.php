a5c7fbae6f96a802c478cffb79af3b87c23dc547
Ned Zimmerman <ned@bight.ca>
2013-07-06 09:52:49 -0400

    Fixes ticket #870 (user can see admin button for books of which they are not the admin).

--- admin/class-pb-catalog-list-table.php
+++ admin/class-pb-catalog-list-table.php
@@ -75,9 +75,12 @@ class Catalog_List_Table extends \WP_List_Table {
 
 		// Build row actions
 		$actions = array(
-			'visit' => sprintf( '<a href="%s">%s</a>', get_site_url( $blog_id ), __( 'Visit Book' ) ),
-			'dashboard' => sprintf( '<a href="%s">%s</a>', get_admin_url( $blog_id ), __( 'Visit Admin', 'pressbooks' ) ),
+			'visit' => sprintf( '<a href="%s">%s</a>', get_site_url( $blog_id ), __( 'Visit Book' ) )
 		);
+		
+		// Only include admin link if user has admin rights to the book in question
+		if ( is_user_member_of_blog( $user_id, $blog_id ) )
+			$actions['dashboard'] = sprintf( '<a href="%s">%s</a>', get_admin_url( $blog_id ), __( 'Visit Admin', 'pressbooks' ) );
 
 		// Return the title contents
 		return sprintf( '<span class="title">%1$s</span> %2$s',
