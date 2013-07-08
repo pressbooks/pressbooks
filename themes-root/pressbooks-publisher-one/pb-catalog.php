6f4570e0cdcaa15d92a993348156c4e21926c866
Sonia <sonia@nudge.ca>
2013-07-08 13:17:40 -0400

    Ticket #862: fix for 1. PB branding 2. login/logout + admin btns 4. responsive 6. footer
    Signed-off-by: Sonia <sonia@nudge.ca>

--- themes-root/pressbooks-publisher-one/pb-catalog.php
+++ themes-root/pressbooks-publisher-one/pb-catalog.php
@@ -160,9 +160,10 @@ $h1_title = __( 'Catalog', 'pressbooks' );
 	<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico" />
 	<title><?php _e( 'Catalog Page', 'pressbooks' ); ?></title>
 	<link rel="stylesheet" type="text/css" href="style-catalog.css" />
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,400italic' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Oswald|Open+Sans:400,400italic,600' rel='stylesheet' type='text/css'>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js" type="text/javascript"></script>
 	<script src="js/jquery.equalizer.min.js" type="text/javascript"></script>
+	<script src="js/small-menu.js" type="text/javascript"></script>	
 	<script type="text/javascript">
 		// <![CDATA[
 		$(function () {
@@ -175,70 +176,88 @@ $h1_title = __( 'Catalog', 'pressbooks' );
 <body>
 
 <div class="catalog-wrap">
-
-	<div class="catalog-sidebar">
-		<?php if ( $profile['pb_catalog_url'] ): ?><a href="<?php echo $profile['pb_catalog_url']; ?>"><?php endif; ?>
-		<img class="catalog-logo" src="<?php echo _logo_url( $profile ); ?>" alt="catalog-logo" width="100" height="99" />
-		<?php if ( $profile['pb_catalog_url'] ): ?></a><?php endif; ?>
-		<p class="about-blurb"><?php echo $profile['pb_catalog_about']; ?></p>
-		<br />
-
-		<!-- Tags -->
-		<?php for ( $i = 1; $i <= 2; ++$i ) : ?>
-		<?php $tags = $catalog->getTags( $i, false ); ?>
-			<h3><?php echo ( ! empty( $profile["pb_catalog_tag_{$i}_name"] ) ) ? $profile["pb_catalog_tag_{$i}_name"] : __( 'Tag', 'pressbooks' ) . " $i"; ?></h3>
-			<ul>
-				<?php
-				foreach ( $tags as $val ) {
-					$tag_url = _tag_url( $catalog->getUserId(), $i, $val['id'] );
-					echo "<li><a href='$tag_url' ";
-					if ( $i == @$_REQUEST['tag_group'] && $val['id'] == @$_REQUEST['tag_id'] ) {
-						echo 'class="active"';
-						$h1_title = __( 'Catalog, filtering by', 'pressbooks' ) . ": {$val['tag']}";
-						$h1_title .= ' <span style="font-weight:normal;font-size:60%;">[<a href="' . _base_url( $catalog->getUserId() ) . '">x</a>]</span>';
+		<div class="log-wrap">	<!-- Login/Logout -->
+		   <?php if (! is_single()): ?>
+		    	<?php if (!is_user_logged_in()): ?>
+					<a href="<?php echo wp_login_url(); ?>" class=""><?php _e('login', 'pressbooks'); ?></a>
+		   	 	<?php else: ?>
+					<a href="<?php echo  wp_logout_url(); ?>" class=""><?php _e('logout', 'pressbooks'); ?></a>
+					<?php if (is_user_member_of_blog()): ?>
+					<a href="<?php echo get_option('home'); ?>/wp-admin"><?php _e('Admin', 'pressbooks'); ?></a>
+					<?php endif; ?>
+		    	<?php endif; ?>
+		    <?php endif; ?>
+		</div> <!-- end .log-wrap -->
+	<div id="catalog-sidebar" class="catalog-sidebar">
+		<h2 class="pressbooks-logo">
+			<a href="/">PressBooks</a>
+		</h2>
+		<p class="tag-menu assistive-text">Menu</p>
+		<div class="sidebar-inner-wrap">
+			<?php if ( $profile['pb_catalog_url'] ): ?><a href="<?php echo $profile['pb_catalog_url']; ?>"><?php endif; ?>
+			<img class="catalog-logo" src="<?php echo _logo_url( $profile ); ?>" alt="catalog-logo" width="100" height="99" />
+			<?php if ( $profile['pb_catalog_url'] ): ?></a><?php endif; ?>
+			<p class="about-blurb"><?php echo $profile['pb_catalog_about']; ?></p>
+			<br />
+	
+			<!-- Tags -->
+			<?php for ( $i = 1; $i <= 2; ++$i ) : ?>
+			<?php $tags = $catalog->getTags( $i, false ); ?>
+				<h3><?php echo ( ! empty( $profile["pb_catalog_tag_{$i}_name"] ) ) ? $profile["pb_catalog_tag_{$i}_name"] : __( 'Tag', 'pressbooks' ) . " $i"; ?></h3>
+				<ul>
+					<?php
+					foreach ( $tags as $val ) {
+						$tag_url = _tag_url( $catalog->getUserId(), $i, $val['id'] );
+						echo "<li><a href='$tag_url' ";
+						if ( $i == @$_REQUEST['tag_group'] && $val['id'] == @$_REQUEST['tag_id'] ) {
+							echo 'class="active"';
+							$h1_title = __( 'Catalog, filtering by', 'pressbooks' ) . ": {$val['tag']}";
+							$h1_title .= ' <span style="font-weight:normal;font-size:60%;">[<a href="' . _base_url( $catalog->getUserId() ) . '">x</a>]</span>';
+						}
+						echo ">{$val['tag']}</a></li>" . "\n";
 					}
-					echo ">{$val['tag']}</a></li>" . "\n";
-				}
-				?>
-			</ul>
-		<?php endfor; ?>
-
+					?>
+				</ul>
+			<?php endfor; ?>
+		</div><!-- end .sidebar-inner-wra -->	
 
 	</div><!-- end catalog-sidebar -->
 
 	<!-- Books! -->
-	<?php
-
-	?>
-	<div class="catalog-content" id="catalog-content">
-
-		<h1><?php echo $h1_title ?></h1>
-
-
-		<!-- Books -->
-		<?php foreach ( $books as $b ) : ?>
-			<div class="book-data">
-
-				<div class="book">
-					<p class="book-description"><a href="<?php echo get_site_url( $b['blogs_id']  ); ?>"><?php echo $b['about']; ?><span class="book-link">&rarr;</span></a></p>
-					<img src="<?php echo $b['cover_url']['pb_cover_medium']; ?>" alt="book-cover" width="225" height="<?php echo $b['cover_height']; ?>" />
-				</div><!-- end .book -->
-
-				<div class="book-info">
-					<h2><?php echo $b['title']; ?></h2>
-
-					<p><a href="<?php echo get_site_url( $b['blogs_id'] ); ?>"><?php echo $b['author']; ?></a></p>
-				</div><!-- end book-info -->
-
-			</div><!-- end .book-data -->
-		<?php
-		endforeach;
-		?>
-
-
-	</div>	<!-- end .catalog -->
+	<div class="catalog-content-wrap">		
+		<div class="catalog-content" id="catalog-content">
+			<h1><?php echo $h1_title ?></h1>
+	
+	
+			<!-- Books -->
+			<?php foreach ( $books as $b ) : ?>
+				<div class="book-data">
+	
+					<div class="book">
+						<p class="book-description"><a href="<?php echo get_site_url( $b['blogs_id']  ); ?>"><?php echo $b['about']; ?><span class="book-link">&rarr;</span></a></p>
+						<img src="<?php echo $b['cover_url']['pb_cover_medium']; ?>" alt="book-cover" width="225" height="<?php echo $b['cover_height']; ?>" />
+					</div><!-- end .book -->
+	
+					<div class="book-info">
+						<h2><?php echo $b['title']; ?></h2>
+	
+						<p><a href="<?php echo get_site_url( $b['blogs_id'] ); ?>"><?php echo $b['author']; ?></a></p>
+					</div><!-- end book-info -->
+	
+				</div><!-- end .book-data -->
+			<?php
+			endforeach;
+			?>
+			</div>	<!-- end .catalog-content-->
+			<div class="footer">
+				<p> <a href="/">PressBooks: the CMS for Books.</a></p>
+			</div>
+		
+		</div>	<!-- end .catalog-content-wrap -->
 
 </div><!-- end .catalog-wrap -->
+
+
 
 <?php wp_footer(); ?>
 </body>
