ecde07be1050a6e2678c0f0f47db1206070b6ba7
Dac Chartrand <dac.chartrand@gmail.com>
2013-07-10 22:16:23 -0400

    Fix error appearing in JavaScript Console Viewport argument value "device-width;" for key "width" is invalid, and has been ignored. Note that ';' is not a separator in viewport values. The list should be comma-separated.

--- themes-book/pressbooks-book/header.php
+++ themes-book/pressbooks-book/header.php
@@ -12,7 +12,7 @@
     <![endif]-->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width; initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico" />
 <title><?php
 	global $page, $paged;
