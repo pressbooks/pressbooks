f502aca2edf07cbf871b2ddf0df998deeef7e8cb
Sonia <sonia@nudge.ca>
2013-07-15 09:31:47 -0400

    Changing data text for Book cover share buttons.
    Signed-off-by: Sonia <sonia@nudge.ca>

--- themes-book/pressbooks-book/page-cover-second-block.php
+++ themes-book/pressbooks-book/page-cover-second-block.php
@@ -11,9 +11,9 @@
 								<?php endif; ?>	
 								
 							  <div id="share">
-								  <div id="twitter" data-url="<?php the_permalink(); ?>" data-text="Check out this book I made using PressBooks" data-title="Tweet"></div>
-								  <div id="facebook" data-url="<?php the_permalink(); ?>" data-text="Check out this book I made using PressBooks" data-title="Like"></div>
-								  <div id="googleplus" data-url="<?php the_permalink(); ?>" data-text="Check out this book I made using PressBooks" data-title="+1"></div>
+								  <div id="twitter" data-url="<?php the_permalink(); ?>" data-text="Check out this great book on PressBooks." data-title="Tweet"></div>
+								  <div id="facebook" data-url="<?php the_permalink(); ?>" data-text="Check out this great book on PressBooks." data-title="Like"></div>
+								  <div id="googleplus" data-url="<?php the_permalink(); ?>" data-text="Check out this great book on PressBooks." data-title="+1"></div>
 </div>	
 						</div>
 							
