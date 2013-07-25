801bac14af36a9a477737acd93965fbb6f40adae
Dac Chartrand <dac.chartrand@gmail.com>
2013-07-25 12:04:06 -0400

    Remove RSD and WLW from headers. We don't use this.

--- hooks.php
+++ hooks.php
@@ -134,3 +134,5 @@ add_action( 'init', function () {
 // -------------------------------------------------------------------------------------------------------------------
 
 add_filter( 'xmlrpc_enabled', '__return_false' );
+remove_action( 'wp_head', 'rsd_link' );
+remove_action( 'wp_head', 'wlwmanifest_link' );
