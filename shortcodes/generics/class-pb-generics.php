2386419b4dedeed35b07c6f64e0ed96af27f1456
Ned Zimmerman <ned@bight.ca>
2013-12-27 16:15:40 -0400

    Allow more complex generic short codes based on $key => $value pairs.

--- shortcodes/generics/class-pb-generics.php
+++ shortcodes/generics/class-pb-generics.php
@@ -15,19 +15,28 @@ class Generics {
 	/**
 	 * @array Protected array of generic shortcodes.
 	 */
-	protected $generics = array( 'blockquote', 'em', 'sup' );
-
+	protected $generics = array(
+		'blockquote' 	=> 'blockquote',
+		'bold' 			=> 'strong',
+		'code'			=> 'code',
+		'em' 			=> 'em',
+		'italics' 		=> 'em',
+		'strong'		=> 'strong',
+		'textbox'		=> 'div class="textbox"',
+	);
+	
 	/**
 	 * This is our constructor, which is private to force the use of getInstance().
 	 * We use an anonymous function to generate handler functions for all $generics.
 	 */
 	private function __construct() {
 		
-		foreach ( $this->generics as $shortcode )
-			add_shortcode( $shortcode, function ( $atts, $content = '', $shortcode ) {
+		foreach ( $this->generics as $shortcode => $tag ) {
+			add_shortcode( $shortcode, function ( $atts, $content = '' ) use( $tag) {
 				if ( ! $content ) { return ''; }
-				return '<' . $shortcode . '>' . do_shortcode( $content ) . '</' . $shortcode . '>';
+				return '<' . $tag . '>' . do_shortcode( $content ) . '</' . $tag . '>';
 			} );
+		}
 	}
 
 	/**
@@ -43,6 +52,3 @@ class Generics {
 	}
 
 }
-
-
-
