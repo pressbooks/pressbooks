5455ebd11a20ea7059e67d38265c88fecda369b4
Ned Zimmerman <ned@bight.ca>
2014-01-18 19:07:53 -0500

    Updated version elsewhere.

--- pressbooks.php
+++ pressbooks.php
@@ -3,7 +3,7 @@
 Plugin Name: PressBooks
 Plugin URI: http://www.pressbooks.com
 Description: Simple Book Production
-Version: 2.2.1
+Version: 2.3
 Author: BookOven Inc.
 Author URI: http://www.pressbooks.com
 Text Domain: pressbooks
