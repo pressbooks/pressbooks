6a50389a151b9563564cbf5c0e58ec59517bd37c
Ned Zimmerman <ned@bight.ca>
2014-03-28 20:30:57 -0400

    Added 5" x 8" page size for PDFs.

--- themes-book/pressbooks-book/functions.php
+++ themes-book/pressbooks-book/functions.php
@@ -461,7 +461,7 @@ function pressbooks_theme_options_pdf_init() {
 			 __( 'Pocket (4.25&quot; &times; 7&quot;)', 'pressbooks' ),
 			 __( 'A4 (21cm &times; 29.7cm)', 'pressbooks' ),
 			 __( 'A5 (14.8cm &times; 21cm)', 'pressbooks' ),
-
+			 __( '5&quot; &times; 8&quot;', 'pressbooks' ),
 		)
 	);
 	add_settings_field(
@@ -915,6 +915,9 @@ function pressbooks_theme_pdf_css_override( $css ) {
 			break;
 		case 8:
 			$css .= "@page { size: 14.8cm 21cm; } \n";
+			break;
+		case 9:
+			$css .= "@page { size: 5in 8in; } \n";
 			break;
 	}
 
