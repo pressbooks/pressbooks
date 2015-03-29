c834e2a0440d3f6bcbedbf3ebee9d88e1bc74b18
Ned Zimmerman <ned@bight.ca>
2015-03-29 15:00:08 -0400

    Initial support for ODT export (#104).

--- admin/templates/export.php
+++ admin/templates/export.php
@@ -108,6 +108,7 @@ if ( ! empty( $_GET['export_warning'] ) && ( get_option( 'pressbooks_email_valid
 	if ( ! isset( $options['xhtml'] ) ) { $options['xhtml'] = 0; }
 	if ( ! isset( $options['wxr'] ) ) { $options['wxr'] = 0; }
 	if ( ! isset( $options['vanillawxr'] ) ) { $options['vanillawxr'] = 0; }
+	if ( ! isset( $options['odt'] ) ) { $options['odt'] = 0; }
 	?>
     <form id="pb-export-form" action="<?php echo $export_form_url ?>" method="POST">
 	    <fieldset>
@@ -128,7 +129,8 @@ if ( ! empty( $_GET['export_warning'] ) && ( get_option( 'pressbooks_email_valid
 	    	<input type="checkbox" id="icml" name="export_formats[icml]" value="1" <?php checked(1, $options['icml'], false); ?>/><label for="icml"> <?php _e( 'ICML (for InDesign)', 'pressbooks' ); ?></label><br />
 	    	<input type="checkbox" id="xhtml" name="export_formats[xhtml]" value="1" <?php checked(1, $options['xhtml'], false); ?>/><label for="xhtml"> <?php _e( 'XHTML', 'pressbooks' ); ?></label><br />
 	    	<input type="checkbox" id="wxr" name="export_formats[wxr]" value="1" <?php checked(1, $options['wxr'], false); ?>/><label for="wxr"> <?php _e( 'PressBooks XML', 'pressbooks' ); ?></label><br />
-	    	<input type="checkbox" id="vanillawxr" name="export_formats[vanillawxr]" value="1" <?php checked(1, $options['vanillawxr'], false); ?>/><label for="vanillawxr"> <?php _e( 'WordPress XML', 'pressbooks' ); ?></label>
+	    	<input type="checkbox" id="vanillawxr" name="export_formats[vanillawxr]" value="1" <?php checked(1, $options['vanillawxr'], false); ?>/><label for="vanillawxr"> <?php _e( 'WordPress XML', 'pressbooks' ); ?></label><br/>
+	    	<input type="checkbox" id="odt" name="export_formats[odt]" value="1" <?php checked(1, $options['odt'], false); ?>/><label for="odt"> <?php _e( 'ODT', 'pressbooks' ); ?></label>
 	    </fieldset>
     </form>
     <div class="clear"></div>
