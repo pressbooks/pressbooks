402591701158170331b67aa242c02da856bb2355
Ned Zimmerman <ned@bight.ca>
2015-04-23 10:38:51 -0400

    Updates to #104.

--- includes/modules/export/odt/class-pb-odt.php
+++ includes/modules/export/odt/class-pb-odt.php
@@ -43,10 +43,14 @@ class Odt extends Export {
 	function __construct( array $args ) {
 
 		// Some defaults
+		if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+			if ( ! defined( 'PB_SAXON_COMMAND' ) )
+			define( 'PB_SAXON_COMMAND', 'java -jar ' . PB_PLUGIN_DIR . 'symbionts/saxonhe/saxon9he.jar' );
 
-		if ( ! defined( 'PB_SAXON_COMMAND' ) )
-			define( 'PB_SAXON_COMMAND', '/usr/bin/java -jar ' . PB_PLUGIN_DIR . '/symbionts/saxonhe/saxon9he.jar' );
-
+		} else {
+			if ( ! defined( 'PB_SAXON_COMMAND' ) )
+			define( 'PB_SAXON_COMMAND', '/usr/bin/java -jar ' . PB_PLUGIN_DIR . 'symbionts/saxonhe/saxon9he.jar' );
+		}
 		// Set the access protected "format/xhtml" URL with a valid timestamp and NONCE
 		$timestamp = time();
 		$md5 = $this->nonce( $timestamp );
@@ -73,25 +77,117 @@ class Odt extends Export {
 		$contentPath = pathinfo($filename);
 		$source = $contentPath['dirname'] . '/source.xhtml';
 		file_put_contents( $source, $this->queryXhtml() );
-		$xslt = PB_PLUGIN_DIR . '/includes/modules/export/odt/xhtml2odt.xsl';
+		$xslt = PB_PLUGIN_DIR . 'includes/modules/export/odt/xhtml2odt.xsl';
 		$content = $contentPath['dirname'] . '/content.xml';
 		$mimeType = $contentPath['dirname'] . "/mimetype";
 		$metaFolder = $contentPath['dirname'] . "/META-INF";
 		$meta = $contentPath['dirname'] ."/meta.xml";
 		$settings = $contentPath['dirname'] . "/settings.xml";
 		$styles = $contentPath['dirname'] . "/styles.xml";
+		$graphicFolder = $contentPath['dirname'] . '/media/';
 		
-		$result = exec( PB_SAXON_COMMAND . ' -xsl:' . $xslt .' -s:' . $source .' -o:' . $content );
+		/*get all images and save it to the link folder to zip*/
+		$urlContent = file_get_contents($source);
 		
-		unlink( $source );
+		$urlContent = preg_replace("/xmlns\=\"http\:\/\/www\.w3\.org\/1999\/xhtml\"/i", '', $urlContent);
+		
+		$doc = new \DOMDocument();
+		$doc->loadXML($urlContent);
+		$xpath = new \DOMXPath($doc);
+		
+		$tables = $xpath->query('//table');
+		//to get column count of a table
+		foreach ($tables as $table){	
+			$colCount = 0;
+			$columns = $xpath->query('//tr[1]/*', $table);
+			foreach ($columns as $column){
+				if ($column->hasAttribute('colspan')){
+					$colCount = $colCount + $column->getAttribute('colspan');
+				}else{
+					$colCount++;
+				}
+			}
+			$table->setAttribute('colcount', $colCount);
+		}
+		
+		file_put_contents( $source, $doc->saveXML() );
+		
+		$images = $xpath->query('//img');
+		
+		$coverImages = $xpath->query('//meta[@name="pb-cover-image"]');
+		if (($images->length > 0) || ($coverImages->length > 0)){
+			//create a folder named as 'media'
+			$graphicPath = $contentPath['dirname'] . '/media/';
+			mkdir($graphicPath);
+		}
+		foreach ($images as $image){
+			//copy all images to the media folder
+			$src = $image->getAttribute('src');
+			$realPath = realpath($src);
+			if ($realPath){
+				$imgFileName = $graphicPath . basename($realPath);
+				if (! copy($realPath, $imgFileName)){
+					$this->logError( 'cannot copy image file ' .  basename($realPath));
+					return false;
+				}
+			}else{
+				$imgContent = file_get_contents($src);
+				if ((!$imgContent) || ($imgContent == "")){
+					$this->logError( 'cannot copy image file ' .  basename($src));
+					return false;
+				}
+				$imgFileName = $graphicPath . basename($src);
+				file_put_contents($imgFileName, $imgContent);
+			}
+		}
+		
+		foreach ($coverImages as $coverImage){
+			//copy cover image to the media folder
+			$src = $coverImage->getAttribute('content');
+			$realPath = realpath($src);
+			if ($realPath){
+				$imgFileName = $graphicPath . basename($realPath);
+				if (! copy($realPath, $imgFileName)){
+					$this->logError( 'cannot copy image file ' .  basename($realPath));
+					return false;
+				}
+			}else{
+				$imgContent = file_get_contents($src);
+				if ((!$imgContent) || ($imgContent == "")){
+					$this->logError( 'cannot copy image file ' .  basename($src));
+					return false;
+				}
+				$imgFileName = $graphicPath . basename($src);
+				file_put_contents($imgFileName, $imgContent);
+			}
+		}
+		
+		try {
+			$result = exec( PB_SAXON_COMMAND . ' -xsl:' . $xslt .' -s:' . $source .' -o:' . $content);
+		} catch ( \Exception $e ) {
+			$this->logError( $e->getMessage() );
+			return false;
+		}
+
+		if ((!file_exists($content)) ||  (!file_exists($mimeType)) || (!file_exists($meta)) || (!file_exists($settings)) || (!file_exists($styles)) || (!file_exists($metaFolder))){
+			$this->logError( 'Transformation failed' );
+			return false;
+		}
 		
 		$zip = new \PclZip( $filename );
 		
-		$list = $zip->add($mimeType . ',' . $content . ',' . $meta . ',' . $settings . ',' . $styles . ',' . $metaFolder, PCLZIP_OPT_NO_COMPRESSION, PCLZIP_OPT_REMOVE_PATH, $contentPath['dirname'] . '/' );
-		if ( $list == 0 ) {
-			die( "Error : " . $zip->errorInfo(true) );
+		if ($images->length > 0){
+			$list = $zip->add($mimeType . ',' . $content . ',' . $meta . ',' . $settings . ',' . $styles . ',' . $graphicFolder . ',' . $metaFolder, PCLZIP_OPT_NO_COMPRESSION, PCLZIP_OPT_REMOVE_PATH, $contentPath['dirname'] . '/' );
+		}else{
+			$list = $zip->add($mimeType . ',' . $content . ',' . $meta . ',' . $settings . ',' . $styles . ',' . $metaFolder, PCLZIP_OPT_NO_COMPRESSION, PCLZIP_OPT_REMOVE_PATH, $contentPath['dirname'] . '/' );
 		}
 		
+		if ( $list == 0 ) {
+			$this->logError( $zip->errorInfo(true) );
+			//die( "Error : " . $zip->errorInfo(true) );
+			return false;
+		}
+		unlink( $source );
 		unlink( $mimeType );
 		unlink( $content );
 		unlink( $meta );
@@ -99,10 +195,31 @@ class Odt extends Export {
 		unlink( $styles );
 		unlink( $metaFolder . '/manifest.xml' );
 		rmdir( $metaFolder);
+		if ($images->length > 0){
+			$this -> deleteDir( $graphicPath );
+		}
 		
 		return true;
 	}
 
+	/*Recursive Directory Deletion for media folder */
+	public static function deleteDir($dirPath) {
+			if (! is_dir($dirPath)) {
+				throw new InvalidArgumentException("$dirPath must be a directory");
+		}
+		if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+			$dirPath .= '/';
+		}
+		$files = glob($dirPath . '*', GLOB_MARK);
+		foreach ($files as $file) {
+			if (is_dir($file)) {
+				self::deleteDir($file);
+			} else {
+				unlink($file);
+			}
+		}
+		rmdir($dirPath);
+	}
 
 	/**
 	 * Query the access protected "format/xhtml" URL, return the results.
