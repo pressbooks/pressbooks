e1bf0f0be239ebcf8fe40bdc23cc5b386d735a62
Ned Zimmerman <ned@Scorpio.lan>
2015-08-07 16:52:32 -0300

    Default network language now applies itself properly to new books; book info language selection sets locale; users' language selection overrides book language selection for dashboard only.

--- admin/pb-admin-metaboxes.php
+++ admin/pb-admin-metaboxes.php
@@ -35,7 +35,6 @@ function title_update( $pid, $post ) {
 	}
 }
 
-
 /**
  * If the user leaves certain meta info blank, forcefully fill it with our own
  *
@@ -60,8 +59,10 @@ function add_required_data( $pid, $post ) {
 
 	$pb_language = get_post_meta( $pid, 'pb_language', true );
 	if ( ! $pb_language ) {
-		// if the pb_language metadata value is not set, set it to 'en'
-		update_post_meta( $pid, 'pb_language', 'en' );
+		// if the pb_language metadata value is not set, set it to the network default
+		$locale = get_site_option( 'WPLANG' );
+		$locale = array_search( $locale, \PressBooks\L10n\wplang_codes() );
+ 		update_post_meta( $pid, 'pb_language', $locale );
 	}
 
 	$pb_cover_image = get_post_meta( $pid, 'pb_cover_image', true );
