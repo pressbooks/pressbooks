db9245bcbb6b79c0a39f91d3de66757184b6312a
Ned Zimmerman <ned@bight.ca>
2015-09-22 16:48:48 -0300

    Added theme information files for all included themes.

---
+++ themes-book/clarke/theme-information.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author  Pressbooks <code@pressbooks.org>
+ * @license GPLv2 (or any later version)
+ */
+
+/* ------------------------------------------------------------------------ *
+ * Provide Pressbooks with some information about this theme.
+ * ------------------------------------------------------------------------ */
+		
+$supported_languages = array(
+	'grc',
+);
---
+++ themes-book/donham/theme-information.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author  Pressbooks <code@pressbooks.org>
+ * @license GPLv2 (or any later version)
+ */
+
+/* ------------------------------------------------------------------------ *
+ * Provide Pressbooks with some information about this theme.
+ * ------------------------------------------------------------------------ */
+		
+$supported_languages = array(
+	'grc',
+	'he',
+);
