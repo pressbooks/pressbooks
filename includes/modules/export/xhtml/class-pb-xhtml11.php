bed74757f74ed2b0d3eaef2ac444db8d688d04b1
Dac Chartrand <dac.chartrand@gmail.com>
2015-09-29 18:21:00 -0400

    Fix xml:lang. See issue #257

--- includes/modules/export/xhtml/class-pb-xhtml11.php
+++ includes/modules/export/xhtml/class-pb-xhtml11.php
@@ -456,9 +456,11 @@ class Xhtml11 extends Export {
 	 */
 	protected function echoDocType( $book_contents, $metadata ) {
 
+		$lang = isset( $metadata['pb_language'] ) ? $metadata['pb_language'] : 'en';
+
 		echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
 		echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">' . "\n";
-		echo '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">' . "\n";
+		echo '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="' . $lang . '">' . "\n";
 	}
 
 
