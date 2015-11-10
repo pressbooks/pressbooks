a8ee4e7cff131d5cbae2d251fda926656979269d
Dac Chartrand <dac.chartrand@gmail.com>
2015-11-10 15:25:12 -0500

    More tests for export class.

--- tests/test-modules-export.php
+++ tests/test-modules-export.php
@@ -110,8 +110,7 @@ class Modules_ExportTest extends \WP_UnitTestCase {
 
 		switch_theme( 'pressbooks-custom-css' );
 
-		$old = get_option( 'pressbooks_theme_options_pdf' );
-		$opt = $old;
+		$opt = get_option( 'pressbooks_theme_options_pdf' );
 
 		$opt['pdf_romanize_parts'] = 0;
 		update_option( 'pressbooks_theme_options_pdf', $opt);
@@ -130,8 +129,6 @@ class Modules_ExportTest extends \WP_UnitTestCase {
 
 		$path = $this->export->getExportScriptPath( 'foobar' );
 		$this->assertFalse( $path );
-
-		update_option( 'pressbooks_theme_options_pdf', $old );
 	}
 
 
@@ -167,7 +164,7 @@ class Modules_ExportTest extends \WP_UnitTestCase {
 //	 * @covers \PressBooks\Modules\Export\Export::logError
 //	 */
 //	public function test_logError() {
-//		// TODO: Testing this as-is would send emails. Need to refactor to allow mocking of postmarkapp endpoint.
+//		// TODO: Testing this as-is would send emails, writes to error log... Need to be refactored.
 //		$this->markTestIncomplete();
 //	}
 
@@ -184,5 +181,77 @@ class Modules_ExportTest extends \WP_UnitTestCase {
 		$this->assertEquals( 'Hello world!', file_get_contents( $file ) );
 	}
 
+
+	/**
+	 * @covers \PressBooks\Modules\Export\Export::timestampedFileName
+	 */
+	public function test_timestampedFileName() {
+
+		$this->_book();
+
+		$file = $this->export->timestampedFileName( 'epub', true );
+		$this->assertStringEndsWith( '.epub', $file );
+		$this->assertStringStartsWith( '/', $file );
+
+		update_option( 'blogname', '!My+Book+Name!' );
+		$file = $this->export->timestampedFileName( 'foo', false );
+		$this->assertStringEndsWith( '.foo', $file );
+		$this->assertStringStartsNotWith( '/', $file );
+		$this->assertNotContains( '!', $file );
+		$this->assertNotContains( '+', $file );
+	}
+
+
+	/**
+	 * @covers \PressBooks\Modules\Export\Export::nonce
+	 * @covers \PressBooks\Modules\Export\Export::verifyNonce
+	 */
+	public function test_nonce_AND_verifyNonce() {
+
+		if ( ! defined( 'NONCE_KEY' ) ) {
+			define( 'NONCE_KEY', '40~wF,SH)lm,Zr+^[b?_M8Z.g4gk%^gnqr+ZtnT,p6_K5.NuuN 0g@Y|T9+yBI|{' );
+		}
+
+		$time1 = time();
+		$nonce1 = $this->export->nonce( $time1 );
+		$this->assertInternalType( 'string', $nonce1 );
+
+		$time2 = $time1 + 1;
+		$nonce2 = $this->export->nonce( $time2 );
+		$this->assertNotEquals( $nonce1, $nonce2 );
+
+		$this->assertTrue( $this->export->verifyNonce( $time1, $nonce1 ) );
+		$this->assertFalse( $this->export->verifyNonce( $time1, $nonce2 ) );
+
+		$time3 = $time1 - ( 60 * 5 + 1 );
+		$nonce3 = $this->export->nonce( $time3 );
+		$this->assertFalse( $this->export->verifyNonce( $time3, $nonce3 ) );
+	}
+
+
+	/**
+	 * @covers \PressBooks\Modules\Export\Export::mimeType
+	 */
+	function test_mimeType() {
+
+		$i = $this->export;
+		$mime = $i::mimeType( __DIR__ . '/data/pb.png' );
+		$this->assertStringStartsWith( 'image/png', $mime );
+	}
+
+
+	/**
+	 * @covers \PressBooks\Modules\Export\Export::getExportFolder
+	 */
+	function test_getExportFolder() {
+
+		$this->_book();
+
+		$i = $this->export;
+		$path = $i::getExportFolder();
+
+		$this->assertTrue( is_dir( $path ) );
+		$this->assertStringStartsWith( 'deny from all', file_get_contents( $path . '.htaccess' ) );
+	}
 
 }
