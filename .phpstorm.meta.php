951ad109682cd2b51c57e0a0c2145ee935192afb
Ned Zimmerman <ned@bight.ca>
2016-01-06 16:59:48 -0400

    Restored PHPStorm meta file.

---
+++ .phpstorm.meta.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @see http://confluence.jetbrains.com/display/PhpStorm/PhpStorm+Advanced+Metadata
+ */
+namespace PHPSTORM_META {
+	/** @noinspection PhpUnusedLocalVariableInspection */
+	/** @noinspection PhpIllegalArrayKeyTypeInspection */
+	$STATIC_METHOD_TYPES = [
+		\PressBooks\Container::get( '' ) => [
+			'Sass' instanceof \PressBooks\Sass,
+			'GlobalTypography' instanceof \PressBooks\GlobalTypography,
+		],
+	];
+}
