d692be1cec42d30785d288868e5ac7bc4424e820
Ned Zimmerman <ned@bight.ca>
2016-01-12 17:48:07 -0400

    Initial integration of built in Google Analytics controls.

--- hooks-admin.php
+++ hooks-admin.php
@@ -15,6 +15,7 @@ require( PB_PLUGIN_DIR . 'includes/admin/pb-dashboard.php' );
 require( PB_PLUGIN_DIR . 'includes/admin/pb-laf.php' );
 require( PB_PLUGIN_DIR . 'includes/admin/pb-metaboxes.php' );
 require( PB_PLUGIN_DIR . 'includes/admin/pb-customcss.php' );
+require( PB_PLUGIN_DIR . 'includes/admin/pb-analytics.php' );
 require( PB_PLUGIN_DIR . 'includes/admin/pb-network-managers.php' );
 require( PB_PLUGIN_DIR . 'includes/admin/pb-fonts.php' );
 
@@ -60,6 +61,14 @@ add_action( 'admin_body_class', '\PressBooks\Admin\Laf\disable_customizer');
 // Hacks
 add_action( 'edit_form_advanced', '\PressBooks\Admin\Laf\edit_form_hacks' );
 
+// Analytics
+add_action( 'network_admin_menu', '\PressBooks\Admin\Analytics\add_network_menu' );
+add_action( 'admin_init', '\PressBooks\Admin\Analytics\network_analytics_settings_init' );
+if ( \PressBooks\Book::isBook() ) {
+	add_action( 'admin_menu', '\PressBooks\Admin\Analytics\add_menu' );
+	add_action( 'admin_init', '\PressBooks\Admin\Analytics\analytics_settings_init' );
+}
+
 // Privacy, Ecommerce, and Export settings
 add_action( 'admin_init', '\PressBooks\Admin\Laf\privacy_settings_init' );
 add_action( 'admin_init', '\PressBooks\Admin\Laf\ecomm_settings_init' );
@@ -72,7 +81,6 @@ add_filter( 'admin_title', '\PressBooks\Admin\Laf\admin_title' );
 add_action( 'admin_notices', '\PressBooks\Admin\Laf\admin_notices' );
 
 // Network Manager routines
-
 add_filter( 'admin_body_class', '\PressBooks\Admin\NetworkManagers\admin_body_class' );
 add_action( 'network_admin_menu', '\PressBooks\Admin\NetworkManagers\add_menu' );
 add_action( 'wp_ajax_pb_update_admin_status', '\PressBooks\Admin\NetworkManagers\update_admin_status' );
