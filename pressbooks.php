a740b92dec475cade3e3e15018aca50e428adf9b
Ned Zimmerman <ned@bight.ca>
2016-01-29 21:46:20 -0400

    Fix build.

--- pressbooks.php
+++ pressbooks.php
@@ -46,7 +46,7 @@ if ( ! defined( 'PB_PLUGIN_VERSION' ) )
 	define ( 'PB_PLUGIN_VERSION', '3.2' );
 
 if ( ! defined( 'PB_PLUGIN_DIR' ) )
-	define ( 'PB_PLUGIN_DIR', WP_PLUGIN_DIR . '/pressbooks/' ); // Must have trailing slash!
+	define ( 'PB_PLUGIN_DIR', __DIR__ . '/' ); // Must have trailing slash! 
 
 if ( ! defined( 'PB_PLUGIN_URL' ) )
 	define ( 'PB_PLUGIN_URL', plugins_url( 'pressbooks/' ) ); // Must have trailing slash!
@@ -76,7 +76,6 @@ spl_autoload_register( '_pressbooks_autoload' );
 // -------------------------------------------------------------------------------------------------------------------
 // Check minimum requirements
 // -------------------------------------------------------------------------------------------------------------------
-
 if ( ! function_exists( 'pb_meets_minimum_requirements' ) && ! @include_once( WP_PLUGIN_DIR . '/pressbooks/compatibility.php' ) ) {
 	add_action( 'admin_notices', function () {
 		echo '<div id="message" class="error fade"><p>' . __( 'Cannot find Pressbooks install.', 'pressbooks' ) . '</p></div>';
