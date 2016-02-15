fe1d87155437456b08a6daeac1255ad16b2d6d64
Ned Zimmerman <ned@bight.ca>
2016-02-15 17:31:12 -0400

    Adjusted link.

--- pressbooks.php
+++ pressbooks.php
@@ -46,7 +46,7 @@ if ( ! defined( 'PB_PLUGIN_VERSION' ) )
 	define ( 'PB_PLUGIN_VERSION', '3.2.0' );
 
 if ( ! defined( 'PB_PLUGIN_DIR' ) )
-	define( 'PB_PLUGIN_DIR', ( is_link( WP_PLUGIN_DIR .  '/pressbooks/' ) ? WP_PLUGIN_DIR .  '/pressbooks/' : __DIR__ . '/' ) ); // Must have trailing slash!
+	define( 'PB_PLUGIN_DIR', ( is_link( WP_PLUGIN_DIR .  '/pressbooks' ) ? WP_PLUGIN_DIR .  '/pressbooks/' : __DIR__ . '/' ) ); // Must have trailing slash!
 	
 if ( ! defined( 'PB_PLUGIN_URL' ) )
 	define ( 'PB_PLUGIN_URL', plugins_url( 'pressbooks/' ) ); // Must have trailing slash!
