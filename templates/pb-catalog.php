d728fe0d7d0b0cd385629586e3eae662febf27cd
Ned Zimmerman <ned@bight.ca>
2016-05-10 09:50:50 -0300

    Use secure requests for all Google Fonts.
    Parity with https://github.com/lumenlearning/pressbooks/commit/320aa6f.

--- templates/pb-catalog.php
+++ templates/pb-catalog.php
@@ -149,7 +149,7 @@ $_current_user_id = $catalog->getUserId();
 	<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico" />
 	<title><?php echo ucfirst( get_userdata( $pb_user_id )->user_login ); _e( '\'s Catalog Page', 'pressbooks' ); ?> | Pressbooks</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo \Pressbooks\Utility\asset_path( 'styles/style-catalog.css' ); ?>" />
-	<link href='<?php echo \Pressbooks\Sanitize\maybe_https( 'http://fonts.googleapis.com/css?family=Oswald|Open+Sans:400,400italic,600' ); ?>' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Oswald|Open+Sans:400,400italic,600' rel='stylesheet' type='text/css'>
 	<script type="text/javascript" src="<?php echo network_site_url( '/wp-includes/js/jquery/jquery.js?ver=1.10.2' ); ?>"></script>
 	<script src="<?php echo \Pressbooks\Utility\asset_path( 'scripts/matchheight.js' ); ?>" type="text/javascript"></script>
 	<script src="<?php echo \Pressbooks\Utility\asset_path( 'scripts/isotope.js' ); ?>" type="text/javascript"></script>
