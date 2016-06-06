7b8eba6463212d89f75abee569148746b8e563e9
Ned Zimmerman <ned@bight.ca>
2016-06-06 14:19:15 -0300

    Set up Clarke 2.0.

--- themes-book/clarke/functions.php
+++ themes-book/clarke/functions.php
@@ -4,33 +4,6 @@
  * @license GPLv2 (or any later version)
  */
 
-
-/**
- * Clarke features we inject ourselves, (not user options)
- *
- * @param $css
- *
- * @return string
- */
-function clarke_theme_pdf_css_override( $css ) {
-
-	// Translate "Part" to whatever language this book is in
-	$css .= '#toc .part a::before { content: "' . __( 'part', 'pressbooks' ) . ' "counter(part) ". "; }' . "\n";
-
-	return $css;
-}
-add_filter( 'pb_pdf_css_override', 'clarke_theme_pdf_css_override' );
-
-
-/**
- * Clarke features we inject ourselves, (not user options)
- *
- * @param $css
- *
- * @return string
- */
-function clarke_theme_ebook_css_override( $css ) {
-
-	return $css;
-}
-add_filter( 'pb_epub_css_override', 'clarke_theme_ebook_css_override' );
+ add_theme_support('pressbooks_global_typography', [
+	 'grc'
+ ]);
