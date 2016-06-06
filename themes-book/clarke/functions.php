313fa04f9b2cd8ebf6d9d073eb1b4296bce274f6
Ned Zimmerman <ned@bight.ca>
2016-06-06 09:15:22 -0300

    Restore Clarke.

--- themes-book/clarke/functions.php
+++ themes-book/clarke/functions.php
@@ -4,10 +4,6 @@
  * @license GPLv2 (or any later version)
  */
 
-function clarke_theme_setup() {
-	add_theme_support( 'pressbooks_global_typography', [ 'grc' ] );
-}
-add_action( 'after_setup_theme', 'clarke_theme_setup' );
 
 /**
  * Clarke features we inject ourselves, (not user options)
