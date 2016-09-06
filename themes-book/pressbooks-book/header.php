c83e16bc8dfdc5ea6109b939eebdc36efda123c9
Chrillep <christian.widlund@gmail.com>
2016-09-06 10:28:39 +0200

    fix if statement for if (gt IE 9)|!(IE)
    current code comments out entire html

--- themes-book/pressbooks-book/header.php
+++ themes-book/pressbooks-book/header.php
@@ -4,7 +4,7 @@
 <!--[if IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie7"> <![endif]-->
 <!--[if IE 8 ]>    <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
 <!--[if IE 9 ]>    <html <?php language_attributes(); ?> class="no-js ie9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"> <!--<![endif]-->
+<!--[if (gt IE 9)|!(IE)]> <html <?php language_attributes(); ?> class="no-js"> <![endif]-->
 <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="https://html5shim.googlecode.com/svn/trunk/html5.js">
