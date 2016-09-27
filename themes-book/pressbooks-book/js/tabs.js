c1bbc325c38dbf602d914c88ee34b989b327c029
bdolor <bpayne@bccampus.ca>
2016-09-27 14:13:44 -0700

    functional tabs with static content

--- themes-book/pressbooks-book/js/tabs.js
+++ themes-book/pressbooks-book/js/tabs.js
@@ -1,11 +1,14 @@
-// ref: http://jqueryui.com/tabs/
-jQuery(document).ready(function($) {
-    $('#tabs').tabs();
+// reference — http://jqueryui.com/tabs/
+jQuery(document).ready(function ($) {
+    $('#tabs').tabs({
+        collapsible: true,
+        active: false,
+        classes: {
+            'ui-tabs-tab': 'ui-corner-top',
+            'ui-tabs-panel': 'ui-corner-bottom'
+        }
+    });
 
-    // $('.ui-tabs-panel').hide();
-    // $(".ui-tabs-active").removeClass("ui-tabs-active");
-    // $(".ui-state-active").removeClass("ui-state-active");
-    // $(".ui-tabs-tab").click(function() {
-    //     $($(this).attr("href") + " .ui-tabs-panel").first().show();
-    // });
+    $('.ui-tabs-nav').removeClass('ui-corner-all');
+
 });
--- themes-book/pressbooks-book/tabs-footer.php
+++ themes-book/pressbooks-book/tabs-footer.php
@@ -1,28 +1,15 @@
 <!-- tabs start -->
-<div id="tabs" class="ui-tabs ui-corner-all ui-widget ui-widget-content">
-
-	<ul role="tablist" class="ui-tabs-nav ui-corner-top ui-helper-reset ui-helper-clearfix ui-widget-header">
-		<li role="tab" tabindex="0"
-		    class="ui-tabs-tab ui-corner-top ui-state-default ui-tab"
-		    aria-controls="tabs-1" aria-labelledby="ui-id-1" aria-selected="false" aria-expanded="false"><a href="#tabs-1"
-		                                                                                                  role="presentation"
-		                                                                                                  tabindex="-1"
-		                                                                                                  class="ui-tabs-anchor"
-		                                                                                                  id="ui-id-1">Read +</a>
-		</li>
-		<li role="tab" tabindex="1" class="ui-tabs-tab ui-corner-top ui-state-default ui-tab" aria-controls="tabs-2"
-		    aria-labelledby="ui-id-2" aria-selected="false" aria-expanded="false"><a href="#tabs-2"
-		                                                                             role="presentation"
-		                                                                             tabindex="1"
-		                                                                             class="ui-tabs-anchor"
-		                                                                             id="ui-id-2">History +</a></li>
+<div id="tabs">
+	<ul>
+		<li><a href="#tabs-1">Read <span class="dashicons"></span></a></li>
+		<li><a href="#tabs-2">History <span class="dashicons"></span></a></li>
 	</ul>
 
-	<div id="tabs-1" style="display:none;">
+	<div id="tabs-1">
 		<p>some content</p>
 	</div>
 
-	<div id="tabs-2" style="display:none;">
+	<div id="tabs-2">
 		<p>some other content</p>
 	</div>
 
