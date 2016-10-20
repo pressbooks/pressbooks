27a3b5cac61737d98d6f915e7371695455ca781f
Ned Zimmerman <ned@bight.ca>
2016-10-20 12:22:48 -0300

    Tag and release Pressbooks 3.9.1.

--- pressbooks.php
+++ pressbooks.php
@@ -3,7 +3,7 @@
 Plugin Name: Pressbooks
 Plugin URI: https://pressbooks.com
 Description: Simple Book Production
-Version: 3.9.0
+Version: 3.9.1
 Author: Book Oven Inc.
 Author URI: https://pressbooks.com
 Text Domain: pressbooks
@@ -45,7 +45,7 @@ add_action( 'wp_login', '_pb_session_kill' );
 // -------------------------------------------------------------------------------------------------------------------
 
 if ( ! defined( 'PB_PLUGIN_VERSION' ) ) {
-	define( 'PB_PLUGIN_VERSION', '3.9.0' );
+	define( 'PB_PLUGIN_VERSION', '3.9.1' );
 }
 
 if ( ! defined( 'PB_PLUGIN_DIR' ) ) {
