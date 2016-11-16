ebf258f7a23be2f1811149c5302fd4f7474969d3
Ned Zimmerman <ned@bight.ca>
2016-11-15 22:35:39 -0400

    Fix template wrapper weirdness.

--- themes-root/pressbooks-publisher/index.php
+++ themes-root/pressbooks-publisher/index.php
@@ -1,4 +1,9 @@
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
+<?php if ( have_posts() ) :
+	while ( have_posts() ) :
+		the_post();
+			get_template_part( 'templates/page', 'header' );
+			get_template_part( 'templates/content', 'page' );
+	endwhile;
+else :
+	get_template_part( 'templates/content' );
+endif;
