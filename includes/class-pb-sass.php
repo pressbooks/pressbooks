504c68d832b7ec1ff4caf87d93b9c3234b1a66c4
Ned Zimmerman <ned@bight.ca>
2017-05-29 22:00:07 -0300

    Add routine to (maybe) regenerate webook stylesheet (fix #727)

--- includes/class-pb-sass.php
+++ includes/class-pb-sass.php
@@ -386,6 +386,23 @@ class Sass {
 	}
 
 	/**
+	 * If the current theme's version has increased, call updateWebBookStyleSheet().
+	 *
+	 * @return void
+	 */
+	function maybeUpdateWebBookStylesheet() {
+		$theme = wp_get_theme();
+		$current_version = $theme->get( 'Version' );
+		$last_version = get_option( 'pressbooks_theme_version', $current_version );
+
+		if ( version_compare( $current_version, $last_version ) > 0 ) {
+			$this->updateWebBookStyleSheet();
+		}
+
+		update_option( 'pressbooks_theme_version', $current_version );
+	}
+
+	/**
 	 * Fix relative/ambiguous URLs to web fonts
 	 *
 	 * @param $css
