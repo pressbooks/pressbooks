d5ccdc6d94661b141d9f131006557733b083c5e1
Dac Chartrand <dac.chartrand@gmail.com>
2017-06-07 13:28:37 -0400

    List books, support pagination (#770)

--- inc/api/endpoints/controller/class-books.php
+++ inc/api/endpoints/controller/class-books.php
@@ -4,12 +4,25 @@ namespace Pressbooks\Api\Endpoints\Controller;
 
 class Books extends \WP_REST_Controller {
 
+	const LIMIT = 10;
+
+	/**
+	 * @var Toc
+	 */
+	private $toc;
+
+	/**
+	 * @var array
+	 */
+	private $linkCollector = [];
+
 	/**
 	 * Table of contents
 	 */
 	public function __construct() {
 		$this->namespace = 'pressbooks/v2';
 		$this->rest_base = 'books';
+		$this->toc = new Toc();
 	}
 
 	/**
@@ -43,6 +56,106 @@ class Books extends \WP_REST_Controller {
 				],
 			],
 		] );
+	}
+
+	public function get_item_schema() {
+
+		$toc = $this->toc->get_item_schema();
+
+		$schema = [
+			'$schema' => 'http://json-schema.org/schema#',
+			'title' => 'book',
+			'type' => 'object',
+			'properties' => [
+				'id' => [
+					'description' => __( 'Unique identifier for the object.' ),
+					'type' => 'integer',
+					'context' => [ 'view' ],
+					'readonly' => true,
+				],
+				'link' => [
+					'description' => __( 'URL to the object.' ),
+					'type' => 'string',
+					'format' => 'uri',
+					'context' => [ 'view' ],
+					'readonly' => true,
+				],
+				'meta' => [
+					// TODO
+				],
+				'toc' => [
+					'description' => __( 'Table of Contents', 'pressbooks' ),
+					'type' => 'object',
+					'properties' => $toc['properties'],
+					'context' => [ 'view' ],
+					'readonly' => true,
+				],
+			],
+		];
+
+		return $this->add_additional_fields_schema( $schema );
+	}
+
+	/**
+	 * @param \WP_REST_Request $request
+	 *
+	 * @return bool
+	 */
+	public function get_items_permissions_check( $request ) {
+		return true;
+	}
+
+	/**
+	 * @param \WP_REST_Request $request
+	 *
+	 * @return \WP_REST_Response
+	 */
+	public function get_items( $request ) {
+
+		// Register missing Toc routes
+		$this->toc->register_routes();
+
+		$request_internal = new \WP_REST_Request( 'GET', '/pressbooks/v2/toc' );
+
+		$results = [];
+		foreach ( $this->getBlogIds( $request ) as $blog_id ) {
+			switch_to_blog( $blog_id );
+
+			$response_toc = rest_do_request( $request_internal );
+			$results[] = [
+				'id' => $blog_id,
+				'link' => get_blogaddress_by_id( $blog_id ),
+				'meta' => [], // TODO
+				'toc' => $response_toc->get_data(),
+			];
+			$this->linkCollector['books'][] = [ 'href' => get_rest_url( $blog_id ) ];
+
+			restore_current_blog();
+		}
+
+		$response = rest_ensure_response( [ 'books' => $results ] );
+		$response->add_links( $this->linkCollector );
+
+		return $response;
+	}
+
+	/**
+	 * @param \WP_REST_Request
+	 *
+	 * @return array blog ids
+	 */
+	private function getBlogIds( $request ) {
+
+		global $wpdb;
+
+		$limit = ! empty( $request['per_page'] ) ? $request['per_page'] : self::LIMIT;
+		$offset = ! empty( $request['page'] ) ? ( $request['page'] - 1 ) * $limit : 0;
+
+		$blogs = $wpdb->get_col(
+			$wpdb->prepare( "SELECT blog_id FROM {$wpdb->blogs} WHERE public = 1 AND archived = 0 AND spam = 0 AND deleted = 0 AND blog_id != %d LIMIT %d, %d ", get_network()->site_id, $offset, $limit )
+		);
+
+		return $blogs;
 	}
 
 }
