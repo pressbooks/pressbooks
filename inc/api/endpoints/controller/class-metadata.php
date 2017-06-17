383272a7648498c92125a7017ef622fa416fecef
Ned Zimmerman <ned@bight.ca>
2017-06-17 18:23:34 -0300

    Fill in some properties.

--- inc/api/endpoints/controller/class-metadata.php
+++ inc/api/endpoints/controller/class-metadata.php
@@ -98,26 +98,10 @@ class Metadata extends \WP_REST_Controller {
 	public function get_metadata( $request ) {
 
 		$meta = Book::getBookInformation();
-		$meta = $this->fixBookInformation( $meta, current_user_can( 'edit_posts' ), [
-			'pb_bisac_subject' => 'about',
-			'pb_subtitle' => 'alternativeHeadline',
-			'pb_author' => 'author',
-			'pb_contributing_authors' => 'contributor',
-			'pb_copyright_holder' => 'copyrightHolder',
-			'pb_copyright_year' => 'copyrightYear',
-			'pb_publication_date' => 'datePublished',
-			'pb_about_50' => 'description',
-			'pb_editor' => 'editor',
-			'pb_cover_image' => 'image',
-			'pb_language' => 'inLanguage',
-			'pb_keywords_tags' => 'keywords',
-			'pb_publisher' => 'publisher',
-			'pb_title' => 'name',
-		] );
+		$meta = $this->buildMetadata( $meta, current_user_can( 'edit_posts' ) );
 
 		$response = rest_ensure_response( $meta );
-		$this->linkCollector['self'] = [ 'href' => rest_url( sprintf( '%s/%s', $this->namespace, $this->rest_base ) ) ];
-		$response->add_links( $this->linkCollector );
+		// TODO Do we need the linkCollector here?
 
 		return $response;
 	}
@@ -125,21 +109,92 @@ class Metadata extends \WP_REST_Controller {
 	/**
 	 * @param array $book_information
 	 * @param $has_permission
-	 * @param array $replacement_keys
 	 *
 	 * @return array
 	 */
-	protected function fixBookInformation( array $book_information, $has_permission, array $replacement_keys ) {
+	protected function buildMetadata( array $book_information, $has_permission ) {
+
+		$mapped_properties = [
+			'pb_bisac_subject' => 'about',
+			'pb_title' => 'name',
+			'pb_short_title' => 'alternateName',
+			'pb_keywords_tags' => 'keywords',
+			'pb_subtitle' => 'alternativeHeadline',
+			'pb_language' => 'inLanguage',
+			'pb_copyright_year' => 'copyrightYear',
+			'pb_about_50' => 'description',
+			'pb_cover_image' => 'image',
+		];
 
 		$new_book_information = [
 			'@context' => 'http://schema.org',
 			'@type' => 'Book',
 		];
 
-		foreach ( $replacement_keys as $old => $new ) {
+		foreach ( $mapped_properties as $old => $new ) {
 			if ( isset( $book_information[ $old ] ) ) {
 				$new_book_information[ $new ] = $book_information[ $old ];
 			}
+		}
+
+		if ( isset( $book_information['pb_author'] ) ) {
+			$new_book_information['author'] = [
+				'@type' => 'Person',
+				'name' => $book_information['pb_author'],
+			];
+
+			if ( isset( $book_information['pb_author_file_as'] ) ) {
+				$new_book_information['author']['alternateName'] = $book_information['pb_author_file_as'];
+			}
+		}
+
+		if ( isset( $book_information['pb_contributing_authors'] ) ) {
+			$contributing_authors = explode( ', ', $book_information['pb_contributing_authors'] );
+			foreach ( $contributing_authors as $contributor ) {
+				$new_book_information['contributor'][] = [
+					'@type' => 'Person',
+					'name' => $contributor,
+				];
+			}
+		}
+
+		if ( isset( $book_information['pb_editor'] ) ) {
+			$new_book_information['editor'] = [
+				'@type' => 'Person',
+				'name' => $book_information['pb_editor'],
+			];
+		}
+
+		if ( isset( $book_information['pb_translator'] ) ) {
+			$new_book_information['translator'] = [
+				'@type' => 'Person',
+				'name' => $book_information['pb_translator'],
+			];
+		}
+
+		if ( isset( $book_information['pb_publisher'] ) ) {
+			$new_book_information['publisher'] = [
+				'@type' => 'Organization',
+				'name' => $book_information['pb_publisher'],
+			];
+
+			if ( isset( $book_information['pb_publisher_city'] ) ) {
+				$new_book_information['publisher']['address'] = [
+					'@type' => 'PostalAddress',
+					'addressLocality' => $book_information['pb_publisher_city'],
+				];
+			}
+		}
+
+		if ( isset( $book_information['pb_publication_date'] ) ) {
+			$new_book_information['datePublished'] = strftime( '%F', $book_information['pb_publication_date'] );
+		}
+
+		if ( isset( $book_information['pb_copyright_holder'] ) ) { // TODO: Person or Organization?
+			$new_book_information['copyrightHolder'] = [
+				'@type' => 'Organization',
+				'name' => $book_information['pb_copyright_holder'],
+			];
 		}
 
 		return $new_book_information;
