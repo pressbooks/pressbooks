3a0135fd5305a53dc8652dd35aaf6764c939244e
Dac Chartrand <dac.chartrand@gmail.com>
2017-06-19 20:49:19 -0400

    Improve search, timeout if too long, add next link in headers.

--- inc/api/endpoints/controller/class-books.php
+++ inc/api/endpoints/controller/class-books.php
@@ -14,9 +14,19 @@ class Books extends \WP_REST_Controller {
 	const LIMIT = 10;
 
 	/**
+	 * Maximum amount of time for search, in seconds
+	 */
+	const MAX_SEARCH_TIME = 15;
+
+	/**
 	 * @var int
 	 */
 	protected $totalBooks = 0;
+
+	/**
+	 * @var int
+	 */
+	protected $lastFoundBookId = 0;
 
 	/**
 	 * @var Toc
@@ -129,6 +139,13 @@ class Books extends \WP_REST_Controller {
 		$params['context']['default'] = 'view';
 		$params['per_page']['maximum'] = self::LIMIT;
 
+		$params['next'] = [
+			'description' => __( 'ID offset, overrides page.', 'pressbooks' ),
+			'type' => 'integer',
+			'sanitize_callback' => 'absint',
+			'validate_callback' => 'rest_validate_request_arg',
+		];
+
 		return $params;
 	}
 
@@ -173,24 +190,18 @@ class Books extends \WP_REST_Controller {
 	 */
 	public function get_items( $request ) {
 
-		// Search
-		$s = ! empty( $request['search'] ) ? $this->searchArgs( $request['search'] ) : [];
-
 		// Register missing routes
 		$this->registerRouteDependencies();
 
-		$results = [];
-		$book_ids = $this->bookIds( $request );
-		foreach ( $book_ids as $id ) {
-			if ( ! empty( $node = $this->renderNode( $id, $s ) ) ) {
-				$response = rest_ensure_response( $node );
-				$response->add_links( $this->linkCollector );
-				$results[] = $this->prepare_response_for_collection( $response );
-				$this->linkCollector = []; // re-initialize
-			}
+		if ( ! empty( $request['search'] ) ) {
+			$s = $this->searchArgs( $request['search'] );
+			$m = []; // TODO: https://codex.wordpress.org/Class_Reference/WP_Meta_Query
+			$response = rest_ensure_response( $this->searchBooks( $request, $s, $m, (int) $request['per_page'] ) );
+			$this->addNextSearchLinks( $request, $response );
+		} else {
+			$response = rest_ensure_response( $this->listBooks( $request ) );
+			$this->addPreviousNextLinks( $request, $response );
 		}
-		$response = rest_ensure_response( $results );
-		$this->addPreviousNextLinks( $request, $response );
 
 		return $response;
 	}
@@ -222,30 +233,7 @@ class Books extends \WP_REST_Controller {
 	}
 
 	/**
-	 * Count all books, return a paginated subset of book ids
-	 *
-	 * @param \WP_REST_Request
-	 *
-	 * @return array blog ids
-	 */
-	protected function bookIds( $request ) {
-
-		global $wpdb;
-
-		$limit = ! empty( $request['per_page'] ) ? $request['per_page'] : self::LIMIT;
-		$offset = ! empty( $request['page'] ) ? ( $request['page'] - 1 ) * $limit : 0;
-
-		$blogs = $wpdb->get_col(
-			$wpdb->prepare( "SELECT SQL_CALC_FOUND_ROWS blog_id FROM {$wpdb->blogs} WHERE public = 1 AND archived = 0 AND spam = 0 AND deleted = 0 AND blog_id != %d LIMIT %d, %d ", get_network()->site_id, $offset, $limit )
-		);
-
-		$this->totalBooks = $wpdb->get_var( 'SELECT FOUND_ROWS()' );
-
-		return $blogs;
-	}
-
-	/**
-	 * Args for WP_Query
+	 * Args for \WP_Query
 	 *
 	 * @see https://codex.wordpress.org/Class_Reference/WP_Query
 	 *
@@ -268,11 +256,12 @@ class Books extends \WP_REST_Controller {
 	 * Render an item node for use in JSON response
 	 *
 	 * @param int $id
-	 * @param array $s(optional) args for WP_Query
+	 * @param array $s (optional) args for \WP_Query
+	 * @param array $m (optional) args for \WP_Meta_Query
 	 *
 	 * @return array
 	 */
-	protected function renderNode( $id, $s = [] ) {
+	protected function renderNode( $id, $s = [], $m = [] ) {
 
 		switch_to_blog( $id );
 
@@ -307,6 +296,55 @@ class Books extends \WP_REST_Controller {
 		return $item;
 	}
 
+	// -------------------------------------------------------------------------------------------------------------------
+	// List Books
+	// -------------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * @param \WP_REST_Request
+	 *
+	 * @return array
+	 */
+	protected function listBooks( $request ) {
+		$results = [];
+		$book_ids = $this->listBookIds( $request );
+		foreach ( $book_ids as $id ) {
+			$response = rest_ensure_response( $this->renderNode( $id ) );
+			$response->add_links( $this->linkCollector );
+			$results[] = $this->prepare_response_for_collection( $response );
+			$this->linkCollector = []; // re-initialize
+			$this->lastFoundBookId = $id;
+		}
+		return $results;
+	}
+
+	/**
+	 * Count all books, update $this->>totalBooks, return a paginated subset of book ids
+	 *
+	 * @param \WP_REST_Request
+	 *
+	 * @return array blog ids
+	 */
+	protected function listBookIds( $request ) {
+
+		global $wpdb;
+
+		$limit = ! empty( $request['per_page'] ) ? $request['per_page'] : self::LIMIT;
+		$offset = ! empty( $request['page'] ) ? ( $request['page'] - 1 ) * $limit : 0;
+
+		$blogs = $wpdb->get_col(
+			$wpdb->prepare(
+				"SELECT SQL_CALC_FOUND_ROWS blog_id FROM {$wpdb->blogs} 
+				WHERE public = 1 AND archived = 0 AND spam = 0 AND deleted = 0 AND blog_id != %d 
+				ORDER BY blog_id LIMIT %d, %d ", get_network()->site_id, $offset, $limit
+			)
+		);
+
+		$this->totalBooks = $wpdb->get_var( 'SELECT FOUND_ROWS()' );
+
+		return $blogs;
+	}
+
 	/**
 	 * Add previous/next links like it's done in WP-API
 	 *
@@ -335,6 +373,105 @@ class Books extends \WP_REST_Controller {
 		if ( $max_pages > $page ) {
 			$next_page = $page + 1;
 			$next_link = add_query_arg( 'page', $next_page, $base );
+			$response->link_header( 'next', $next_link );
+		}
+	}
+
+	// -------------------------------------------------------------------------------------------------------------------
+	// Search Books
+	// -------------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * @param \WP_REST_Request
+	 * @param array $s args for \WP_Query
+	 * @param array $m args for \WP_Meta_Query
+	 * @param int $per_page
+	 *
+	 * @return array
+	 */
+	protected function searchBooks( $request, $s, $m, $per_page ) {
+
+		if ( ! empty( $request['next'] ) ) {
+			$this->lastFoundBookId = $request['next'];
+		}
+
+		$start_time = time();
+		$searched_books = 0;
+		$found_books = 0;
+		$results = [];
+		while ( $found_books < $per_page ) {
+			$book_ids = $this->searchBookIds( $request );
+			foreach ( $book_ids as $id ) {
+				$node = $this->renderNode( $id, $s, $m );
+				if ( ! empty( $node ) ) {
+					$response = rest_ensure_response( $node );
+					$response->add_links( $this->linkCollector );
+					$results[] = $this->prepare_response_for_collection( $response );
+					$this->linkCollector = []; // re-initialize
+					++$found_books;
+				}
+				++$searched_books;
+				$this->lastFoundBookId = $id;
+				if ( time() - $start_time > self::MAX_SEARCH_TIME ) {
+					break 2;
+				}
+			}
+			if ( $searched_books >= $this->totalBooks ) {
+				break;
+			}
+		}
+
+		if ( ! $searched_books ) {
+			$this->lastFoundBookId = 0;
+		}
+
+		return $results;
+	}
+
+	/**
+	 * Count all books, update $this->>totalBooks, return a paginated subset of book ids
+	 *
+	 * @param \WP_REST_Request
+	 *
+	 * @return array blog ids
+	 */
+	protected function searchBookIds( $request ) {
+
+		global $wpdb;
+
+		$limit = ! empty( $request['per_page'] ) ? $request['per_page'] : self::LIMIT;
+
+		$blogs = $wpdb->get_col(
+			$wpdb->prepare(
+				"SELECT SQL_CALC_FOUND_ROWS blog_id FROM {$wpdb->blogs} 
+				 WHERE public = 1 AND archived = 0 AND spam = 0 AND deleted = 0 AND blog_id != %d AND blog_id > %d 
+				 ORDER BY blog_id LIMIT  %d ", get_network()->site_id, $this->lastFoundBookId, $limit
+			)
+		);
+
+		$this->totalBooks = $wpdb->get_var( 'SELECT FOUND_ROWS()' );
+
+		return $blogs;
+	}
+
+	/**
+	 * Add a next link for search results
+	 *
+	 * @param \WP_REST_Request $request
+	 * @param \WP_REST_Response $response
+	 */
+	protected function addNextSearchLinks( $request, $response ) {
+
+		$max_pages = (int) ceil( $this->totalBooks / (int) $request['per_page'] );
+
+		$response->header( 'X-WP-Total', (int) $this->totalBooks );
+		$response->header( 'X-WP-TotalPages', $max_pages );
+
+		if ( $this->lastFoundBookId ) {
+			unset( $request['page'] );
+			$request_params = $request->get_query_params();
+			$base = add_query_arg( $request_params, rest_url( sprintf( '%s/%s', $this->namespace, $this->rest_base ) ) );
+			$next_link = add_query_arg( 'next', $this->lastFoundBookId, $base );
 			$response->link_header( 'next', $next_link );
 		}
 	}
