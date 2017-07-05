fb4d24c670a51397eccc46cbeef7e15947598be7
Ned Zimmerman <ned@bight.ca>
2017-07-05 10:13:54 -0300

    Restore full test for add_meta_boxes().

--- tests/test-metaboxes.php
+++ tests/test-metaboxes.php
@@ -23,12 +23,15 @@ class MetaboxesTest extends \WP_UnitTestCase {
 		global $wp_meta_boxes;
 		$c = custom_metadata_manager::instance();
 
+		update_option( 'pressbooks_show_expanded_metadata', 1 );
+
 		\Pressbooks\Admin\Metaboxes\add_meta_boxes();
 
 		$this->assertArrayHasKey( 'chapter', $wp_meta_boxes );
 		$this->assertArrayHasKey( 'part', $wp_meta_boxes );
 		$this->assertArrayHasKey( 'metadata', $c->metadata );
 		$this->assertArrayHasKey( 'general-book-information', $c->metadata['metadata'] );
+		$this->assertArrayHasKey( 'additional-catalog-information', $c->metadata['metadata'] );
 	}
 
 }
