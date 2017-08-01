a924bb3370a71a998c381bc375160397b5f732ab
Ned Zimmerman <ned@bight.ca>
2017-08-01 19:51:29 -0300

    Front end (completion?).

--- assets/src/scripts/organize.js
+++ assets/src/scripts/organize.js
@@ -1,5 +1,7 @@
 // This script is loaded when a user is on the [ Text → Organize ] page
 
+import CountUp from 'countup.js';
+
 let Pressbooks = {
 	'oldPart':        null,
 	'newPart':        null,
@@ -205,6 +207,7 @@ jQuery( document ).ready( function ( $ ) {
 	// Chapter switches
 
 	$( '.chapter_privacy' ).change( function () {
+		let post_status;
 
 		let col = $( this ).parent().prev( '.column-status' );
 		let id = $( this ).attr( 'id' );
@@ -240,6 +243,7 @@ jQuery( document ).ready( function ( $ ) {
 	} );
 
 	$( '.chapter_show_title_check' ).change( function () {
+		let chapter_show_title;
 
 		let id = $( this ).attr( 'id' );
 		id = id.split( '_' );
@@ -265,6 +269,7 @@ jQuery( document ).ready( function ( $ ) {
 	} );
 
 	$( '.chapter_export_check' ).change( function () {
+		let chapter_export;
 
 		let id = $( this ).attr( 'id' );
 		id = id.split( '_' );
@@ -295,6 +300,7 @@ jQuery( document ).ready( function ( $ ) {
 	// Front-matter switches
 
 	$( '.fm_privacy' ).change( function () {
+		let post_status;
 
 		let col = $( this ).parent().prev( '.column-status' );
 		let id = $( this ).attr( 'id' );
@@ -330,6 +336,7 @@ jQuery( document ).ready( function ( $ ) {
 	} );
 
 	$( '.fm_show_title_check' ).change( function () {
+		let chapter_show_title;
 
 		let id = $( this ).attr( 'id' );
 		id = id.split( '_' );
@@ -355,6 +362,7 @@ jQuery( document ).ready( function ( $ ) {
 	} );
 
 	$( '.fm_export_check' ).change( function () {
+		let chapter_export;
 
 		let id = $( this ).attr( 'id' );
 		id = id.split( '_' );
@@ -385,6 +393,7 @@ jQuery( document ).ready( function ( $ ) {
 	// Back-matter switches
 
 	$( '.bm_privacy' ).change( function () {
+		let post_status;
 
 		let col = $( this ).parent().prev( '.column-status' );
 		let id = $( this ).attr( 'id' );
@@ -420,6 +429,7 @@ jQuery( document ).ready( function ( $ ) {
 	} );
 
 	$( '.bm_show_title_check' ).change( function () {
+		let chapter_show_title;
 
 		let id = $( this ).attr( 'id' );
 		id = id.split( '_' );
@@ -445,6 +455,7 @@ jQuery( document ).ready( function ( $ ) {
 	} );
 
 	$( '.bm_export_check' ).change( function () {
+		let chapter_export;
 
 		let id = $( this ).attr( 'id' );
 		id = id.split( '_' );
@@ -505,7 +516,9 @@ jQuery( document ).ready( function ( $ ) {
 			'_ajax_nonce': PB_OrganizeToken.wordCountNonce,
 		};
 		$.post( ajaxurl, data, function ( response ) {
-			$( '.wc-selected-for-export' ).text( response );
+			const current_count = parseInt( $( '#wc-selected-for-export' ).text(), 10 );
+			let count_up = new CountUp( 'wc-selected-for-export', current_count, response, 0, 2.5 );
+			count_up.start();
 		} );
 	}
 
