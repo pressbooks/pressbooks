65ae1208d718170287e1d723044c9436e3ecf76e
Dac Chartrand <dac.chartrand@gmail.com>
2017-08-03 13:47:17 -0400

    Add tests

--- assets/src/scripts/export.js
+++ assets/src/scripts/export.js
@@ -5,11 +5,11 @@ import Cookies from 'js-cookie';
 jQuery( function ( $ ) {
     /* Swap out and animate the 'Export Your Book' button */
 	$( '#pb-export-button' ).click( function ( e ) {
-        e.preventDefault();
+		e.preventDefault();
 		$( '#loader' ).show();
 		$( '#pb-export-button' ).hide();
-        $( '.export-file-container' ).unbind( 'mouseenter mouseleave' ); // Disable Download & Delete Buttons
-        $( '.export-control button' ).prop('disabled', true);
+		$( '.export-file-container' ).unbind( 'mouseenter mouseleave' ); // Disable Download & Delete Buttons
+		$( '.export-control button' ).prop('disabled', true);
 		$( '#pb-export-form' ).submit();
 	} ); //
     /* Show and hide download & delete button */
--- tests/test-image.php
+++ tests/test-image.php
@@ -2,8 +2,17 @@
 
 class ImageTest extends \WP_UnitTestCase {
 
+	public function test_is_default_cover() {
+		$url = \Pressbooks\Image\default_cover_url();
+		$path = \Pressbooks\Image\default_cover_path();
+		$this->assertTrue( \Pressbooks\Image\is_default_cover( $url ) );
+		$this->assertTrue( \Pressbooks\Image\is_default_cover( $path ) );
+		$this->assertFalse( \Pressbooks\Image\is_default_cover( 'https://pressbooks.com/wp-content/uploads/2015/04/hero-image-4.png' ) );
+	}
+
 	public function test_is_valid_image() {
 		$this->assertTrue( \Pressbooks\Image\is_valid_image( __DIR__ . '/data/pb.png', 'pb.png' ) );
+		$this->assertFalse( \Pressbooks\Image\is_valid_image( 'binary', 'pb.png', true ) );
 		$this->assertFalse( \Pressbooks\Image\is_valid_image( __DIR__ . '/data/pb.png', 'pb.unknown' ) );
 		$this->assertFalse( \Pressbooks\Image\is_valid_image( __DIR__ . '/data/template.php', 'pb.png' ) );
 	}
@@ -48,5 +57,7 @@ class ImageTest extends \WP_UnitTestCase {
 		$result = \Pressbooks\Image\proper_image_extension( $file, 'pb.unknown' ); // Not an image name
 		$this->assertEquals( 'pb.unknown', $result );
 	}
+
+
 
 }
