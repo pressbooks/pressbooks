03fe7f80525044c738960c4a43212f586c64436b
Ned Zimmerman <ned@bight.ca>
2017-08-15 22:34:45 -0300

    Fix bad merge.

--- assets/src/scripts/cloner.js
+++ assets/src/scripts/cloner.js
@@ -1,15 +1,6 @@
-jQuery( function( $ ) {
-	$( '#pb-cloner-button' ).click( function( e ) {
+jQuery( function ( $ ) {
+	$( '#pb-cloner-button' ).click( function ( e ) {
 		e.preventDefault();
-<<<<<<< ours
-		$( '#pb-cloner-button' ).hide( 0, function() {
-			$( '#loader' ).show( 0, function() {
-				$( '#pb-cloner-form' ).submit();
-			} );
-		} );
-	} );
-} );
-=======
 		$( '#pb-cloner-button' ).hide();
 		$( '#loader' ).show();
 		const submission = function () {
@@ -18,4 +9,3 @@ jQuery( function( $ ) {
 		setTimeout( submission, 0 );
 	} );
 } );
->>>>>>> theirs
