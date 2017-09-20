9f47f2bfb2f67207a0e8daecbab4b2fa23ca031e
Dac Chartrand <dac.chartrand@gmail.com>
2017-09-20 13:05:26 -0400

    Only run on main site. Increase check period to 24 hours.

--- hooks-admin.php
+++ hooks-admin.php
@@ -33,12 +33,15 @@ $is_book = Book::isBook();
 // Check for updates
 // -------------------------------------------------------------------------------------------------------------------
 
-$updater = new \Puc_v4p2_Vcs_PluginUpdateChecker(
-	new \Pressbooks\Updater( 'https://github.com/pressbooks/pressbooks/' ),
-	__DIR__ . '/pressbooks.php', // Fully qualified path to the main plugin file
-	'pressbooks'
-);
-$updater->setBranch( 'master' );
+if ( ! $is_book ) {
+	$updater = new \Puc_v4p2_Vcs_PluginUpdateChecker(
+		new \Pressbooks\Updater( 'https://github.com/pressbooks/pressbooks/' ),
+		__DIR__ . '/pressbooks.php', // Fully qualified path to the main plugin file
+		'pressbooks',
+		24
+	);
+	$updater->setBranch( 'master' );
+}
 
 // -------------------------------------------------------------------------------------------------------------------
 // Look & feel of admin interface and Dashboard
