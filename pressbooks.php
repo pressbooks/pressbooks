547f56b8a9380a28853b7fc3fb6ba4e6f6f33484
Dac Chartrand <dac.chartrand@gmail.com>
2017-10-26 10:01:21 -0400

    Fix escapeshellcmd() (#974)
    * Fix escapeshellcmd() accross the board, add filter for those who need to change `en_US.UTF-8`
    
    * Fix typo.

--- pressbooks.php
+++ pressbooks.php
@@ -84,6 +84,19 @@ if ( ! defined( 'PB_ROOT_THEME' ) ) {
 	define( 'PB_ROOT_THEME', 'pressbooks-publisher' );
 }
 
+/**
+ * Set locale to UTF8 so escapeshellcmd() doesn't strip valid characters
+ *
+ * @since 4.3.5
+ * @see https://bugs.php.net/bug.php?id=54391
+ *
+ * @param string $pb_lc_ctype
+ * @return string
+ */
+$pb_lc_ctype = apply_filters( 'pb_lc_ctype', 'en_US.UTF-8' );
+setlocale( LC_CTYPE, 'UTF8', $pb_lc_ctype );
+putenv( "LC_CTYPE={$pb_lc_ctype}" );
+
 // -------------------------------------------------------------------------------------------------------------------
 // Composer autoloader (if needed)
 // -------------------------------------------------------------------------------------------------------------------
