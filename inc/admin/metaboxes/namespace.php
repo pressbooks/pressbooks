cfbf72b631821fda023944248e67c58815d96777
Ned Zimmerman <ned@bight.ca>
2017-10-31 14:45:04 -0300

    Update custom copyright explanation. [ci skip]

--- inc/admin/metaboxes/namespace.php
+++ inc/admin/metaboxes/namespace.php
@@ -329,7 +329,7 @@ function add_meta_boxes() {
 		'field_type' => 'wysiwyg',
 		'group' => 'copyright',
 		'label' => __( 'Copyright Notice', 'pressbooks' ),
-		'description' => __( 'Enter a custom copyright notice, with whatever infomation you like. This will override the auto-generated copyright notice, and be inserted after the title page.', 'pressbooks' ),
+		'description' => __( 'Enter a custom copyright notice, with whatever information you like. This will override the auto-generated copyright notice if All Rights Reserved or no license is selected, and will be inserted after the title page. If you select a Creative Commons license, the custom notice will appear after the license text in both the webbook and your exports.', 'pressbooks' ),
 		]
 	);
 
