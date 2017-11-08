3f5eca2dce75bf2d3fae474a3d8485a568b300b9
Ned Zimmerman <ned@bight.ca>
2017-11-08 10:29:16 -0500

    Allow editing of part slug.

--- inc/admin/metaboxes/namespace.php
+++ inc/admin/metaboxes/namespace.php
@@ -117,8 +117,6 @@ function add_metadata_styles( $hook ) {
 			$assets = new Assets( 'pressbooks', 'plugin' );
 			wp_enqueue_style( 'metadata', $assets->getPath( 'styles/metadata.css' ) );
 		} elseif ( 'part' === $post_type ) {
-			$assets = new Assets( 'pressbooks', 'plugin' );
-			wp_enqueue_style( 'part', $assets->getPath( 'styles/part.css' ) );
 			add_filter(
 				'page_attributes_dropdown_pages_args', function () {
 					return [ 'post_type' => '__GARBAGE__' ];
