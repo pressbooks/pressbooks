aa135958cc031922915af4612abb32a6d0044f47
Ned Zimmerman <ned@bight.ca>
2017-11-18 19:41:20 -0400

    Add test for theme migration.

--- tests/test-theme.php
+++ tests/test-theme.php
@@ -1,15 +1,25 @@
 <?php
 
-class ThemeTest extends \WP_UnitTestCase {
+class ThemeTest extends \WP_UnitTestCase
+{
+    use utilsTrait;
 
-	public function test_update_template_root() {
+    public function test_migrate_book_themes() {
+        $this->_book();
+        delete_option( 'pressbooks_theme_migration' );
+        \Pressbooks\Theme\migrate_book_themes();
+        $this->assertEquals( 1, get_option( 'pressbooks_theme_migration' ) );
+        \Pressbooks\Theme\migrate_book_themes();
+        $this->assertEquals( 2, get_option( 'pressbooks_theme_migration' ) );
+    }
 
-		$old = get_option( 'template_root' );
+    public function test_update_template_root() {
+        $old = get_option( 'template_root' );
 
-		update_option( 'template_root', '/plugins/pressbooks/themes-book' );
-		\Pressbooks\Theme\update_template_root();
-		$this->assertEquals( '/themes', get_option( 'template_root' ) );
+        update_option( 'template_root', '/plugins/pressbooks/themes-book' );
+        \Pressbooks\Theme\update_template_root();
+        $this->assertEquals( '/themes', get_option( 'template_root' ) );
 
-		update_option( 'template_root', $old ); // Put back to normal
-	}
+        update_option( 'template_root', $old ); // Put back to normal
+    }
 }
