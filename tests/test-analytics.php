e58e262d81165532598cc82aa8f859897c18b3dd
Dac Chartrand <dac.chartrand@gmail.com>
2017-12-18 16:28:48 -0500

    Code coverage.

--- tests/test-analytics.php
+++ tests/test-analytics.php
@@ -6,10 +6,66 @@ class AnalyticsTest extends \WP_UnitTestCase {
 
 	use utilsTrait;
 
-	public function test_add_menu() {
+	public function test_network_analytics_settings_init() {
+		global $wp_registered_settings;
+		\Pressbooks\Admin\Analytics\network_analytics_settings_init();
+		$this->assertArrayHasKey( 'ga_mu_uaid', $wp_registered_settings );
+		$this->assertArrayHasKey( 'ga_mu_site_specific_allowed', $wp_registered_settings );
+	}
 
+	public function test_book_analytics_settings_init() {
+		global $wp_registered_settings;
+		\Pressbooks\Admin\Analytics\book_analytics_settings_init();
+		$this->assertArrayHasKey( 'ga_mu_uaid', $wp_registered_settings );
+	}
+
+	public function test_add_menu() {
 		$this->expectOutputRegex( '/<\/p>/' );
 		\Pressbooks\Admin\Analytics\analytics_settings_section_callback();
+	}
+
+	public function test_analytics_book_callback() {
+		$args[0] = 'Hello World!';
+		ob_start();
+		\Pressbooks\Admin\Analytics\analytics_book_callback( $args );
+		$buffer = ob_get_clean();
+
+		$this->assertContains( 'ga_mu_uaid', $buffer );
+		$this->assertContains( 'Hello World!', $buffer );
+	}
+
+	public function test_analytics_network_callback() {
+		$args[0] = 'Hello World!';
+		ob_start();
+		\Pressbooks\Admin\Analytics\analytics_network_callback( $args );
+		$buffer = ob_get_clean();
+
+		$this->assertContains( 'ga_mu_uaid', $buffer );
+		$this->assertContains( 'Hello World!', $buffer );
+	}
+
+	public function test_analytics_books_allowed_callback() {
+		$args[0] = 'Hello World!';
+		ob_start();
+		\Pressbooks\Admin\Analytics\analytics_books_allowed_callback( $args );
+		$buffer = ob_get_clean();
+
+		$this->assertContains( 'ga_mu_site_specific_allowed', $buffer );
+		$this->assertContains( 'Hello World!', $buffer );
+	}
+
+	public function test_display_network_analytics_settings() {
+		ob_start();
+		\Pressbooks\Admin\Analytics\display_network_analytics_settings( );
+		$buffer = ob_get_clean();
+		$this->assertContains( '</form>', $buffer );
+	}
+
+	public function test_display_book_analytics_settings() {
+		ob_start();
+		\Pressbooks\Admin\Analytics\display_book_analytics_settings( );
+		$buffer = ob_get_clean();
+		$this->assertContains( '</form>', $buffer );
 	}
 
 	public function test_print_analytics() {
