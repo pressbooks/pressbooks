8dc713827d5864552c9ea81f44372ca833703e63
Dac Chartrand <dac.chartrand@gmail.com>
2018-01-26 15:52:41 -0500

    Batch API Endpoint

--- tests/test-api.php
+++ tests/test-api.php
@@ -126,4 +126,75 @@ class ApiTest extends \WP_UnitTestCase {
 		$this->assertTrue( is_bool( $result ) );
 	}
 
+	/**
+	 * @see \Pressbooks\Api\init_batch for documentation
+	 */
+	public function test_batch() {
+		$server = $this->_setupBookApi();
+
+		// Set admin with site wide permissions
+		$user_id = $this->factory()->user->create( [ 'role' => 'administrator' ] );
+		wp_set_current_user( $user_id );
+		update_site_option( 'site_admins', [ wp_get_current_user()->user_login ] );
+
+		// Invalid request
+
+		$request = new \WP_REST_Request( 'GET', '/pressbooks/v2/batch' );
+		$response = $server->dispatch( $request );
+		$this->assertEquals( 400, $response->get_status() );
+
+		// URL Format
+
+		$request = new \WP_REST_Request( 'GET', '/pressbooks/v2/batch' );
+		parse_str( 'requests[]=/pressbooks/v2/front-matter&requests[]=/pressbooks/v2/back-matter', $params );
+		$request->set_query_params( $params );
+		$response = $server->dispatch( $request );
+		$data = $response->get_data();
+
+		$this->assertEquals( 2, count( $data ) );
+		$this->assertInstanceOf( '\WP_REST_Response', $data[0] );
+		$this->assertInstanceOf( '\WP_REST_Response', $data[1] );
+		$this->assertEquals( 200, $data[0]->get_data()['status'] );
+		$this->assertEquals( 200, $data[1]->get_data()['status'] );
+		$this->assertEquals( 'Introduction', $data[0]->get_data()['body'][0]['title']['rendered'] );
+		$this->assertEquals( 'Appendix', $data[1]->get_data()['body'][0]['title']['rendered'] );
+
+		// JSON Object Format
+
+		$this->assertFalse( get_user_by( 'slug', 'batchuser001' ) );
+		$this->assertFalse( get_user_by( 'slug', 'batchuser002' ) );
+
+		$post = '
+			{
+				"requests": [
+					{
+						"path": "/wp/v2/users",
+						"headers": [],
+						"body": {"username": "batchuser001", "email": "batchuser001@pressbooks.test", "password": "abcd1234"},
+						"method": "POST"
+					},
+					{
+						"path": "/wp/v2/users",
+						"headers": [],
+						"body": {"username": "batchuser002", "email": "batchuser002@pressbooks.test", "password": "abcd1234"},
+						"method": "POST"
+					}
+				]
+			}';
+
+		$request = new \WP_REST_Request( 'POST', '/pressbooks/v2/batch' );
+		$request->set_body_params( json_decode( $post, true ) );
+		$response = $server->dispatch( $request );
+		$data = $response->get_data();
+
+		$this->assertEquals( 2, count( $data ) );
+		$this->assertInstanceOf( '\WP_REST_Response', $data[0] );
+		$this->assertInstanceOf( '\WP_REST_Response', $data[1] );
+		$this->assertEquals( 201, $data[0]->get_data()['status'] );
+		$this->assertEquals( 201, $data[1]->get_data()['status'] );
+
+		$this->assertInstanceOf( '\WP_User', get_user_by( 'slug', 'batchuser001' ) );
+		$this->assertInstanceOf( '\WP_User', get_user_by( 'slug', 'batchuser002' ) );
+	}
+
 }
