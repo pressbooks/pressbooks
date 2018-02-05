4c3d3fcdc069d3191cfa6367c562a0c4c3f9ad05
Dac Chartrand <dac.chartrand@gmail.com>
2018-02-05 14:09:26 -0500

    Fix JS syntax error (#1072)

--- symbionts/custom-metadata/js/custom-metadata-manager.js
+++ symbionts/custom-metadata/js/custom-metadata-manager.js
@@ -23,7 +23,7 @@
 			id_name = split_id[0] + '-' + instance_num;
 			$clone.attr( 'id', id_name );
 			$clone.children('input').removeAttr('value');
-			$clone.insertAfter( $last ).hide().fadeIn().find( ':input' ).not( 'type="button"' ).val(''); // todo: figure out if default value
+			$clone.insertAfter( $last ).hide().fadeIn().find( ':input' ).not( ':input[type=button]' ).val(''); // todo: figure out if default value
 		});
 
 		// deleting fields
