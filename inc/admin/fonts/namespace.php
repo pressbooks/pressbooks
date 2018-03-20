ca5462b2b339f9ebc6464219a835c7549f918939
Dac Chartrand <dac.chartrand@gmail.com>
2018-03-20 17:18:05 -0400

    Webbooks with (deprecated) Custom CSS theme should revert to McLuhan (#1160)
    Fixes pressbooks/pressbooks-book#113

--- inc/admin/fonts/namespace.php
+++ inc/admin/fonts/namespace.php
@@ -12,9 +12,8 @@ use Pressbooks\Container;
  * Compile Sass for everything that has to do with dynamically generated font stacks
  */
 function update_font_stacks() {
-	$stylesheet = ( pb_is_custom_theme() ) ? 'pressbooks-book' : null;
 	Container::get( 'GlobalTypography' )->updateGlobalTypographyMixin();
-	Container::get( 'Styles' )->updateWebBookStyleSheet( $stylesheet );
+	Container::get( 'Styles' )->updateWebBookStyleSheet();
 	\Pressbooks\Editor\update_editor_style();
 }
 
