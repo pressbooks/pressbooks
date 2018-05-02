a16470c9cb46ed78d1837e44f2d5fa191b37cd16
Lukas <Kaiser>
2018-05-02 10:50:12 +0200

    Problem with slash at end of Path

--- inc/utility/namespace.php
+++ inc/utility/namespace.php
@@ -43,12 +43,14 @@ function scandir_by_date( $dir ) {
 
 	$ignored = [ '.', '..', '.svn', '.git', '.htaccess' ];
 
+	$dir = rtrim( $dir, '/' ) . '/';
+
 	$files = [];
 	foreach ( scandir( $dir ) as $file ) {
 		if ( in_array( $file, $ignored, true ) || is_dir( $dir . $file ) ) {
 			continue;
 		}
-		$files[ $file ] = filemtime( $dir . '/' . $file );
+		$files[ $file ] = filemtime( $dir . $file );
 	}
 	arsort( $files );
 	$files = array_keys( $files );
