3f38165f735b801c830e5825044461f2a0036ce0
Dac Chartrand <dac.chartrand@gmail.com>
2018-06-21 17:56:03 -0400

    Removing trailing slash

--- xdebug-filter.php
+++ xdebug-filter.php
@@ -5,7 +5,7 @@ if ( function_exists( 'xdebug_set_filter' ) ) {
 	xdebug_set_filter(
 		XDEBUG_FILTER_CODE_COVERAGE,
 		XDEBUG_PATH_WHITELIST,
-		[ __DIR__ . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR ]
+		[ __DIR__ . '/inc' ]
 	);
 	echo 'Setup XDEBUG_PATH_WHITELIST...' . PHP_EOL;
 }
