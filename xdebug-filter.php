3ad891da039b4b7cca299a89592d079fa0f90aef
Dac Chartrand <dac.chartrand@gmail.com>
2018-06-21 17:26:11 -0400

    Gah

--- xdebug-filter.php
+++ xdebug-filter.php
@@ -4,6 +4,6 @@ if ( function_exists( 'xdebug_set_filter' ) ) {
 	xdebug_set_filter(
 		XDEBUG_FILTER_CODE_COVERAGE,
 		XDEBUG_PATH_WHITELIST,
-		[ __DIR__ . '/../inc/' ]
+		[ __DIR__ . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR ]
 	);
 }
