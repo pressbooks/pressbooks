453ef09fdd633756319e8dea4cc14f3dd8af4342
Dac Chartrand <dac.chartrand@gmail.com>
2018-06-21 17:43:58 -0400

    Is it even working?

--- xdebug-filter.php
+++ xdebug-filter.php
@@ -1,9 +1,12 @@
 <?php
 
+// @codingStandardsIgnoreStart
 if ( function_exists( 'xdebug_set_filter' ) ) {
 	xdebug_set_filter(
 		XDEBUG_FILTER_CODE_COVERAGE,
 		XDEBUG_PATH_WHITELIST,
 		[ __DIR__ . DIRECTORY_SEPARATOR . 'inc' . DIRECTORY_SEPARATOR ]
 	);
+	echo 'Setup XDEBUG_PATH_WHITELIST...' . PHP_EOL;
 }
+// @codingStandardsIgnoreEnd
