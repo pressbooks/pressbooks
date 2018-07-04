79eb1f860eaedc5cb777bb523e8c2cc1c79597b9
Ned Zimmerman <ned@bight.ca>
2018-07-04 01:04:44 -0300

    Fix suppression of TOC part and chapter numbers for Buckram 1.0 themes (#1249)

--- tests/test-pdfoptions.php
+++ tests/test-pdfoptions.php
@@ -1,6 +1,19 @@
 <?php
 
 class PDFOptionsTest extends \WP_UnitTestCase {
+	use utilsTrait;
+
+	public function test_scssOverrides() {
+		$this->_book( 'pressbooks-donham' );
+
+		update_option( 'pressbooks_theme_options_global', [
+			'chapter_numbers' => 0,
+		] );
+
+		$result = \Pressbooks\Modules\ThemeOptions\PDFOptions::scssOverrides( '' );
+		$this->assertContains( 'div.part-title-wrap > .part-number, div.chapter-title-wrap > .chapter-number, #toc .part a::before, #toc .chapter a::before { display: none !important; }', $result );
+
+	}
 
 	public function test_replaceRunningContentTags() {
 		$result = \Pressbooks\Modules\ThemeOptions\PDFOptions::replaceRunningContentTags( '%section_title%' );
