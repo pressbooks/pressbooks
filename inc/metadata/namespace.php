1d96afd92980e4cbb2870ac9551e31b6e162c479
Ned Zimmerman <ned@bight.ca>
2018-07-16 14:43:15 -0300

    Update Thema codes to 1.3.0, supply localized versions when appropriate (fix #1265) (#1266)
    - [x] Update Thema codes to 1.3.0
    - [x] Supply localized versions when appropriate

--- inc/metadata/namespace.php
+++ inc/metadata/namespace.php
@@ -2,12 +2,15 @@
 
 namespace Pressbooks\Metadata;
 
-use Pressbooks\Book;
-use Pressbooks\Licensing;
+use function \Pressbooks\L10n\get_book_language;
+use function \Pressbooks\L10n\get_locale;
 use function \Pressbooks\Sanitize\is_valid_timestamp;
+use function \Pressbooks\Utility\get_contents;
 use function \Pressbooks\Utility\is_assoc;
 use function \Pressbooks\Utility\oxford_comma;
 use function \Pressbooks\Utility\oxford_comma_explode;
+use Pressbooks\Book;
+use Pressbooks\Licensing;
 
 /**
  * Returns an html blob of meta elements based on what is set in 'Book Information'
@@ -759,14 +762,20 @@ function schema_to_section_information( $section_schema, $book_schema ) {
  * @since 4.4.0
  *
  * @param bool $include_qualifiers Whether or not the Theme subject qualifiers should be included.
- *
+ *.
  * @return array
  */
 function get_thema_subjects( $include_qualifiers = false ) {
-	$json = \Pressbooks\Utility\get_contents( PB_PLUGIN_DIR . 'symbionts/thema/Thema_v1.2_en.json' );
+	if ( \Pressbooks\Book::isBook() ) {
+		$locale = substr( get_book_language(), 0, 2 );
+	} else {
+		$locale = substr( get_locale(), 0, 2 );
+	}
+	$lang = ( in_array( $locale, [ 'de', 'en', 'es', 'fr', 'pt' ], true ) ) ? $locale : 'en';
+	$json = get_contents( PB_PLUGIN_DIR . "symbionts/thema/thema-${lang}.json" );
 	$values = json_decode( $json );
 	$subjects = [];
-	foreach ( $values->CodeList->Code as $code ) {
+	foreach ( $values->CodeList->ThemaCodes->Code as $code ) {
 		if ( ctype_alpha( substr( $code->CodeValue, 0, 1 ) ) || $include_qualifiers && ctype_digit( substr( $code->CodeValue, 0, 1 ) ) ) {
 			if ( strlen( $code->CodeValue ) === 1 ) {
 				$subjects[ $code->CodeValue ] = [
