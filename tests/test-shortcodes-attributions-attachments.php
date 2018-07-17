2342d5988f68ab71ade37d52c54511a78bfd1698
bdolor <bpayne@bccampus.ca>
2018-07-17 14:56:41 -0700

    add setUp and init test

--- tests/test-shortcodes-attributions-attachments.php
+++ tests/test-shortcodes-attributions-attachments.php
@@ -4,8 +4,25 @@ class Shortcodes_Attributions_Attachments extends \WP_UnitTestCase {
 
 	use utitsTrait;
 
-	public function setUp(){
+	protected $att;
 
+	public function setUp() {
+		parent::setUp();
+
+		$this->att = $this->getMockBuilder( '\Pressbooks\Shortcodes\Attribution\Attachments' )
+		                  ->setMethods( NULL )
+		                  ->disableOriginalConstructor()
+		                  ->getMock();
+	}
+
+	public function test_getInstance() {
+
+		$val = $this->att->init();
+
+		$this->assertTrue( $val instanceof \Pressbooks\Shortcodes\Attributions\Attachments );
+
+		global $shortcode_tags;
+		$this->assertArrayHasKey( 'media_attributions', $shortcode_tags );
 	}
 
 }
