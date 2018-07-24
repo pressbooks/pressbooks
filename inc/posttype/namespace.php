fd1bbe442401568e593107abc05b5d2e5ca43adb
bdolor <bpayne@bccampus.ca>
2018-07-23 19:20:57 -0700

    revert spacing for multiple statement alignment not same

--- inc/posttype/namespace.php
+++ inc/posttype/namespace.php
@@ -38,131 +38,131 @@ function list_post_types() {
 function register_post_types() {
 	register_extended_post_type(
 		'chapter', [
-			'admin_cols'            => [
+			'admin_cols' => [
 				'chapter_type' => [
 					'taxonomy' => 'chapter-type',
 				],
 			],
-			'quick_edit'            => false,
-			'capability_type'       => 'post',
-			'has_archive'           => true,
-			'hierarchical'          => false,
-			'menu_icon'             => 'dashicons-book',
-			'supports'              => [ 'title', 'editor', 'author', 'comments', 'page-attributes', 'revisions' ],
-			'show_in_menu'          => false,
-			'show_in_admin_bar'     => true,
-			'show_in_rest'          => true,
-			'rest_base'             => 'chapters',
+			'quick_edit' => false,
+			'capability_type' => 'post',
+			'has_archive' => true,
+			'hierarchical' => false,
+			'menu_icon' => 'dashicons-book',
+			'supports' => [ 'title', 'editor', 'author', 'comments', 'page-attributes', 'revisions' ],
+			'show_in_menu' => false,
+			'show_in_admin_bar' => true,
+			'show_in_rest' => true,
+			'rest_base' => 'chapters',
 			'rest_controller_class' => '\Pressbooks\Api\Endpoints\Controller\Posts',
-			'rewrite'               => [
-				'slug'       => 'chapter',
+			'rewrite' => [
+				'slug' => 'chapter',
 				'with_front' => false,
 			],
 		],
 		[
 			'singular' => __( 'Chapter', 'pressbooks' ),
-			'plural'   => __( 'Chapters', 'pressbooks' ),
+			'plural' => __( 'Chapters', 'pressbooks' ),
 		]
 	);
 	register_extended_post_type(
 		'part',
 		[
-			'quick_edit'            => false,
-			'capability_type'       => 'page',
-			'has_archive'           => true,
-			'hierarchical'          => true,
-			'supports'              => [ 'title', 'editor', 'page-attributes', 'revisions' ],
-			'show_in_menu'          => false,
-			'show_in_admin_bar'     => true,
-			'show_in_rest'          => true,
-			'rest_base'             => 'parts',
+			'quick_edit' => false,
+			'capability_type' => 'page',
+			'has_archive' => true,
+			'hierarchical' => true,
+			'supports' => [ 'title', 'editor', 'page-attributes', 'revisions' ],
+			'show_in_menu' => false,
+			'show_in_admin_bar' => true,
+			'show_in_rest' => true,
+			'rest_base' => 'parts',
 			'rest_controller_class' => '\Pressbooks\Api\Endpoints\Controller\Posts',
-			'rewrite'               => [
-				'slug'       => 'part',
+			'rewrite' => [
+				'slug' => 'part',
 				'with_front' => false,
 			],
 		],
 		[
 			'singular' => __( 'Part', 'pressbooks' ),
-			'plural'   => __( 'Parts', 'pressbooks' ),
+			'plural' => __( 'Parts', 'pressbooks' ),
 		]
 	);
 	register_extended_post_type(
 		'front-matter',
 		[
-			'admin_cols'            => [
+			'admin_cols' => [
 				'front_matter_type' => [
 					'taxonomy' => 'front-matter-type',
 				],
 			],
-			'quick_edit'            => false,
-			'capability_type'       => 'post',
-			'has_archive'           => true,
-			'hierarchical'          => false,
-			'supports'              => [ 'title', 'editor', 'author', 'comments', 'revisions', 'page-attributes' ],
-			'show_in_menu'          => false,
-			'show_in_admin_bar'     => true,
-			'show_in_rest'          => true,
-			'rest_base'             => 'front-matter',
+			'quick_edit' => false,
+			'capability_type' => 'post',
+			'has_archive' => true,
+			'hierarchical' => false,
+			'supports' => [ 'title', 'editor', 'author', 'comments', 'revisions', 'page-attributes' ],
+			'show_in_menu' => false,
+			'show_in_admin_bar' => true,
+			'show_in_rest' => true,
+			'rest_base' => 'front-matter',
 			'rest_controller_class' => '\Pressbooks\Api\Endpoints\Controller\Posts',
-			'rewrite'               => [
-				'slug'       => 'front-matter',
+			'rewrite' => [
+				'slug' => 'front-matter',
 				'with_front' => false,
 			],
 		],
 		[
 			'singular' => __( 'Front Matter', 'pressbooks' ),
-			'plural'   => __( 'Front Matter', 'pressbooks' ),
+			'plural' => __( 'Front Matter', 'pressbooks' ),
 		]
 	);
 	register_extended_post_type(
 		'back-matter',
 		[
-			'admin_cols'            => [
+			'admin_cols' => [
 				'back_matter_type' => [
 					'taxonomy' => 'back-matter-type',
 				],
 			],
-			'quick_edit'            => false,
-			'capability_type'       => 'post',
-			'has_archive'           => true,
-			'hierarchical'          => false,
-			'supports'              => [ 'title', 'editor', 'author', 'comments', 'revisions', 'page-attributes' ],
-			'show_in_menu'          => false,
-			'show_in_admin_bar'     => true,
-			'show_in_rest'          => true,
-			'rest_base'             => 'back-matter',
+			'quick_edit' => false,
+			'capability_type' => 'post',
+			'has_archive' => true,
+			'hierarchical' => false,
+			'supports' => [ 'title', 'editor', 'author', 'comments', 'revisions', 'page-attributes' ],
+			'show_in_menu' => false,
+			'show_in_admin_bar' => true,
+			'show_in_rest' => true,
+			'rest_base' => 'back-matter',
 			'rest_controller_class' => '\Pressbooks\Api\Endpoints\Controller\Posts',
-			'rewrite'               => [
-				'slug'       => 'back-matter',
+			'rewrite' => [
+				'slug' => 'back-matter',
 				'with_front' => false,
 			],
 		],
 		[
 			'singular' => __( 'Back Matter', 'pressbooks' ),
-			'plural'   => __( 'Back Matter', 'pressbooks' ),
+			'plural' => __( 'Back Matter', 'pressbooks' ),
 		]
 	);
 	register_extended_post_type(
 		'metadata',
 		[
-			'labels'              => [
+			'labels' => [
 				'menu_name' => __( 'Book Info', 'pressbooks' ),
 			],
-			'quick_edit'          => false,
-			'capability_type'     => 'post',
+			'quick_edit' => false,
+			'capability_type' => 'post',
 			'exclude_from_search' => true,
-			'hierarchical'        => false,
-			'publicly_queryable'  => false,
-			'rewrite'             => false,
-			'show_in_menu'        => false,
-			'show_in_nav_menus'   => false,
-			'supports'            => false,
-			'menu_icon'           => 'dashicons-info',
+			'hierarchical' => false,
+			'publicly_queryable' => false,
+			'rewrite' => false,
+			'show_in_menu' => false,
+			'show_in_nav_menus' => false,
+			'supports' => false,
+			'menu_icon' => 'dashicons-info',
 		],
 		[
 			'singular' => __( 'Book Information', 'pressbooks' ),
-			'plural'   => __( 'Book Information', 'pressbooks' ),
+			'plural' => __( 'Book Information', 'pressbooks' ),
 		]
 	);
 }
@@ -181,7 +181,6 @@ function comments_open( $open, $post_id ) {
 			return false;
 		}
 	}
-
 	return $open;
 }
 
@@ -195,8 +194,8 @@ function register_meta() {
 
 	$defaults = [
 		'show_in_rest' => true,
-		'single'       => true,
-		'type'         => 'string',
+		'single' => true,
+		'type' => 'string',
 	];
 
 	\register_meta(
@@ -240,7 +239,7 @@ function register_meta() {
 	\register_meta(
 		'post', 'pb_authors', array_merge(
 			$defaults, [
-				'single'      => false,
+				'single' => false,
 				'description' => __( 'Chapter Author (appears in Web/ebook/PDF output)', 'pressbooks' ),
 			]
 		)
@@ -333,7 +332,7 @@ function register_post_statii() {
  */
 function add_post_types_rss( $args ) {
 	$blog_public = get_option( 'blog_public' );
-	$num_posts   = get_option( 'posts_per_rss' );
+	$num_posts = get_option( 'posts_per_rss' );
 
 	// only if book is public
 	if ( 1 === absint( $blog_public ) ) {
@@ -349,7 +348,6 @@ function add_post_types_rss( $args ) {
 			$args['post_type'] = [ 'post' ];
 		}
 	}
-
 	return $args;
 }
 
@@ -363,10 +361,10 @@ function add_post_types_rss( $args ) {
  */
 function add_posttypes_to_hypothesis( $posttypes ) {
 	$posttypes = [
-		'part'         => __( 'parts', 'pressbooks' ),
-		'chapter'      => __( 'chapters', 'pressbooks' ),
+		'part' => __( 'parts', 'pressbooks' ),
+		'chapter' => __( 'chapters', 'pressbooks' ),
 		'front-matter' => __( 'front matter', 'pressbooks' ),
-		'back-matter'  => __( 'back matter', 'pressbooks' ),
+		'back-matter' => __( 'back matter', 'pressbooks' ),
 	];
 
 	return $posttypes;
@@ -431,6 +429,5 @@ function get_post_type_label( $posttype ) {
 		default:
 			$label = false;
 	endswitch;
-
 	return $label;
 }
