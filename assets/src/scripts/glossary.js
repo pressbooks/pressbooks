54ccb44b98eb58c67753cc2d6c368b1a38d3ab85
alex-418 <aparedes@bccampus.ca>
2018-08-01 17:05:38 -0700

    searches for and adds id to shortcode for the highlighted term

--- assets/src/scripts/glossary.js
+++ assets/src/scripts/glossary.js
@@ -1,38 +1,57 @@
 ( function () {
-	tinymce.create( 'tinymce.plugins.glossary', {
-		init: function ( ed, url ) {
-			ed.addButton( 'glossary', {
-				title: PB_GlossaryToken.glossary_title,
-				text: 'GL',
-				icon: false,
-				onclick: function () {
-					let mySelection = ed.selection.getContent();
-					let glossaryTerm;
-					if ( mySelection !== '' ) {
+    tinymce.create( 'tinymce.plugins.glossary', {
+        init: function ( ed, url ) {
+            ed.addButton( 'glossary', {
+                title: PB_GlossaryToken.glossary_title,
+                text: 'GL',
+                icon: false,
+                onclick: function () {
+                    // make sure we clea
+                    let json_str = PB_GlossaryToken.glossary_terms.replace(/&quot;/g, '"');
+                    let terms = jQuery.parseJSON(json_str);
+                    let mySelection = ed.selection.getContent();
+                    let glossaryTerm;
+
+                    // if there was a highlighted selection, check for it's existence and get the value of the ID
+                    if (mySelection !== '') {
+                        // get the keys
+                        let myKeys = Object.keys(terms);
+                        // check if mySelection exists
+                        let matchingKeys = myKeys.filter(function (key) {
+                            return key.indexOf(mySelection) !== -1
+                        });
+                        // get the value of the id for the match
+                        let matchingID = matchingKeys.map(function (key) {
+                            return terms[key]['id']
+                        });
+                        // display the UI to lookup and select an existing term
                         glossaryTerm = prompt(
                             'Glossary Content',
                             mySelection
                         );
-						ed.selection.setContent( '[pb_glossary]' + glossaryTerm + '[/pb_glossary]' );
-					} else {
-						glossaryTerm = prompt(
-							'Glossary Content',
-							'Enter your glossary content here.'
-						);
-						if ( glossaryTerm !== '' ) {
-							ed.execCommand(
-								'mceInsertContent',
-								false,
-								'[pb_glossary]' + glossaryTerm + '[/pb_glossary]'
-							);
-						}
-					}
-				},
-			} );
-		},
-		createControl: function ( n, cm ) {
-			return null;
-		},
-	} );
-	tinymce.PluginManager.add( 'glossary', tinymce.plugins.glossary );
+                        // insert the short-code with the id as an attribute
+                        ed.selection.setContent('[pb_glossary' + ' id="' + matchingID[0] + '"]' + glossaryTerm + '[/pb_glossary]');
+
+                        // if there was no highlighted selection, display the UI to lookup and select an existing term
+                    } else {
+                        glossaryTerm = prompt(
+                            'Glossary Content',
+                            'Enter your glossary content here.'
+                        );
+                        if ( glossaryTerm !== '' ) {
+                            ed.execCommand(
+                                'mceInsertContent',
+                                false,
+                                '[pb_glossary]' + glossaryTerm + '[/pb_glossary]'
+                            );
+                        }
+                    }
+                },
+            } );
+        },
+        createControl: function ( n, cm ) {
+            return null;
+        },
+    } );
+    tinymce.PluginManager.add( 'glossary', tinymce.plugins.glossary );
 } )();
