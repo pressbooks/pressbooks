8fe8af8e590cb8d9b8eab9de86680ae27aa075a7
Ned Zimmerman <ned@bight.ca>
2018-08-06 17:37:03 -0300

    Bundle the dependency to avoid issues with unpkg.com

--- inc/modules/export/htmlbook/class-htmlbook.php
+++ inc/modules/export/htmlbook/class-htmlbook.php
@@ -25,6 +25,7 @@ use Pressbooks\HTMLBook\Inline\Footnote;
 use Pressbooks\HTMLBook\Validator;
 use Pressbooks\Modules\Export\Export;
 use Pressbooks\Sanitize;
+use PressbooksMix\Assets;
 
 class HTMLBook extends Export {
 
@@ -235,9 +236,11 @@ class HTMLBook extends Export {
 
 		if ( is_super_admin( get_current_user_id() ) || WP_DEBUG ) {
 			if ( ! empty( $_GET['debug'] ) ) {
-				$url = get_generated_content_url( '/scss-debug' ) . '/' . clean_filename( $_GET['debug'] ) . '.css';
-				echo "<link rel='stylesheet' href='$url' type='text/css' />\n";
-				echo "<script src='https://unpkg.com/pagedjs/dist/paged.polyfill.js'></script>\n";
+				$assets = new Assets( 'pressbooks', 'plugin' );
+				$css = get_generated_content_url( '/scss-debug' ) . '/' . clean_filename( $_GET['debug'] ) . '.css';
+				$js = $assets->getPath( 'scripts/paged.polyfill.js' );
+				echo "<link rel='stylesheet' href='$css' type='text/css' />\n";
+				echo "<script src='$js'></script>\n";
 			}
 		}
 
