a85d158e0fc7995d34fa4df10bfb45e2bc3a4ad2
alex-418 <aparedes@bccampus.ca>
2018-08-08 14:23:49 -0700

    fix all linting errors and warnings

--- assets/src/scripts/glossary.js
+++ assets/src/scripts/glossary.js
@@ -1,149 +1,150 @@
-(function () {
-    tinymce.create('tinymce.plugins.glossary', {
-        init: function (ed, url) {
+( function () {
 
-            // get and clean up the data
-            let json_str = PB_GlossaryToken.glossary_terms.replace(/&quot;/g, '"');
-            let terms = jQuery.parseJSON(json_str);
+	tinymce.create( 'tinymce.plugins.glossary', {
+		init: function ( ed, url ) {
 
-            // get the keys
-            let keys = Object.keys(terms);
+			// get and clean up the data
+			let json_str = PB_GlossaryToken.glossary_terms.replace( /&quot;/g, '"' );
+			let terms = jQuery.parseJSON( json_str );
 
-            // get values for the combobox
-            function getListTerms() {
-                let terms = [];
-                let termlist = {};
+			// get the keys
+			let keys = Object.keys( terms );
 
-                for (let i of keys) {
-                    termlist = {};
-                    termlist['text'] = i;
-                    termlist['value'] = i;
-                    terms.push(termlist);
-                }
+			// get values for the combobox
+			function getListTerms() {
+				let terms = [];
+				let termlist = {};
 
-                return terms;
-            }
+				for ( let i of keys ) {
+					termlist = {};
+					termlist['text'] = i;
+					termlist['value'] = i;
+					terms.push( termlist );
+				}
 
-            // compares the term to an existing key for a match, converts both to lowercase to be case insensitive
-            function termCompare(term) {
-                match = keys.filter(item => item.toLowerCase().indexOf(term.toLowerCase()) !== -1);
-                return match;
-            }
+				return terms;
+			}
 
-            // checks if the term exists, returns the value or false if not found
-            function termMatch(termname) {
+			// compares the term to an existing key for a match, converts both to lowercase to be case insensitive
+			function termCompare( term ) {
+				let match = keys.filter( item => item.toLowerCase().indexOf( term.toLowerCase() ) !== -1 );
+				return match;
+			}
 
-                matchresults = termCompare(termname);
+			// checks if the term exists, returns the value or false if not found
+			function termMatch( termname ) {
 
-                if (typeof matchresults[0] === 'undefined') {
-                    return false;
-                } else {
-                    return matchresults[0];
-                }
-            }
+				let matchresults = termCompare( termname );
 
-            // returns the ID of a term in the glossary
-            function termID(termvalue) {
+				if ( typeof matchresults[0] === 'undefined' ) {
+					return false;
+				} else {
+					return matchresults[0];
+				}
+			}
 
-                match = termCompare(termvalue);
+			// returns the ID of a term in the glossary
+			function termID( termvalue ) {
 
-                if (typeof match[0] === 'undefined') {
-                    return '';
-                } else {
-                    // get the id for the match, returns an empty array if none found
-                    matchingID = match.map(function (key) {
-                        return terms[key]['id']
-                    });
+				let matches = termCompare( termvalue );
 
-                    // check if matchingID array does not exist, is not an array, or is empty
-                    if (Array.isArray(matchingID) || matchingID.length) {
-                        return matchingID[0];
-                    }
-                }
-            }
+				if ( typeof matches[0] === 'undefined' ) {
+					return '';
+				} else {
+					// get the id for the match, returns an empty array if none found
+					let matchingID = matches.map( function ( key ) {
+						return terms[key]['id']
+					} );
 
-            // This button adds the glossary short-code that generates a list of all terms
-            ed.addButton('glossary_all', {
-                title: PB_GlossaryToken.glossary_all_title,
-                text: 'Glossary',
-                icon: false,
-                onclick: function () {
-                    ed.selection.setContent('[pb_glossary]');
-                },
-            });
+					// check if matchingID array does not exist, is not an array, or is empty
+					if ( Array.isArray( matchingID ) || matchingID.length ) {
+						return matchingID[0];
+					}
+				}
+			}
 
-            // This button adds the single glossary term short-code with the corresponding term id as an attribute
-            ed.addButton('glossary', {
-                title: PB_GlossaryToken.glossary_title,
-                text: 'GL',
-                icon: false,
-                onclick: function () {
-                    // get the highlighted selection
-                    let mySelection = ed.selection.getContent();
-                    // placeholder for our default listbox value
-                    let listValue = '';
-                    // placeholder for our term doesn't exist message
-                    let termExists = '';
+			// This button adds the glossary short-code that generates a list of all terms
+			ed.addButton( 'glossary_all', {
+				title: PB_GlossaryToken.glossary_all_title,
+				text: 'Glossary',
+				icon: false,
+				onclick: function () {
+					ed.selection.setContent( '[pb_glossary]' );
+				},
+			} );
 
-                    // if the selection matches an existing term, let's set it so we can use it as our default listbox value
-                    if (termMatch(mySelection) !== false) {
-                        listValue = termMatch(mySelection);
-                    } else {
-                        termExists = 'Glossary term <b>"' + mySelection + '"</b> not found.<br />Please create it, or select a term from the list below:';
-                    }
+			// This button adds the single glossary term short-code with the corresponding term id as an attribute
+			ed.addButton( 'glossary', {
+				title: PB_GlossaryToken.glossary_title,
+				text: 'GL',
+				icon: false,
+				onclick: function () {
+					// get the highlighted selection
+					let mySelection = ed.selection.getContent();
+					// placeholder for our default listbox value
+					let listValue = '';
+					// placeholder for our term doesn't exist message
+					let termExists = '';
 
-                    // display the UI
-                    tinymce.activeEditor.windowManager.open({
+					// if the selection matches an existing term, let's set it so we can use it as our default listbox value
+					if ( termMatch( mySelection ) !== false ) {
+						listValue = termMatch( mySelection );
+					} else {
+						termExists = 'Glossary term <b>"' + mySelection + '"</b> not found.<br />Please create it, or select a term from the list below:';
+					}
 
-                        title: 'Glossary terms',
-                        width: 500,
-                        height: 100,
+					// display the UI
+					tinymce.activeEditor.windowManager.open( {
 
-                        buttons: [{
-                            text: 'Insert',
-                            subtype: 'primary',
-                            onclick: 'submit',
-                        },
-                            {
-                                text: 'Close',
-                                onclick: 'close'
-                            }
-                        ],
+						title: 'Glossary terms',
+						width: 500,
+						height: 100,
 
-                        body: [
-                            {
-                                type: 'container',
-                                name: 'container',
-                                html: termExists
-                            },
-                            {
-                                type: 'listbox',
-                                name: 'terms',
-                                label: 'Select a Term',
-                                values: getListTerms(),
-                                value: listValue
-                            },
-                        ],
-                        // insert the short-code with the associated term ID
-                        onsubmit: function (e) {
-                            // if term exists, replace their selection with the short-code
-                            if (termMatch(mySelection) !== false) {
-                                ed.selection.setContent('[pb_glossary' + ' id="' + termID(e.data.terms) + '"]' + e.data.terms + '[/pb_glossary]');
-                            } else {
-                                // prepend the short-code with their selection to avoid over-writing it
-                                ed.selection.setContent(mySelection + ' [pb_glossary' + ' id="' + termID(e.data.terms) + '"]' + e.data.terms + '[/pb_glossary]');
-                            }
-                        },
-                    },);
+						buttons: [ {
+							text: 'Insert',
+							subtype: 'primary',
+							onclick: 'submit',
+						},
+						{
+							text: 'Close',
+							onclick: 'close',
+						},
+						],
 
-                    // insert the short-code with the id as an attribute
-                },
-            });
-        },
-        createControl: function (n, cm) {
-            return null;
-        },
-    });
-    tinymce.PluginManager.add('glossary_all', tinymce.plugins.glossary.all);
-    tinymce.PluginManager.add('glossary', tinymce.plugins.glossary);
-})();
+						body: [
+							{
+								type: 'container',
+								name: 'container',
+								html: termExists,
+							},
+							{
+								type: 'listbox',
+								name: 'terms',
+								label: 'Select a Term',
+								values: getListTerms(),
+								value: listValue,
+							},
+						],
+						// insert the short-code with the associated term ID
+						onsubmit: function ( e ) {
+							// if term exists, replace their selection with the short-code
+							if ( termMatch( mySelection ) !== false ) {
+								ed.selection.setContent( '[pb_glossary id="' + termID( e.data.terms ) + '"]' + e.data.terms + '[/pb_glossary]' );
+							} else {
+								// prepend the short-code with their selection to avoid over-writing it
+								ed.selection.setContent( mySelection + ' [pb_glossary id="' + termID( e.data.terms ) + '"]' + e.data.terms + '[/pb_glossary]' );
+							}
+						},
+					}, );
+
+					// insert the short-code with the id as an attribute
+				},
+			} );
+		},
+		createControl: function ( n, cm ) {
+			return null;
+		},
+	} );
+	tinymce.PluginManager.add( 'glossary_all', tinymce.plugins.glossary.all );
+	tinymce.PluginManager.add( 'glossary', tinymce.plugins.glossary );
+} )();
