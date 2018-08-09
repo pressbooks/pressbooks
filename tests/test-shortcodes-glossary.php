0d8b80811e89ae89973d5e83d7f7c34441a642d4
bdolor <bpayne@bccampus.ca>
2018-08-09 10:17:54 -0700

    add more glossary tests, ensure alphabetical display of gl terms

--- tests/test-shortcodes-glossary.php
+++ tests/test-shortcodes-glossary.php
@@ -23,14 +23,14 @@ class Shortcodes_Glossary extends \WP_UnitTestCase {
 	private function _createGlossaryTerms() {
 		$args1 = [
 			'post_type'    => 'glossary',
-			'post_title'   => 'Neural Network',
-			'post_content' => 'A computer system modeled on the human brain and nervous system',
+			'post_title'   => 'Support Vector Machine',
+			'post_content' => 'An <i>algorithm</i> that uses a nonlinear mapping to transform the original training data into a higher dimension',
 		];
 
 		$args2 = [
 			'post_type'    => 'glossary',
-			'post_title'   => 'Support Vector Machine',
-			'post_content' => 'An algorithm that uses a nonlinear mapping to transform the original training data into a higher dimension',
+			'post_title'   => 'Neural Network',
+			'post_content' => 'A computer system modeled on the <b>human brain</b> and <a href="https://en.wikipedia.org/wiki/Nervous_system" target="_blank">nervous system</a>.',
 		];
 
 		$this->factory()->post->create( $args1 );
@@ -52,7 +52,33 @@ class Shortcodes_Glossary extends \WP_UnitTestCase {
 	public function test_glossaryTerms() {
 		$dl = $this->gl->glossaryTerms();
 
-		$this->assertEquals( '<section data-type="glossary"><header><h2>Glossary Terms</h2></header><dl data-type="glossary"><dt data-type="glossterm"><dfn id="dfn-neural-network">Neural Network</dfn></dt><dd data-type="glossdef">A computer system modeled on the human brain and nervous system</dd><dt data-type="glossterm"><dfn id="dfn-support-vector-machine">Support Vector Machine</dfn></dt><dd data-type="glossdef">An algorithm that uses a nonlinear mapping to transform the original training data into a higher dimension</dd></dl></section>', $dl );
+		// assures alphabetical listing and format
+		$this->assertEquals( '<section data-type="glossary"><header><h2>Glossary Terms</h2></header><dl data-type="glossary"><dt data-type="glossterm"><dfn id="dfn-neural-network">Neural Network</dfn></dt><dd data-type="glossdef">A computer system modeled on the <b>human brain</b> and <a href="https://en.wikipedia.org/wiki/Nervous_system" target="_blank">nervous system</a>.</dd><dt data-type="glossterm"><dfn id="dfn-support-vector-machine">Support Vector Machine</dfn></dt><dd data-type="glossdef">An <i>algorithm</i> that uses a nonlinear mapping to transform the original training data into a higher dimension</dd></dl></section>', $dl );
+
+	}
+
+	public function test_registerGlossaryButtons() {
+		$args = [ 'bold', 'italics', 'underline' ];
+
+		$buttons = $this->gl->registerGlossaryButtons( $args );
+
+		$this->assertEquals( [
+			'bold',
+			'italics',
+			'underline',
+			'glossary',
+			'glossary_all',
+		], $buttons );
+	}
+
+	public function test_addGlossaryPlugin() {
+		$plugin_array    = [ 'footnotes' => 'http://example.org/wp-content/plugins/pressbooks/assets/src/scripts/footnote.js' ];
+		$glossary_plugin = $this->gl->addGlossaryPlugin( $plugin_array );
+		$result          = [
+			'footnotes' => 'http://example.org/wp-content/plugins/pressbooks/assets/src/scripts/footnote.js',
+			'glossary'  => 'http://example.org/wp-content/plugins/pressbooks/assets/src/scripts/glossary.js',
+		];
+		$this->assertEquals( $result, $glossary_plugin );
 
 	}
 
