e78cf2beb86a3048a2c3a6826a2eb2fcea4cb59c
Dac Chartrand <dac.chartrand@gmail.com>
2018-09-17 08:54:47 -0400

    Do not update style sheets in ajax calls and cron jobs (#1355)

--- tests/utils-trait.php
+++ tests/utils-trait.php
@@ -166,15 +166,21 @@ There are many maths like it but these ones are mine.
 
 
 	/**
-	 * Fake ajax
+	 * @return int
 	 */
 	private function _fakeAjax() {
-
-		if ( ! defined( 'DOING_AJAX' ) ) {
-			define( 'DOING_AJAX', true );
-		}
+		add_filter( 'wp_doing_ajax', '__return_true' );
 		add_filter( 'wp_die_ajax_handler', '__return_false', 1, 1 ); // Override die()
-		error_reporting( error_reporting() & ~E_WARNING ); // Suppress warnings
+		return error_reporting( error_reporting() & ~E_WARNING ); // Suppress warnings
+	}
+
+	/**
+	 * @param int $old_error_reporting
+	 */
+	private function _fakeAjaxDone( $old_error_reporting ) {
+		remove_filter( 'wp_doing_ajax', '__return_true' );
+		remove_filter( 'wp_die_ajax_handler', '__return_false', 1 );
+		error_reporting( $old_error_reporting );
 	}
 
 	/**
