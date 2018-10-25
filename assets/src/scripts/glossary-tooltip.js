5b4c53cbc2545f8db8d66f9aecef06aabc539b88
Dac Chartrand <dac.chartrand@gmail.com>
2018-10-25 16:03:51 -0400

    Only one glossary tooltip at a time (fixes #1421)

--- assets/src/scripts/glossary-tooltip.js
+++ assets/src/scripts/glossary-tooltip.js
@@ -1,9 +1,9 @@
 // This script is loaded when a user is on a books web view
-
 ( function () {
 
 // Show the tooltip
 	jQuery( document ).on( 'click', '.tooltip', function () {
+		jQuery( '.tooltip.on' ).tooltip( 'close' ).removeClass( 'on' );
 		jQuery( this ).addClass( 'on' );
 		jQuery( this ).tooltip( {
 			items: '.tooltip.on',
