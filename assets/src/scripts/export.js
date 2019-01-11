af4d014e688cc01b25062300d0e939a0dfc03651
Dac Chartrand <dac.chartrand@gmail.com>
2019-01-11 15:50:53 -0500

    Refactor cookies

--- assets/src/scripts/export.js
+++ assets/src/scripts/export.js
@@ -3,8 +3,15 @@
 import Cookies from 'js-cookie';
 
 jQuery( function ( $ ) {
+
+	let cookie_key = 'pb_export';
+	let cookie_json = Cookies.getJSON( cookie_key );
+	if ( typeof cookie_json === 'undefined' ) {
+		cookie_json = {};
+	}
+
 	/* Collapsible form */
-	$( '#pb-export-hndle' ).click( function( e ) {
+	$( '#pb-export-hndle' ).click( function ( e ) {
 		let hndle = $( '#pb-export-hndle' );
 		if ( hndle.hasClass( 'dashicons-arrow-up' ) ) {
 			hndle.removeClass( 'dashicons-arrow-up' );
@@ -35,10 +42,8 @@ jQuery( function ( $ ) {
 		.find( 'input' )
 		.each( function () {
 			let name = $( this ).attr( 'name' );
-			let val = Cookies.get( 'pb_' + name );
-			let v;
 			// Defaults
-			if ( typeof val === 'undefined' ) {
+			if ( jQuery.isEmptyObject( cookie_json ) ) {
 				// Defaults
 				if (
 					name === 'export_formats[pdf]' ||
@@ -51,20 +56,25 @@ jQuery( function ( $ ) {
 					$( this ).prop( 'checked', false );
 				}
 			} else {
-				// Toggle based on user's cookie
-				if ( typeof val === 'boolean' ) {
-					v = val;
-				} else {
-					v = val === 'true';
+				let val = 0;
+				if ( cookie_json.hasOwnProperty( name ) ) {
+					val = cookie_json[ name ];
 				}
-				$( this ).prop( 'checked', v );
+				$( this ).prop( 'checked', !! val );
 			}
 			if ( $( this ).attr( 'disabled' ) ) {
 				$( this ).prop( 'checked', false );
 			}
 		} )
 		.change( function () {
-			Cookies.set( 'pb_' + $( this ).attr( 'name' ), $( this ).prop( 'checked' ), {
+			let my_json_key = $( this ).attr( 'name' );
+			let my_json_value = $( this ).prop( 'checked' );
+			if ( my_json_value ) {
+				cookie_json[ my_json_key ] = 1;
+			} else {
+				delete cookie_json[ my_json_key ];
+			}
+			Cookies.set( cookie_key, cookie_json, {
 				path: '/',
 				expires: 365,
 			} );
