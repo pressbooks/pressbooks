pressbooks/pressbooks

commit 5955b26c8da9a471945c390be432994b6e5da01f

Move tinymce code (fixes #1376) (#1385)

hooks-admin.php
@@ -170,6 +170,17 @@ if ( $is_book ) {
 	add_action( 'save_post', '\Pressbooks\Book::deleteBookObjectCache', 1000 );
 	add_action( 'wp_trash_post', '\Pressbooks\Book::deletePost' );
 	add_action( 'wp_trash_post', '\Pressbooks\Book::deleteBookObjectCache', 1000 );
+	add_action( 'edit_form_after_title', '\Pressbooks\Metadata\add_expanded_metadata_box' );
+	add_action( 'add_meta_boxes', '\Pressbooks\Admin\Metaboxes\replace_authordiv' );
+	add_filter( 'attachment_fields_to_edit', '\Pressbooks\Admin\Attachments\add_metadata_attachment', 10, 2 );
+	add_filter( 'attachment_fields_to_save', '\Pressbooks\Admin\Attachments\save_metadata_attachment', 10, 2 );
+}
+
+// -------------------------------------------------------------------------------------------------------------------
+// Tinymce
+// -------------------------------------------------------------------------------------------------------------------
+
+if ( $is_book ) {
 	add_filter( 'mce_external_languages', '\Pressbooks\Editor\add_languages' );
 	add_filter( 'tiny_mce_before_init', '\Pressbooks\Editor\mce_before_init_insert_formats' );
 	add_filter( 'tiny_mce_before_init', '\Pressbooks\Editor\mce_valid_word_elements' );
@@ -180,10 +191,8 @@ if ( $is_book ) {
 	add_filter( 'mce_buttons_3', '\Pressbooks\Editor\mce_buttons_3', 11 );
 	add_filter( 'wp_link_query_args', '\Pressbooks\Editor\customize_wp_link_query_args' );
 	add_filter( 'wp_link_query', '\Pressbooks\Editor\add_anchors_to_wp_link_query', 1, 2 );
-	add_action( 'edit_form_after_title', '\Pressbooks\Metadata\add_expanded_metadata_box' );
-	add_action( 'add_meta_boxes', '\Pressbooks\Admin\Metaboxes\replace_authordiv' );
-	add_filter( 'attachment_fields_to_edit', '\Pressbooks\Admin\Attachments\add_metadata_attachment', 10, 2 );
-	add_filter( 'attachment_fields_to_save', '\Pressbooks\Admin\Attachments\save_metadata_attachment', 10, 2 );
+	add_action( 'admin_enqueue_scripts', '\Pressbooks\Editor\admin_enqueue_scripts' );
+	add_action( 'admin_init', '\Pressbooks\Editor\add_editor_style' );
 }
 
 // -------------------------------------------------------------------------------------------------------------------
@@ -229,7 +238,6 @@ if ( $is_book ) {
 
 	// Init
 	add_action( 'admin_init', '\Pressbooks\Admin\Fonts\fix_missing_font_stacks' );
-	add_action( 'admin_init', '\Pressbooks\Editor\add_editor_style' );
 
 	// Overrides
 	add_filter( 'pb_epub_css_override', [ '\Pressbooks\Modules\ThemeOptions\EbookOptions', 'scssOverrides' ] );
@@ -279,15 +287,6 @@ if ( $is_book ) {
 	add_action(
 		'admin_init', function () {
 			remove_action( 'admin_enqueue_scripts', [ 'WP_Internal_Pointers', 'enqueue_scripts' ] );
-		}
-	);
-
-	// Fix for "are you sure you want to leave page" message when editing a part
-	add_action(
-		'admin_enqueue_scripts', function () {
-			if ( 'part' === get_post_type() ) {
-				wp_dequeue_script( 'autosave' );
-			}
 		}
 	);
 

assets/dist/scripts/glossary-tooltip.js
@@ -1,0 +1,1 @@
+!function(t){var o={};function e(n){if(o[n])return o[n].exports;var r=o[n]={i:n,l:!1,exports:{}};return t[n].call(r.exports,r,r.exports,e),r.l=!0,r.exports}e.m=t,e.c=o,e.d=function(t,o,n){e.o(t,o)||Object.defineProperty(t,o,{configurable:!1,enumerable:!0,get:n})},e.n=function(t){var o=t&&t.__esModule?function(){return t.default}:function(){return t};return e.d(o,"a",o),o},e.o=function(t,o){return Object.prototype.hasOwnProperty.call(t,o)},e.p="",e(e.s=24)}({24:function(t,o,e){t.exports=e("rFAV")},rFAV:function(t,o){jQuery(document).on("click",".tooltip",function(){jQuery(this).addClass("on"),jQuery(this).tooltip({items:".tooltip.on",show:!1,hide:!1,position:{my:"center bottom",at:"center top"}}),jQuery(this).trigger("mouseenter")}),jQuery(document).on("click",".tooltip.on",function(){jQuery(this).tooltip("close"),jQuery(this).removeClass("on")}),jQuery(".tooltip").on("mouseout",function(t){t.stopImmediatePropagation()})}});

assets/dist/scripts/tooltip.js
@@ -1,1 +1,0 @@
-!function(t){var o={};function e(n){if(o[n])return o[n].exports;var r=o[n]={i:n,l:!1,exports:{}};return t[n].call(r.exports,r,r.exports,e),r.l=!0,r.exports}e.m=t,e.c=o,e.d=function(t,o,n){e.o(t,o)||Object.defineProperty(t,o,{configurable:!1,enumerable:!0,get:n})},e.n=function(t){var o=t&&t.__esModule?function(){return t.default}:function(){return t};return e.d(o,"a",o),o},e.o=function(t,o){return Object.prototype.hasOwnProperty.call(t,o)},e.p="",e(e.s=24)}({24:function(t,o,e){t.exports=e("NpAV")},NpAV:function(t,o){jQuery(document).on("click",".tooltip",function(){jQuery(this).addClass("on"),jQuery(this).tooltip({items:".tooltip.on",show:!1,hide:!1,position:{my:"center bottom",at:"center top"}}),jQuery(this).trigger("mouseenter")}),jQuery(document).on("click",".tooltip.on",function(){jQuery(this).tooltip("close"),jQuery(this).removeClass("on")}),jQuery(".tooltip").on("mouseout",function(t){t.stopImmediatePropagation()})}});

inc/editor/namespace.php
@@ -10,6 +10,7 @@ use function Pressbooks\Sanitize\normalize_css_urls;
 use PressbooksMix\Assets;
 use Pressbooks\Container;
 use Pressbooks\HtmlParser;
+use Pressbooks\Shortcodes\Glossary\Glossary;
 
 /**
  * Ensure that Word formatting that we like doesn't get filtered out.
@@ -27,6 +28,10 @@ function mce_valid_word_elements( $init_array ) {
 
 /**
  * Localize TinyMCE plugins.
+ *
+ * @param array $array
+ *
+ * @return array
  */
 function add_languages( $array ) {
 	$array[] = PB_PLUGIN_DIR . 'languages/tinymce.php';
@@ -35,28 +40,82 @@ function add_languages( $array ) {
 
 /**
  * Adds style select dropdown, textbox and background color buttons to the MCE buttons array.
+ *
+ * @param array $buttons
+ *
+ * @return array
  */
 function mce_buttons_2( $buttons ) {
-
-	array_splice( $buttons, 0, 0, 'styleselect' );
-	$p = array_search( 'styleselect', $buttons, true );
-	array_splice( $buttons, $p + 1, 0, 'textboxes' );
-	$p = array_search( 'textboxes', $buttons, true );
-	array_splice( $buttons, $p + 1, 0, 'underline' );
+	// Prepend elements to the beginning of array
+	array_unshift( $buttons, 'styleselect', 'textboxes', 'underline' );
+	// Insert after hr button
 	$p = array_search( 'hr', $buttons, true );
 	array_splice( $buttons, $p + 1, 0, 'alignjustify' );
+	// Insert after forecolor button
 	$p = array_search( 'forecolor', $buttons, true );
 	array_splice( $buttons, $p + 1, 0, 'backcolor' );
+
 	return $buttons;
 }
 
 /**
  * Adds anchor, superscript and subscript buttons to the MCE buttons array.
+ *
+ * @param array $buttons
+ *
+ * @return array
  */
 function mce_buttons_3( $buttons ) {
+	// Prepend element to the beginning of array
 	array_unshift( $buttons, 'table' );
+	// Push elements onto the end of array
 	array_push( $buttons, 'apply_class', 'anchor', 'superscript', 'subscript' );
+	// Footnotes
+	array_push( $buttons, 'footnote', 'ftnref_convert' );
+	// Glossary
+	// to avoid 'inception' like glossary within a glossary, restricting
+	// glossary buttons means less chance of needing to untangle the labyrinth
+	global $typenow;
+	if ( empty( $typenow ) && ! empty( $_GET['post'] ) && 'edit' === $_GET['action'] ) {
+		$post = get_post( $_GET['post'] );
+		$typenow = $post->post_type;
+	} elseif ( ! empty( $_GET['post_type'] ) ) {
+		$typenow = $_GET['post_type'];
+	}
+	if ( 'glossary' !== $typenow ) {
+		array_push( $buttons, 'glossary', 'glossary_all' );
+	}
+
 	return $buttons;
+}
+
+/**
+ * @param string $hook
+ */
+function admin_enqueue_scripts( $hook ) {
+	$assets = new Assets( 'pressbooks', 'plugin' );
+
+	// Footnotes
+	wp_localize_script(
+		'editor', 'PB_FootnotesToken', [
+			'nonce' => wp_create_nonce( 'pb-footnote-convert' ),
+			'fn_title' => __( 'Insert Footnote', 'pressbooks' ),
+			'ftnref_title' => __( 'Convert MS Word Footnotes', 'pressbooks' ),
+		]
+	);
+	if ( 'post-new.php' === $hook || 'post.php' === $hook ) {
+		wp_enqueue_script( 'my_custom_quicktags', $assets->getPath( 'scripts/quicktags.js' ), [ 'quicktags' ] );
+	}
+
+	// Glossary
+	wp_localize_script(
+		'editor', 'PB_GlossaryToken', [
+			'nonce'              => wp_create_nonce( 'pb-glossary' ),
+			'glossary_title'     => __( 'Insert Glossary Term', 'pressbooks' ),
+			'glossary_all_title' => __( 'Insert Glossary List', 'pressbooks' ),
+			'glossary_terms'     => wp_json_encode( Glossary::init()->getGlossaryTerms() ),
+		]
+	);
 }
 
 
@@ -80,11 +139,20 @@ function mce_button_scripts( $plugin_array ) {
 	$plugin_array['anchor'] = $assets->getPath( 'scripts/anchor.js' );
 	$plugin_array['table'] = $assets->getPath( 'scripts/table.js' );
 
+	// Footnotes
+	$plugin_array['footnote'] = $assets->getPath( 'scripts/footnote.js' );
+	$plugin_array['ftnref_convert'] = $assets->getPath( 'scripts/ftnref-convert.js' );
+
+	// Glossary
+	$plugin_array['glossary'] = $assets->getPath( 'scripts/glossary.js' );
+
 	return $plugin_array;
 }
 
 /**
  * Adds Pressbooks custom CSS classes to the style select dropdown initiated above.
+ *
+ * @see https://codex.wordpress.org/TinyMCE_Custom_Styles#Enabling_styleselect
  *
  * @param array $init_array
  *

inc/shortcodes/footnotes/class-footnotes.php
@@ -6,8 +6,6 @@
 
 namespace Pressbooks\Shortcodes\Footnotes;
 
-use PressbooksMix\Assets;
-
 class Footnotes {
 
 	/**
@@ -53,8 +51,6 @@ class Footnotes {
 		// do_shortcode() is registered as a default filter on 'the_content' with a priority of 11.
 		// We need to run $this->footNoteContent() after this, and after attributions which is set to 12, set to 13
 		add_filter( 'the_content', [ $obj, 'footnoteContent' ], 13 );
-		add_action( 'init', [ $obj, 'footnoteButton' ] ); // TinyMCE button
-		add_action( 'admin_enqueue_scripts', [ $obj, 'myCustomQuicktags' ] ); // Quicktag button
 	}
 
 	/**
@@ -161,77 +157,6 @@ class Footnotes {
 
 		return $content;
 	}
-
-
-	/**
-	 * Register our plugin with TinyMCE
-	 */
-	function footnoteButton() {
-
-		if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) {
-			return;
-		}
-
-		if ( get_user_option( 'rich_editing' ) ) {
-
-			add_action(
-				'admin_enqueue_scripts', function () {
-					wp_localize_script(
-						'editor', 'PB_FootnotesToken', [
-							'nonce' => wp_create_nonce( 'pb-footnote-convert' ),
-							'fn_title' => __( 'Insert Footnote', 'pressbooks' ),
-							'ftnref_title' => __( 'Convert MS Word Footnotes', 'pressbooks' ),
-						]
-					);
-				}
-			);
-
-			add_filter( 'mce_external_plugins', [ $this, 'addFootnotePlugin' ] );
-			add_filter( 'mce_buttons_3', [ $this, 'registerFootnoteButtons' ] );
-		}
-
-	}
-
-
-	/**
-	 * Quicktag buttons for text mode editor
-	 */
-	function myCustomQuicktags() {
-		$assets = new Assets( 'pressbooks', 'plugin' );
-		wp_enqueue_script( 'my_custom_quicktags', $assets->getPath( 'scripts/quicktags.js' ), [ 'quicktags' ] );
-	}
-
-
-	/**
-	 * Add buttons to TinyMCE interface
-	 *
-	 * @param $buttons
-	 *
-	 * @return array
-	 */
-	function registerFootnoteButtons( $buttons ) {
-		$buttons[] = 'footnote';
-		$buttons[] = 'ftnref_convert';
-
-		return $buttons;
-	}
-
-
-	/**
-	 * Some JavaScript for our TinyMCE buttons
-	 *
-	 * @param $plugin_array
-	 *
-	 * @return mixed
-	 */
-	function addFootnotePlugin( $plugin_array ) {
-		$assets = new Assets( 'pressbooks', 'plugin' );
-		$plugin_array['footnote'] = $assets->getPath( 'scripts/footnote.js' );
-		$plugin_array['ftnref_convert'] = $assets->getPath( 'scripts/ftnref-convert.js' );
-
-		return $plugin_array;
-	}
-
 
 	/**
 	 * Echo a failure message for jQuery then die

inc/shortcodes/glossary/class-glossary.php
@@ -32,30 +32,31 @@ class Glossary {
 	}
 
 	/**
-	 * Some JavaScript for our TinyMCE buttons
-	 *
-	 * @since 5.5.0
-	 *
-	 * @param $plugin_array
-	 *
-	 * @return mixed
+	 * @param Glossary $obj
 	 */
-	function addGlossaryPlugin( $plugin_array ) {
-		$assets = new Assets( 'pressbooks', 'plugin' );
-		$plugin_array['glossary'] = $assets->getPath( 'scripts/glossary.js' );
+	static public function hooks( Glossary $obj ) {
+		add_shortcode( self::SHORTCODE, [ $obj, 'shortcodeHandler' ] );
+		add_filter(
+			'no_texturize_shortcodes',
+			function ( $excluded_shortcodes ) {
+				$excluded_shortcodes[] = Glossary::SHORTCODE;
 
-		return $plugin_array;
+				return $excluded_shortcodes;
+			}
+		);
+		add_action( 'init', [ $obj, 'addTooltipScripts' ] );
 	}
 
 	/**
 	 * Add JavaScript for the tooltip
 	 *
 	 * @since 5.5.0
-	 *
 	 */
-	function addTooltipScripts() {
-		$assets = new Assets( 'pressbooks', 'plugin' );
-		wp_enqueue_script( 'glossary-tooltip', $assets->getPath( 'scripts/tooltip.js' ), [ 'jquery-ui-tooltip' ], false, true );
+	public function addTooltipScripts() {
+		if ( ! is_admin() ) {
+			$assets = new Assets( 'pressbooks', 'plugin' );
+			wp_enqueue_script( 'glossary-tooltip', $assets->getPath( 'scripts/glossary-tooltip.js' ), [ 'jquery-ui-tooltip' ], false, true );
+		}
 	}
 
 	/**
@@ -68,7 +69,7 @@ class Glossary {
 	 *
 	 * @return array
 	 */
-	function getGlossaryTerms( $reset = false ) {
+	public function getGlossaryTerms( $reset = false ) {
 		// Cheap cache
 		static $glossary_terms = null;
 		if ( $reset || $glossary_terms === null ) {
@@ -98,57 +99,6 @@ class Glossary {
 	}
 
 	/**
-	 * Register our plugin with TinyMCE
-	 *
-	 * @since 5.5.0
-	 *
-	 */
-	function glossaryButton() {
-
-		if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) {
-			return;
-		}
-		if ( get_user_option( 'rich_editing' ) ) {
-
-			add_action(
-				'admin_enqueue_scripts', function () {
-					wp_localize_script(
-						'editor', 'PB_GlossaryToken', [
-							'nonce'              => wp_create_nonce( 'pb-glossary' ),
-							'glossary_title'     => __( 'Insert Glossary Term', 'pressbooks' ),
-							'glossary_all_title' => __( 'Insert Glossary List', 'pressbooks' ),
-							'glossary_terms'     => wp_json_encode( $this->getGlossaryTerms() ),
-						]
-					);
-				}
-			);
-
-			add_filter( 'mce_external_plugins', [ $this, 'addGlossaryPlugin' ] );
-
-			// to avoid 'inception' like glossary within a glossary, restricting
-			// glossary buttons means less chance of needing to untangle the labyrinth
-			global $typenow;
-
-			if ( empty( $typenow ) && ! empty( $_GET['post'] ) && 'edit' === $_GET['action'] ) {
-				$post = get_post( $_GET['post'] );
-				$typenow = $post->post_type;
-			} elseif ( ! empty( $_GET['post_type'] ) ) {
-				$typenow = $_GET['post_type'];
-			}
-
-			if ( 'glossary' !== $typenow ) {
-				add_filter(
-					'mce_buttons_3', [
-						$this,
-						'registerGlossaryButtons',
-					]
-				);
-			}
-		}
-
-	}
-
-	/**
 	 * Returns the HTML <dl> description list of all glossary terms
 	 *
 	 * @since 5.5.0
@@ -158,7 +108,7 @@ class Glossary {
 	 *
 	 * @return string
 	 */
-	function glossaryTerms( $type = '' ) {
+	public function glossaryTerms( $type = '' ) {
 		$output = '';
 		$glossary = '';
 		$terms = $this->getGlossaryTerms();
@@ -199,7 +149,7 @@ class Glossary {
 	 *
 	 * @return string
 	 */
-	function glossaryTooltip( $term_id, $content ) {
+	public function glossaryTooltip( $term_id, $content ) {
 
 		// get the glossary post object the ID belongs to
 		$terms = get_post( $term_id['id'] );
@@ -216,39 +166,6 @@ class Glossary {
 	}
 
 	/**
-	 * @param Glossary $obj
-	 */
-	static public function hooks( Glossary $obj ) {
-		add_shortcode( self::SHORTCODE, [ $obj, 'shortcodeHandler' ] );
-		add_filter(
-			'no_texturize_shortcodes',
-			function ( $excluded_shortcodes ) {
-				$excluded_shortcodes[] = Glossary::SHORTCODE;
-
-				return $excluded_shortcodes;
-			}
-		);
-		add_action( 'init', [ $obj, 'glossaryButton' ] ); // TinyMCE button
-		add_action( 'init', [ $obj, 'addTooltipScripts' ] );
-	}
-
-	/**
-	 * Add buttons to TinyMCE interface
-	 *
-	 * @since 5.5.0
-	 *
-	 * @param $buttons
-	 *
-	 * @return array
-	 */
-	function registerGlossaryButtons( $buttons ) {
-		$buttons[] = 'glossary';
-		$buttons[] = 'glossary_all';
-
-		return $buttons;
-	}
-
-	/**
 	 * Gets the tooltip if the param contains the post id,
 	 * or a list of terms if it's just the short-code
 	 *
@@ -259,7 +176,7 @@ class Glossary {
 	 *
 	 * @return string
 	 */
-	function shortcodeHandler( $atts, $content ) {
+	public function shortcodeHandler( $atts, $content ) {
 		$a = shortcode_atts(
 			[
 				'id' => '',

commit 9f0b0131eeb81c2f4febf1ea6fde199e1c22d3ed

Fix alert when saving parts (resolves #1555) - Reverts a regression introduced in 5955b26c8da9a471945c390be432994b6e5da01f

hooks-admin.php
@@ -305,6 +305,15 @@ if ( $is_book ) {
 		}
 	);
 
+	// Fix for "are you sure you want to leave page" message when editing a part
+	add_action(
+		'admin_enqueue_scripts', function () {
+			if ( 'part' === get_post_type() ) {
+				wp_dequeue_script( 'autosave' );
+			}
+		}
+	);
+
 	// Hide welcome screen
 	add_action(
 		'load-index.php', function () {