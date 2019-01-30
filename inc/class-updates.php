36fa670117f682f4b5d694558c1f8812e8bf8cfc
Dac Chartrand <dac.chartrand@gmail.com>
2019-01-30 15:15:58 -0500

    Fix GitHub Updater
    Cannot do a `plugins_loaded` inside another `plugins_loaded` action. Make sure factory is called only once.

--- inc/class-updates.php
+++ inc/class-updates.php
@@ -59,14 +59,17 @@ class Updates {
 	 * @see https://github.com/YahnisElsts/plugin-update-checker
 	 */
 	public function gitHubUpdater() {
-		$updater = \Puc_v4_Factory::buildUpdateChecker(
-			'https://github.com/pressbooks/pressbooks/',
-			untrailingslashit( PB_PLUGIN_DIR ) . '/pressbooks.php', // Fully qualified path to the main plugin file
-			'pressbooks',
-			24
-		);
-		$updater->setBranch( 'master' );
-		$updater->getVcsApi()->enableReleaseAssets();
+		static $updater = null;
+		if ( $updater === null ) {
+			$updater = \Puc_v4_Factory::buildUpdateChecker(
+				'https://github.com/pressbooks/pressbooks/',
+				untrailingslashit( PB_PLUGIN_DIR ) . '/pressbooks.php', // Fully qualified path to the main plugin file
+				'pressbooks',
+				24
+			);
+			$updater->setBranch( 'master' );
+			$updater->getVcsApi()->enableReleaseAssets();
+		}
 	}
 
 	/**
