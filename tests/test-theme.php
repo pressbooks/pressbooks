20cfa1f5f89f818451561e87f44d3450ff6a4c64
Ned Zimmerman <ned@bight.ca>
2019-02-21 15:38:59 -0500

    Refactor

--- tests/test-theme.php
+++ tests/test-theme.php
@@ -1,23 +1,49 @@
 <?php
 
-class ThemeTest extends \WP_UnitTestCase
-{
-    use utilsTrait;
+use function Pressbooks\Theme\update_lock_file;
 
-    public function test_migrate_book_themes() {
-        $this->_book();
-        delete_option( 'pressbooks_theme_migration' );
-        \Pressbooks\Theme\migrate_book_themes();
-        $this->assertEquals( 5, get_option( 'pressbooks_theme_migration' ) );
-    }
+class ThemeTest extends \WP_UnitTestCase {
+	use utilsTrait;
 
-    public function test_update_template_root() {
-        $old = get_option( 'template_root' );
+	/**
+	 * @var Lock
+	 */
+	protected $lock;
 
-        update_option( 'template_root', '/plugins/pressbooks/themes-book' );
-        \Pressbooks\Theme\update_template_root();
-        $this->assertEquals( '/themes', get_option( 'template_root' ) );
+	public function setUp() {
+		$this->lock = new \Pressbooks\Theme\Lock();
+	}
 
-        update_option( 'template_root', $old ); // Put back to normal
-    }
+	public function test_migrate_book_themes() {
+		$this->_book();
+		delete_option( 'pressbooks_theme_migration' );
+		\Pressbooks\Theme\migrate_book_themes();
+		$this->assertEquals( 5, get_option( 'pressbooks_theme_migration' ) );
+	}
+
+	public function test_update_template_root() {
+		$old = get_option( 'template_root' );
+
+		update_option( 'template_root', '/plugins/pressbooks/themes-book' );
+		\Pressbooks\Theme\update_template_root();
+		$this->assertEquals( '/themes', get_option( 'template_root' ) );
+
+		update_option( 'template_root', $old ); // Put back to normal
+	}
+
+	public function test_update_lock_file() {
+		$this->_book();
+		$lock = $this->lock::init();
+		$old_lock = $lock->lockTheme();
+		$this->assertArrayHasKey( 'stylesheet', $old_lock );
+		$this->assertEquals( $old_lock['stylesheet'], get_stylesheet() );
+		$new_data = [ 'stylesheet' => '' ];
+		\Pressbooks\Theme\update_lock_file( $new_data );
+		$new_lock = $lock->getLockData();
+		$this->assertEquals( $old_lock, $new_lock );
+		$new_data = [ 'stylesheet' => 'pressbooks-what' ];
+		\Pressbooks\Theme\update_lock_file( $new_data );
+		$new_lock = $lock->getLockData();
+		$this->assertEquals( 'pressbooks-what', $new_lock['stylesheet'] );
+	}
 }
