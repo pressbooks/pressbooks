96f1fcc6c8bea15cde67e6fd4cc1e56c0d6d8cb8
Dac Chartrand <dac.chartrand@gmail.com>
2019-03-21 14:11:31 -0400

    Increase code coverage

--- tests/test-modules-export-export.php
+++ tests/test-modules-export-export.php
@@ -366,6 +366,7 @@ class Modules_Export_ExportTest extends \WP_UnitTestCase {
 		( new \Pressbooks\Contributors() )->insert( 'Ned Zimmerman', $meta_post->ID );
 		$user_id = $this->factory()->user->create( [ 'role' => 'contributor' ] );
 		wp_set_current_user( $user_id );
+		update_option( 'pressbooks_theme_options_global', [ 'parse_subsections' => 1 ] );
 
 		$paths = [];
 		$xhtml_path = null;
