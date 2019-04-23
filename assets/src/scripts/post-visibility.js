d84da56f61dc3774730daf57980bdbfa7976730f
Dac Chartrand <dac.chartrand@gmail.com>
2019-04-23 16:32:57 -0400

    Fix inputs missing programmatic labels (fix #1634) (#1677)
    * Fix inputs missing programmatic labels
    * Change to selectWoo, a fork of select2 with improved accessibility.
    * Ajax load thema subject to improve speed when loading Book Info page.
    * Fix errors reported by https://validator.w3.org/nu/
    
    Remaining WAVE errors (obvious ones) come from TinyMCE and Wordpress Color Picker.  A "1 X Empty link" WAVE error comes from WordPress own code: `<a class='ab-item' href='#'><span class="ab-icon"></span><span class="screen-reader-text">Menu</span></a>` Won't fix for now.

--- assets/src/scripts/post-visibility.js
+++ assets/src/scripts/post-visibility.js
@@ -30,4 +30,10 @@ jQuery( document ).ready( function ( $ ) {
 			$( '#post_password' ).hide();
 		}
 	} );
+
+	// Accessibility fix: Chapter Type, Front Matter Type, and Back Matter Type selection menus in the sidebar are mising aria-labels
+	// https://github.com/johnbillion/extended-cpts/issues/119
+	$( '#front-matter-typedropdown' ).attr( 'aria-label', $( '#front-matter-typediv' ).find( 'h2:first' ).text() );
+	$( '#chapter-typedropdown' ).attr( 'aria-label', $( '#chapter-typediv' ).find( 'h2:first' ).text() );
+	$( '#back-matter-typedropdown' ).attr( 'aria-label', $( '#back-matter-typediv' ).find( 'h2:first' ).text() );
 } );
