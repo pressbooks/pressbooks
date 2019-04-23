fe34a87de3e065a727d8a0155078d3ebd8aac6ab
dannylonglegs <ddanfernan@gmail.com>
2019-04-23 15:45:19 -0400

    Add initial sitemap page

--- requires-admin.php
+++ requires-admin.php
@@ -9,6 +9,7 @@ require_once( __DIR__ . '/inc/admin/covergenerator/namespace.php' );
 require_once( __DIR__ . '/inc/admin/attachments/namespace.php' );
 require_once( __DIR__ . '/inc/admin/dashboard/namespace.php' );
 require_once( __DIR__ . '/inc/admin/diagnostics/namespace.php' );
+require_once( __DIR__ . '/inc/admin/sitemap/namespace.php' );
 require_once( __DIR__ . '/inc/admin/fonts/namespace.php' );
 require_once( __DIR__ . '/inc/admin/laf/namespace.php' );
 require_once( __DIR__ . '/inc/admin/metaboxes/namespace.php' );
---
+++ templates/admin/sitemap.blade.php
@@ -0,0 +1,5 @@
+{!! $notices or '' !!}
+<div class="wrap">
+	<h1>{{ __( 'Sitemap', 'pressbooks' ) }}</h1>
+	<ul>{{ $output }}</ul>
+</div>
