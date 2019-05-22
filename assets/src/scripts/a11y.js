194e0493e0cbacdb0943394e518f8111a08ef2c3
dannylonglegs <ddanfernan@gmail.com>
2019-05-22 15:33:43 -0400

    Add function to add aria labels to color picker

--- assets/src/scripts/a11y.js
+++ assets/src/scripts/a11y.js
@@ -26,4 +26,43 @@ document.addEventListener( 'DOMContentLoaded', function () {
 	addAttribute( 'div.updated', 'role', 'status' );
 	addAttribute( 'div.notice', 'role', 'status' );
 	addAttribute( 'div.error', 'role', 'alert' );
+
+	// Add aria-label attributes to color picker palette boxes
+	// https://github.com/Automattic/Iris/issues/69
+	 ( function (){
+		let colors = document.querySelectorAll( 'a.iris-palette' );
+
+		for ( let i=0; i < colors.length; i++ ) {
+			let irisPalette = colors[ i ];
+			let rgb = colors[ i ].style.backgroundColor;
+			let color = '';
+
+			if ( rgb === 'rgb(0, 0, 0)' ){
+				color = 'black'
+			}
+			if ( rgb === 'rgb(255, 255, 255)' ){
+				color = 'white'
+			}
+			if ( rgb === 'rgb(221, 51, 51)' ){
+				color = 'red'
+			}
+			if ( rgb === 'rgb(221, 153, 51)' ){
+				color = 'orange'
+			}
+			if ( rgb === 'rgb(238, 238, 34)' ){
+				color = 'yellow'
+			}
+			if ( rgb === 'rgb(129, 215, 66)' ){
+				color = 'green'
+			}
+			if ( rgb === 'rgb(30, 115, 190)' ){
+				color = 'blue'
+			}
+			if ( rgb === 'rgb(130, 36, 227)' ){
+				color = 'purple'
+			}
+
+			irisPalette.setAttribute( 'aria-label', 'Select ' + color );
+		}
+	} )();
 } );
