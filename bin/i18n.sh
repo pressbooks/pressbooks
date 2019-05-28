566a923412d42e50300b35ecc423172fe291bb82
Dac Chartrand <dac.chartrand@gmail.com>
2019-05-28 11:46:33 -0400

    Update languages, new threshold
    The minimum translation completion threshold required in order for a file to be downloaded is 25%

--- bin/i18n.sh
+++ bin/i18n.sh
@@ -2,5 +2,5 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PARENT="$(dirname $DIR)"
 tx push -s
-tx pull -a
+tx pull -a --minimum-perc=25
 for file in $PARENT/languages/*.po ; do msgfmt $file -o `echo $file | sed 's/\(.*\.\)po/\1mo/'` ; done
