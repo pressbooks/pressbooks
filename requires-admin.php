7f1123c55486d5af6b0aad1f8fb43a8d03d50e2e
Dac Chartrand <dac.chartrand@gmail.com>
2019-10-09 13:39:02 -0400

    Dependency updates (#1801)
    Update composer (and npm) dependencies.
    Update Travis build environments.

--- requires-admin.php
+++ requires-admin.php
@@ -12,6 +12,5 @@ require_once( __DIR__ . '/inc/admin/diagnostics/namespace.php' );
 require_once( __DIR__ . '/inc/admin/fonts/namespace.php' );
 require_once( __DIR__ . '/inc/admin/laf/namespace.php' );
 require_once( __DIR__ . '/inc/admin/metaboxes/namespace.php' );
-require_once( __DIR__ . '/inc/admin/networkmanagers/namespace.php' );
 require_once( __DIR__ . '/inc/admin/organize/namespace.php' );
 require_once( __DIR__ . '/inc/admin/plugins/namespace.php' );
