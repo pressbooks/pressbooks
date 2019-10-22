c920966f541ab3c58d2d36fe8d5c65926d37a619
Steel Wagstaff <steel@pressbooks.com>
2019-10-21 19:11:18 -0500

    Fix heading order on 'my catalog' page
    Fixes heading order (no longer skips from h1 to h4 and h5). Partially addresses #1775

--- inc/admin/class-catalog-list-table.php
+++ inc/admin/class-catalog-list-table.php
@@ -465,17 +465,17 @@ class Catalog_List_Table extends \WP_List_Table {
 
 			<div class="postbox">
 				<div class="inside">
-					<h4><?php _e( 'Organize your public Catalog page.', 'pressbooks' ); ?></h4>
-					<h5><span data-icon="a" class="show-hide-icon"></span><?php _e( 'Show/Hide books', 'pressbooks' ); ?></h5>
+					<h2><?php _e( 'Organize your public Catalog page.', 'pressbooks' ); ?></h2>
+					<h3><span data-icon="a" class="show-hide-icon"></span><?php _e( 'Show/Hide books', 'pressbooks' ); ?></h3>
 					<p><?php printf( __( 'To display a book in your catalog choose "%s" under Catalog Status. ', 'pressbooks' ), '<strong>' . __( 'Show in Catalog', 'pressbooks' ) . '</strong>' ); ?>
 						<br>
 						<?php printf( __( 'To hide a book in your catalog choose "%s" under Catalog Status.', 'pressbooks' ), '<strong>' . __( 'Hide in Catalog', 'pressbooks' ) . '</strong>' ); ?>
 					</p>
 
-					<h5><span data-icon="g" class="sort-icon"></span><?php _e( 'Catalog sorting', 'pressbooks' ); ?></h5>
+					<h3><span data-icon="g" class="sort-icon"></span><?php _e( 'Catalog sorting', 'pressbooks' ); ?></h3>
 					<p><?php printf( __( 'To add sorting ability, add your Tag names to your <a href="%s">Catalog Profile</a> page (ex: Authors, Book Genre), then add the appropriate tags to each individual book.', 'pressbooks' ), $edit_url ); ?></p>
 
-					<h5><span data-icon="f" class="share-icon"></span><?php _e( 'Share your catalog', 'pressbooks' ); ?></h5>
+					<h3><span data-icon="f" class="share-icon"></span><?php _e( 'Share your catalog', 'pressbooks' ); ?></h3>
 					<p><?php _e( 'The public link to your catalog page', 'pressbooks' ); ?>: <a href="<?php echo $view_url; ?>"><?php echo $view_url; ?></a></p>
 				</div>
 			</div><!-- end .postbox -->
