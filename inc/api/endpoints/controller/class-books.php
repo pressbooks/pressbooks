f12650fc6914e6de48aa445b7d6a18f6d58fe094
Ricardo Aragon <richard015ar@gmail.com>
2020-05-06 17:00:33 -0300

    pb-1797 - Avoid hardcoding fields. Reverting .gitignore

--- inc/api/endpoints/controller/class-books.php
+++ inc/api/endpoints/controller/class-books.php
@@ -237,7 +237,12 @@ class Books extends \WP_REST_Controller {
 		$metadata_info_array = $this->bookDataCollector->get( $id, BookDataCollector::BOOK_INFORMATION_ARRAY );
 
 		// https://github.com/pressbooks/pressbooks/issues/1797
-		$keys = [ 'pb_word_count', 'pb_storage_size', 'pb_h5p_activities', 'pb_in_catalog' ];
+		$keys = [
+			BookDataCollector::WORD_COUNT,
+			BookDataCollector::STORAGE_SIZE,
+			BookDataCollector::H5P_ACTIVITIES,
+			BookDataCollector::IN_CATALOG
+		];
 		$metadata_blog_meta = $this->bookDataCollector->getMultipleMeta( $id, $keys );
 
 		$metadata = array_merge( $metadata_info_array, $metadata_blog_meta );
