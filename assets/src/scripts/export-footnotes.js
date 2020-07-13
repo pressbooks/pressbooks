72393f289367440267eb4b92b1a380d51296b34b
Ricardo Aragon <ricardo@pressbooks.com>
2020-07-13 13:55:47 -0300

    fixing brackets syntax for JS Script

--- assets/src/scripts/export-footnotes.js
+++ assets/src/scripts/export-footnotes.js
@@ -2,7 +2,9 @@ function moveFootnotes() {
 	let footnotes = document.getElementsByClassName( 'footnote-indirect' );
 	for ( let i = 0; i < footnotes.length; i++ ) {
 		let ref = document.getElementById( footnotes[i].getAttribute( 'data-fnref' ) );
-		if ( ref ) footnotes[i].appendChild( ref );
+		if ( ref ) {
+			footnotes[i].appendChild( ref );
+		}
 	}
 }
 if ( typeof Prince != 'undefined' ) {
