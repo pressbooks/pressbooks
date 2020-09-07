88d6237e21169c227e21543a21deefa725cb12f6
Ricardo Aragon <ricardo@pressbooks.com>
2020-09-07 17:39:50 -0300

    Reducing lines for pipeline

--- inc/metadata/namespace.php
+++ inc/metadata/namespace.php
@@ -227,12 +227,10 @@ function book_information_to_schema( $book_information, $network_excluded_direct
 
 	if ( isset( $book_information['pb_primary_subject'] ) ) {
 		$name = Metadata\get_subject_from_thema( $book_information['pb_primary_subject'] );
-		if ( is_null( $name ) || ! $name ) {
-			$name = $book_information['pb_primary_subject'];
-		}
 		$book_schema['about'][] = [
 			'@type' => 'Thing',
-			'identifier' => $book_information['pb_primary_subject'],
+			'identifier' => ( is_null( $name ) || ! $name ) ?
+						$book_information['pb_primary_subject'] : $name,
 			'name' => $name,
 		];
 	}
@@ -241,13 +239,10 @@ function book_information_to_schema( $book_information, $network_excluded_direct
 		$additional_subjects = explode( ', ', $book_information['pb_additional_subjects'] );
 		foreach ( $additional_subjects as $additional_subject ) {
 			$name = Metadata\get_subject_from_thema( $additional_subject );
-			if ( is_null( $name ) || ! $name ) {
-				$name = $additional_subject;
-			}
 			$book_schema['about'][] = [
 				'@type' => 'Thing',
 				'identifier' => $additional_subject,
-				'name' => $name,
+				'name' => ( is_null( $name ) || ! $name ) ? $additional_subject : $name,
 			];
 		}
 	}
