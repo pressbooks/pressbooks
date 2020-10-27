3637fcf43314e4af632161a1007f0c5a0b53e9ee
Ho Man Chan <homan98@gmail.com>
2020-10-27 17:36:17 -0400

    Update version and localizations

--- pressbooks.php
+++ pressbooks.php
@@ -5,7 +5,7 @@ Plugin URI: https://pressbooks.org
 GitHub Plugin URI: pressbooks/pressbooks
 Release Asset: true
 Description: Simple Book Production
-Version: 5.17.0
+Version: 5.17.1
 Author: Pressbooks (Book Oven Inc.)
 Author URI: https://pressbooks.org
 Text Domain: pressbooks
