c66d06564c739fb2337973d78965a24a724e8b7c
arzola <oscararzola@gmail.com>
2021-01-08 12:23:50 -0600

    Improved replacement and added new test case

--- tests/test-login.php
+++ tests/test-login.php
@@ -7,17 +7,25 @@ class Login extends \WP_UnitTestCase {
 	 */
 	public function test_if_wp_prefix_is_removed() {
 
-		$scenario1 = 'https://institution.pressbooks.pub/wp/booktitle/wp-login.php?action=lostpassword';
-		$expected = 'https://institution.pressbooks.pub/booktitle/wp-login.php?action=lostpassword';
+		$scenario1 = 'https://network.pressbooks.pub/wp/booktitle/wp-login.php?action=lostpassword';
+		$expected = 'https://network.pressbooks.pub/booktitle/wp-login.php?action=lostpassword';
 
 		$actual = \Pressbooks\Registration\remove_wp_prefix($scenario1);
 
 		$this->assertEquals($expected, $actual);
 
-		$scenario = 'https://institution.pressbooks.pub/wp/wp-login.php?action=lostpassword';
-		$expected = 'https://institution.pressbooks.pub/wp-login.php?action=lostpassword';
+		$scenario2 = 'https://network.pressbooks.pub/wp/wp-login.php?action=lostpassword';
+		$expected = 'https://network.pressbooks.pub/wp/wp-login.php?action=lostpassword';
 
-		$actual = \Pressbooks\Registration\remove_wp_prefix($scenario);
+		$actual = \Pressbooks\Registration\remove_wp_prefix($scenario2);
+
+		$this->assertEquals($expected, $actual);
+
+
+		$scenario3 = 'https://network.pressbooks.pub/wp-login.php?action=lostpassword';
+		$expected = 'https://network.pressbooks.pub/wp-login.php?action=lostpassword';
+
+		$actual = \Pressbooks\Registration\remove_wp_prefix($scenario3);
 
 		$this->assertEquals($expected, $actual);
 
