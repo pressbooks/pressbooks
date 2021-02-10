17ed72b0324a86a4529709f90d75d41e5912e390
arzola <oscararzola@gmail.com>
2021-02-09 20:27:09 -0600

    H5P Activities anchor

---
+++ templates/interactive/h5p.blade.php
@@ -0,0 +1,6 @@
+<div class="textbox interactive-content">
+	<span class="interactive-content__icon"></span>
+	<p>{{ __( 'An interactive H5P element has been excluded from this version of the text. You can view it online here:', 'pressbooks' ) }}
+		<a href="{{ $url.$id }}" title="{{ $title }}">{{ $url }}</a>
+	</p>
+</div>
