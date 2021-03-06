4b9ebf22ea564f5e0246d20516d5fed8dfa190e0
Ricardo Aragon <richard015ar@gmail.com>
2021-03-06 18:38:36 -0500

    Add Log module for track information and send to S3 and/or cloudwatch

---
+++ inc/log/class-s3storageprovider.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Pressbooks\Log;
+
+use Aws\Credentials\CredentialProvider;
+use Aws\Exception\AwsException as AwsException;
+use Aws\S3\S3Client as S3Client;
+use function Pressbooks\Utility\debug_error_log;
+
+class S3StorageProvider implements StorageProvider {
+
+	/**
+	 * @var string
+	 */
+	private $bucket_name;
+
+	/**
+	 * @var string
+	 */
+	private $secret_key;
+
+	/**
+	 * @var string
+	 */
+	private $access_key_id;
+
+	/**
+	 * @var string
+	 */
+	private $version;
+
+	/**
+	 * @var string
+	 */
+	private $region;
+
+	/**
+	 * @var S3Client
+	 */
+	private $client;
+
+	/**
+	 * @var string
+	 */
+	private $file_path;
+
+	const FILENAME_FORMAT = '-saml_logs.csv';
+
+	const AWS_CONFIG_FILENAME = 'does_not_exist.ini';
+
+	const AWS_MAIN_LOG_FOLDER = 'saml_logs';
+
+	private function create() {
+		if ( self::areEnvironmentVariablesPresent() && is_null( $this->client ) ) {
+			$this->region = env( 'AWS_S3_REGION' );
+			$this->version = env( 'AWS_S3_VERSION' );
+			$this->bucket_name = env( 'AWS_S3_OIDC_BUCKET' );
+			$this->access_key_id = env( 'AWS_ACCESS_KEY_ID' );
+			$this->secret_key = env( 'AWS_SECRET_ACCESS_KEY' );
+			$environment = env( 'WP_ENV' ) ? env( 'WP_ENV' ) : 'production';
+			$scheme = is_ssl() ? 'https' : 'http';
+			if ( is_null( $this->file_path ) ) {
+				$this->file_path = 's3://' . $this->bucket_name . '/' . self::AWS_MAIN_LOG_FOLDER . '/' . $environment .
+					'/' . wp_hash( network_home_url( '', $scheme ) ) . '/' . current_time( 'Y-m' ) . self::FILENAME_FORMAT;
+			}
+			if ( is_null( $this->client ) ) {
+				$this->client = new S3Client( [
+					'region' => $this->region,
+					'version' => $this->version,
+					'credentials' => CredentialProvider::env(),
+				] );
+			}
+			return true;
+		} else {
+			debug_error_log( 'Error initializing S3 Storage Provider: Some environment variables are not present.' );
+		}
+		return ! is_null( $this->client );
+	}
+
+	/**
+	 * NOTE: We will use the same environment variables for OIDC plugin temporary.
+	 *
+	 * @return bool
+	 */
+	public static function areEnvironmentVariablesPresent() {
+		if (
+			! is_null( env( 'LOG_LOGIN_ATTEMPTS' ) ) &&
+			! is_null( env( 'AWS_S3_OIDC_BUCKET' ) ) &&
+			! is_null( env( 'AWS_SECRET_ACCESS_KEY' ) ) &&
+			! is_null( env( 'AWS_ACCESS_KEY_ID' ) ) &&
+			! is_null( env( 'AWS_S3_VERSION' ) ) &&
+			! is_null( env( 'AWS_S3_REGION' ) )
+		) {
+			if ( is_null( env( 'AWS_CONFIG_FILE' ) ) ) {
+				// this is an env variable needed for AWS SDK
+				putenv( 'AWS_CONFIG_FILE=' . __DIR__ . '/' . self::AWS_CONFIG_FILENAME );
+			}
+			return true;
+		} else {
+			debug_error_log( 'Error initializing S3 Storage Provider: Some environment variables are not present.' );
+		}
+		return false;
+	}
+
+	public function setFilePath( string $file_path ) {
+		$this->file_path = $file_path;
+	}
+
+	public function store( array $data, string $file_header = null ) {
+		if ( $this->create() ) {
+			try {
+				$this->client->registerStreamWrapper();
+				$stream = fopen( $this->file_path, 'a' );
+				$data = $this->getDataFormat( $data );
+				if ( ! is_null( $file_header ) && ! file_exists( $this->file_path ) ) {
+					$data = $file_header . $data;
+				}
+				fwrite( $stream, $data );
+				fclose( $stream );
+				return true;
+			} catch ( AwsException $e ) {
+				debug_error_log( 'Error saving logs in S3: ' . $e->getMessage() );
+			}
+		}
+		return false;
+	}
+
+	public function getDataFormat( array $data ) {
+		$data_csv_format = '';
+		$current_date = current_time( 'mysql' );
+		foreach ( $data as $key => $value ) {
+			$data_csv_format .= $current_date . ',' . $key . ',"' . print_r( $value, true ) . '"' . "\n"; // @codingStandardsIgnoreLine
+		}
+		return $data_csv_format;
+	}
+
+	public function setClient( $client ) {
+		$this->client = $client;
+	}
+}
---
+++ tests/test-log.php
@@ -0,0 +1,127 @@
+<?php
+
+use Aws\CloudWatchLogs\CloudWatchLogsClient;
+use Aws\S3\S3Client as S3Client;
+use Maxbanton\Cwh\Handler\CloudWatch;
+use Monolog\Logger;
+use Pressbooks\Log;
+
+class LogTest extends \WP_UnitTestCase {
+
+	/**
+	 * @var Log\S3StorageProvider
+	 */
+	private $s3_provider_mock;
+
+	/**
+	 * @var Log\CloudWatchProvider
+	 */
+	private $cloudwatch_provider_mock;
+
+	/**
+	 * @var Log\Log
+	 */
+	private $log;
+
+	const TEST_FILE_PATH = 'tests/data/log.csv';
+
+	/**
+	 * Test setup
+	 */
+	public function setUp() {
+		$this->setEnvironmentVariables();
+	}
+
+	private function setEnvironmentVariables() {
+		putenv( 'AWS_S3_OIDC_BUCKET=fakeBucket' );
+		putenv( 'AWS_SECRET_ACCESS_KEY=fakeAccessKey' );
+		putenv( 'AWS_ACCESS_KEY_ID=fakeKeyId' );
+		putenv( 'AWS_S3_VERSION=fakeVersion' );
+		putenv( 'AWS_S3_REGION=fakeRegion' );
+	}
+
+	private function setS3ClientMock() {
+		$s3_client_mock = $this
+			->getMockBuilder( S3Client::class )
+			->disableOriginalConstructor()
+			->setMethods([
+				'registerStreamWrapper',
+			])
+			->getMock();
+		$this->s3_provider_mock = new Log\S3StorageProvider();
+		$this->s3_provider_mock->setClient( $s3_client_mock );
+		$this->s3_provider_mock->setFilePath( self::TEST_FILE_PATH );
+		$this->log = new Log\Log( $this->s3_provider_mock );
+	}
+
+	private function setLoggerMock() {
+		$logger_mock = $this
+			->getMockBuilder( Logger::class )
+			->disableOriginalConstructor()
+			->setMethods([
+				'debug',
+				'pushHandler',
+			])
+			->getMock();
+		$logger_mock->expects( $this->any() )
+			->method( 'debug' )
+			->will( $this->onConsecutiveCalls( true ) );
+		$logger_mock->expects( $this->any() )
+			->method( 'pushHandler' )
+			->will( $this->onConsecutiveCalls( true ) );
+		$cloudwatch_logs_mock = $this
+			->getMockBuilder( CloudWatchLogsClient::class )
+			->disableOriginalConstructor()
+			->getMock();
+		$handler = $this
+			->getMockBuilder( CloudWatch::class )
+			->disableOriginalConstructor()
+			->setMethods([
+				'setFormatter',
+			])
+			->getMock();
+		$handler->expects( $this->any() )
+			->method( 'setFormatter' )
+			->will( $this->onConsecutiveCalls( true ) );
+		$this->cloudwatch_provider_mock = new Log\CloudWatchProvider();
+		$this->cloudwatch_provider_mock->setLogger( $logger_mock );
+		$this->cloudwatch_provider_mock->setHandler( $handler );
+		$this->cloudwatch_provider_mock->setClient( $cloudwatch_logs_mock );
+		$this->log = new Log\Log( $this->cloudwatch_provider_mock );
+	}
+
+	/**
+	 * @group log
+	 */
+	public function test_s3_store() {
+		$this->setS3ClientMock();
+		$this->log->addRowToData( 'Test key 1', ['Test value'] );
+		$this->log->addRowToData( 'Test key 2', [
+			'Test a' => 'Test b',
+			'Test c' => 'Test d',
+		] );
+		$this->assertTrue( $this->log->store() );
+		$file_content = str_getcsv( file_get_contents( self::TEST_FILE_PATH ) );
+		unlink( self::TEST_FILE_PATH );
+		$this->assertEquals( 'Test key 1', $file_content[1] );
+		$this->assertContains( 'Test value', $file_content[2] );
+		$this->assertEquals( 'Test key 2', $file_content[3] );
+		$this->assertContains( 'Test b', $file_content[4] );
+		$this->assertContains( 'Test d', $file_content[4] );
+		$this->assertContains( '[Test a] =>', $file_content[4] );
+		$this->assertContains( '[Test c] =>', $file_content[4] );
+	}
+
+	/**
+	 * @group log
+	 */
+	public function test_cloudwatch_store() {
+		$this->setLoggerMock();
+		$this->log->addRowToData( 'Test key 1', ['Test value'] );
+		$this->log->addRowToData( 'Test key 2', [
+			'Test a' => 'Test b',
+			'Test c' => 'Test d',
+		] );
+		$this->assertTrue( $this->log->store() );
+	}
+}
