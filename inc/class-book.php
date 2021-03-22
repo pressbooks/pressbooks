ea41807881eb1814672c4064014b9b9a8955b29e
SteelWagstaff <steel@pressbooks.com>
2021-03-22 15:01:42 -0700

    Link spacing

--- inc/class-book.php
+++ inc/class-book.php
@@ -204,7 +204,7 @@ class Book {
 		if ( self::removeInvalidatedBisacCodes( $blog_id, $book_information_array ) ) {
 			// @codingStandardsIgnoreStart
 			add_error( __(
-				"This book was using a <a href='https://bisg.org/page/InactivatedCodes' target='_blank'> retired BISAC subject term</a> , which has been replaced in your book with a recommended BISAC replacement. You may wish to check the BISAC subject terms manually to confirm that you are satisfied with these replacements."
+				"This book was using a <a href='https://bisg.org/page/InactivatedCodes' target='_blank'> retired BISAC subject term</a>, which has been replaced in your book with a recommended BISAC replacement. You may wish to check the BISAC subject terms manually to confirm that you are satisfied with these replacements."
 			) );
 			// @codingStandardsIgnoreEnd
 			return true;
