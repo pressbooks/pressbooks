3de0d2f07cf4674566dad55d4df7b4ea4be0ea11
arzola <oscararzola@gmail.com>
2021-06-03 16:32:41 -0500

    Fixed test

--- tests/test-datacollector-book.php
+++ tests/test-datacollector-book.php
@@ -226,14 +226,14 @@ class DataCollector_BookTest extends \WP_UnitTestCase {
 		$path = $this->bookDataCollector->getCoverThumbnail( $blog_id, 'http://presssbooks.test/https-cover-image.jpg' );
 		$this->assertEquals( 'https://presssbooks.test/https-cover-image.jpg', $path );
 
-		$_SERVER['HTTPS'] = 'off';
-
 		$attachment_id = $this->factory->attachment->create_upload_object( __DIR__ . '/data/skates.jpg', $blog_id );
 		$attachment_path = wp_get_attachment_url( $attachment_id );
 
 		$path = $this->bookDataCollector->getCoverThumbnail( $blog_id, $attachment_path, $attachment_id );
 
 		$this->assertEquals( 1, preg_match( '/https:\/\/.*-768x1024\.jpg/', $path ) );
+
+		$_SERVER['HTTPS'] = 'off';
 	}
 
 
