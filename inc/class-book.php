8d1149f4e7735a243d9c28ab3136f4a948d6f4ad
Ricardo Aragon <richard015ar@gmail.com>
2021-10-02 13:29:25 -0400

    Check contributors cached format in bookInformation method to know when read from the database instead cache

--- inc/class-book.php
+++ inc/class-book.php
@@ -109,7 +109,18 @@ class Book {
 		if ( static::useCache() ) {
 			$cached_book_information = wp_cache_get( $cache_id, 'pb' );
 			if ( $cached_book_information ) {
-				if ( ! $read_contributors_from_cache ) {
+				$contributors_cached_as_string = true;
+				foreach ( $contributors->valid as $contributor_type ) {
+					if (
+						isset( $cached_book_information[ $contributor_type ] ) &&
+						! empty( $cached_book_information[ $contributor_type ] ) &&
+						is_array( $cached_book_information[ $contributor_type ] )
+					) {
+						$contributors_cached_as_string = false;
+						break;
+					}
+				}
+				if ( ! $read_contributors_from_cache || $contributors_cached_as_string !== $contributors_as_string ) {
 					$cached_book_information = array_merge(
 						$cached_book_information,
 						$contributors->getAll(
