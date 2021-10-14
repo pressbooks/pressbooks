f446d5f9aadf562d723e7d35c1088b9e86f386e1
Felipe Dalcin <felipe@pressbooks.com>
2021-10-14 17:05:40 -0300

    Improve creators metadata

--- inc/modules/export/epub/templates/epub3/opf.php
+++ inc/modules/export/epub/templates/epub3/opf.php
@@ -19,17 +19,17 @@ use HumanNameParser\Parser;
 
 echo '<?xml version="1.0" encoding="UTF-8" ?>' . "\n";
 ?>
-<package version="3.0" xmlns="http://www.idpf.org/2007/opf" unique-identifier="PrimaryID">
+<package version="3.0" xmlns="http://www.idpf.org/2007/opf" unique-identifier="pub-identifier">
 
 	<metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf">
 		<?php
 		// Required, Title
-		echo '<dc:title>' . ( ! empty( $meta['pb_title'] ) ? $meta['pb_title'] : get_bloginfo( 'name' ) ) . '</dc:title>';
+		echo '<dc:title id="pub-title">' . ( ! empty( $meta['pb_title'] ) ? $meta['pb_title'] : get_bloginfo( 'name' ) ) . '</dc:title>';
 		unset( $meta['pb_title'] );
 		echo "\n";
 
 		// Required, Language
-		echo '<dc:language>' . $lang . '</dc:language>';
+		echo '<dc:language id="pub-language">' . $lang . '</dc:language>';
 		unset( $meta['pb_language'] );
 		echo "\n";
 
@@ -37,16 +37,19 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>' . "\n";
 		echo '<meta property="dcterms:modified">' . ( date( 'Y-m-d\TH:i:s\Z' ) ) . '</meta>';
 		echo "\n";
 
-		// Required, Primary ID
-        // TODO: Add DOI?
+		// Required, Primary I
 		if ( ! empty( $meta['pb_ebook_isbn'] ) ) {
-			echo '<dc:identifier id="PrimaryID">' . trim( $meta['pb_ebook_isbn'] ) . '</dc:identifier>';
-		} else {
-			echo '<dc:identifier id="PrimaryID">' . trim( get_bloginfo( 'url' ) ) . '</dc:identifier>';
+			echo '<dc:identifier id="pub-identifier">' . trim( $meta['pb_ebook_isbn'] ) . '</dc:identifier>';
+            unset( $meta['pb_ebook_isbn'] );
 		}
-
-		unset( $meta['pb_ebook_isbn'] );
-		echo "\n";
+        elseif ( ! empty( $meta['pb_book_doi'] ) ) {
+            echo '<dc:identifier id="pub-identifier">' . trim( $meta['pb_book_doi'] ) . '</dc:identifier>';
+            unset( $meta['pb_book_doi'] );
+        }
+        else {
+            echo '<dc:identifier id="pub-identifier">' . trim(get_bloginfo('url')) . '</dc:identifier>';
+        }
+        echo "\n";
 
 		// Pick best non-html description
 		if ( ! empty( $meta['pb_about_50'] ) ) {
@@ -57,99 +60,83 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>' . "\n";
 			unset( $meta['pb_about_140'] );
 		}
 
-		// First author
-        // Check to see if we have pb_editors.
-        // If yes, add each one as a dc:creator with role edt in order in which they are listed in book info
-        // Check to see if we have pb_authors.
-        // yes, add each one as a dc:creator with role aut in the order in which they are listed in book info
-        // Check to see if we have pb_translators
-        // If yes, add each one as a dc:creator with role trl
-        // Check to see if we have pb_illustrators
-        // If yes, add each one as a dec:creator with role ill
-        $index = 1;
-        if ( ! \Pressbooks\Utility\empty_space( $meta['pb_editors'] ) ) {
-            $editors = explode_remove_and(';', $meta['pb_editors']);
-            foreach ( $editors as $editor ) {
-                $contributor_number = $index;
-                if ( $index < 10 ) {
-                    $contributor_number = str_pad( $index, 2, '0', STR_PAD_LEFT );
-                }
-                echo "<dc:creator id='creator{$contributor_number}'>{$editor}</dc:creator>\n";
-                echo "<meta refines='#creator{$contributor_number}' property='role' scheme='marc:relators' id='role'>edt</meta>\n";
-                // TODO: add file-as if possible
-                // echo "<meta refines='#creator{$contributor_number}' property='file-as'>last name, first name</meta>";
-                $index++;
-            }
-        }
-        if ( ! \Pressbooks\Utility\empty_space( $meta['pb_authors'] ) ) {
-            $authors = explode_remove_and(';', $meta['pb_authors']);
-            foreach ( $authors as $author ) {
-                $contributor_number = $index;
-                if ( $index < 10 ) {
-                    $contributor_number = str_pad( $index, 2, '0', STR_PAD_LEFT );
-                }
-                echo "<dc:creator id='creator{$contributor_number}'>{$author}</dc:creator>\n";
-                echo "<meta refines='#creator{$contributor_number}' property='role' scheme='marc:relators' id='role'>aut</meta>\n";
-                // TODO: add file-as if possible
-                // echo "<meta refines='#creator{$contributor_number}' property='file-as'>last name, first name</meta>";
-                $index++;
-            }
-        }
-        if ( ! \Pressbooks\Utility\empty_space( $meta['pb_translators'] ) ) {
-            $translators = explode_remove_and( ';', $meta['pb_translators'] );
-            foreach ( $translators as $translator ) {
-                $contributor_number = $index;
-                if ( $index < 10 ) {
-                    $contributor_number = str_pad( $index, 2, '0', STR_PAD_LEFT );
-                }
-                echo "dc:creator id='creator{$contributor_number}>{$translator}</dc:creator>\n";
-                echo "<meta refines='#creator{$contributor_number}' property='role' scheme='marc:relators' id='role'>trl</meta>\n";
-                $index++;
-            }
-        }
-        if ( ! Pressbooks\Utility\empty_space( $meta['pb_illustrators'] ) ) {
-            $illustrators = explode_remove_and( ';', $meta['pb_illustrators'] );
-            foreach( $illustrators as $illustrator ) {
-                $contributor_number = $index;
-                if( $index < 10 ) {
-                    $contributor_number = str_pad( $index, 2, '0', STR_PAD_LEFT );
-                }
-                echo "dc:creator id='creator{$contributor_number}>{$illustrator}</dc:creator>\n";
-                echo "<meta refines='#creator{$contributor_number}' property='role' scheme='marc:relators' id='role'>ill</meta>\n";
-                $index++;
-            }
-        }
-        if ( $index === 1 ) {
+
+        // Add creators in the following order: Editors, Authors, Translators, Illustrators
+		$index = 1;
+		if ( ! \Pressbooks\Utility\empty_space( $meta['pb_editors'] ) ) {
+//			echo \Pressbooks\Modules\Export\get_epub_contributor_meta( $meta['pb_editors'], $index, 'edt' );
+			            $editors = explode_remove_and(';', $meta['pb_editors']);
+			            foreach ( $editors as $editor ) {
+			                $contributor_number = $index;
+			                if ( $index < 10 ) {
+			                    $contributor_number = str_pad( $index, 2, '0', STR_PAD_LEFT );
+			                }
+			                echo "<dc:creator id='creator{$contributor_number}'>{$editor}</dc:creator>\n";
+			                echo "<meta refines='#creator{$contributor_number}' property='role' scheme='marc:relators' id='role{$contributor_number}'>edt</meta>\n";
+			                // TODO: add file-as if possible
+			                // echo "<meta refines='#creator{$contributor_number}' property='file-as'>last name, first name</meta>";
+			                $index++;
+			            }
+		}
+		if ( ! \Pressbooks\Utility\empty_space( $meta['pb_authors'] ) ) {
+//			echo \Pressbooks\Modules\Export\get_epub_contributor_meta( $meta['pb_authors'], $index, 'aut' );
+			            $authors = explode_remove_and(';', $meta['pb_authors']);
+			            foreach ( $authors as $author ) {
+			                $contributor_number = $index;
+			                if ( $index < 10 ) {
+			                    $contributor_number = str_pad( $index, 2, '0', STR_PAD_LEFT );
+			                }
+			                echo "<dc:creator id='creator{$contributor_number}'>{$author}</dc:creator>\n";
+			                echo "<meta refines='#creator{$contributor_number}' property='role' scheme='marc:relators' id='role{$contributor_number}'>aut</meta>\n";
+			                // TODO: add file-as if possible
+			                // echo "<meta refines='#creator{$contributor_number}' property='file-as'>last name, first name</meta>";
+			                $index++;
+			            }
+		}
+		if ( ! \Pressbooks\Utility\empty_space( $meta['pb_translators'] ) ) {
+//			echo \Pressbooks\Modules\Export\get_epub_contributor_meta( $meta['pb_translators'], $index, 'trl' );
+			            $translators = explode_remove_and( ';', $meta['pb_translators'] );
+			            foreach ( $translators as $translator ) {
+			                $contributor_number = $index;
+			                if ( $index < 10 ) {
+			                    $contributor_number = str_pad( $index, 2, '0', STR_PAD_LEFT );
+			                }
+			                echo "<dc:creator id='creator{$contributor_number}'>{$translator}</dc:creator>\n";
+			                echo "<meta refines='#creator{$contributor_number}' property='role' scheme='marc:relators' id='role{$contributor_number}'>trl</meta>\n";
+			                $index++;
+			            }
+		}
+		if ( ! \Pressbooks\Utility\empty_space( $meta['pb_illustrators'] ) ) {
+//			echo \Pressbooks\Modules\Export\get_epub_contributor_meta( $meta['pb_illustrators'], $index, 'ill' );
+			            $illustrators = explode_remove_and( ';', $meta['pb_illustrators'] );
+			            foreach( $illustrators as $illustrator ) {
+			                $contributor_number = $index;
+			                if( $index < 10 ) {
+			                    $contributor_number = str_pad( $index, 2, '0', STR_PAD_LEFT );
+			                }
+			                echo "<dc:creator id='creator{$contributor_number}'>{$illustrator}</dc:creator>\n";
+			                echo "<meta refines='#creator{$contributor_number}' property='role' scheme='marc:relators' id='role{$contributor_number}'>ill</meta>\n";
+			                $index++;
+			            }
+		}
+		if ( $index === 1 ) {
 			echo '<dc:creator id="creator">Pressbooks</dc:creator>';
 		}
 
-		// Contributing authors
-        // TODO: Check to see if we have pb_contributor
-        // If yes, add each one as a dc:contributor with role ctb
-		if ( ! empty( $meta['pb_contributors'] ) ) {
+		// Add contributors
+		if ( ! \Pressbooks\Utility\empty_space( $meta['pb_contributors'] ) ) {
 			$contributors = explode_remove_and( ';', $meta['pb_contributors'] );
 			foreach ( $contributors as $contributor ) {
-				echo '<dc:contributor>' . trim( $contributor ) . '</dc:contributor>' . "\n";
-                echo "<meta refines='#contributor{$contributor_number}' property='role' scheme='marc:relators' id='role'>ctb</meta>";
-			}
-			// TODO: WHY DO THIS?
-            unset( $meta['pb_contributors'] );
+                $contributor_number = $index;
+                if ($index < 10) {
+                    $contributor_number = str_pad($index, 2, '0', STR_PAD_LEFT);
+                }
+                echo "<dc:contributor id='contributor{$contributor_number}'>{$contributor}</dc:contributor>\n";
+                echo "<meta refines='#contributor{$contributor_number}' property='role' scheme='marc:relators' id='role{$contributor_number}'>ctb</meta>\n";
+                $index++;
+            }
+			unset( $meta['pb_contributors'] );
 		}
-
-		// Refines
-		echo '<meta refines="#author" property="file-as">';
-
-		try {
-			// TODO: Refactor to use term metadata: contributor_last_name, contributor_first_name
-			$nameparser = new Parser();
-			$author = $nameparser->parse( $first_author );
-			$author_file_as = $author->getLastName() . ', ' . $author->getFirstName();
-		} catch ( NameParsingException $e ) {
-			$author_file_as = $first_author;
-		}
-		echo $first_author;
-		echo '</meta>';
-		unset( $meta['pb_authors'] );
 
 		// Copyright
 		if ( ! empty( $meta['pb_copyright_year'] ) || ! empty( $meta['pb_copyright_holder'] ) ) {
@@ -198,13 +185,9 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>' . "\n";
 					// TODO: There should be default behaviour? E.g. echo "<!-- $key, $val -->\n";
 					break;
 			}
-		}
-
-		// Required for Kindle: Cover Image
-		echo '<meta name="cover" content="cover-image" />' . "\n";
-		?>
-	</metadata>
-
+        }
+	?>
+    </metadata>
 
 	<manifest>
 		<?php
@@ -217,7 +200,6 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>' . "\n";
 		<item id="stylesheet" href="OEBPS/<?php echo $stylesheet; ?>"  media-type="text/css" />
 		<?php endif; ?>
 	</manifest>
-
 
 	<spine toc="ncx">
 		<?php
@@ -268,7 +250,6 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>' . "\n";
 		}
 
 		?>
-	</guide>
-
+    </guide>
 
 </package>
