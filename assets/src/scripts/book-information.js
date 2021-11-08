ae82c9da347d0ff98dd4533f4f92673422b9e953
Felipe Dalcin <felipe@pressbooks.com>
2021-11-08 10:44:37 -0300

    fix: improve book info page descriptions

--- assets/src/scripts/book-information.js
+++ assets/src/scripts/book-information.js
@@ -1,8 +1,32 @@
 /* global PB_BookInfoToken */
 
 jQuery( document ).ready( function ( $ ) {
+	/**
+	 * @param selector
+	 * @param id
+	 */
+	const addAriaDescribedBy = function ( selector, id ) {
+		const input = jQuery( selector );
+
+		input.attr( 'aria-describedby', id );
+		input.parent().parent().find( 'span[class=description]' ).attr( 'id', id );
+	};
+
+	const inputs = [
+		'pb_short_title', 'pb_publisher', 'pb_publisher_city', 'pb_publication_date', 'pb_onsale_date', 'pb_ebook_isbn',
+		'pb_print_isbn', 'pb_language', 'pb_cover_image', 'primary-subject', 'additional-subjects', 'pb_is_based_on',
+		'pb_copyright_year', 'pb_copyright_holder', 'pb_book_license', 'pb_about_140', 'pb_about_50', 'pb_series_title',
+		'pb_series_number', 'pb_keywords_tags', 'pb_hashtag', 'pb_list_price_print', 'pb_list_price_pdf', 'pb_list_price_epub',
+		'pb_list_price_web', 'pb_audience', 'pb_bisac_subject', 'pb_bisac_regional_theme',
+	];
+
+	for ( let input of inputs ) {
+		addAriaDescribedBy( '#' + input, input + '_description' );
+	}
+
 	// Set an initial focus to help users of assistive technology
 	$( '#pb_title' ).trigger( 'focus' );
+
 	// Select2, The jQuery replacement for select boxes
 	$( '#primary-subject' ).select2( {
 		placeholder: PB_BookInfoToken.selectSubjectText,
