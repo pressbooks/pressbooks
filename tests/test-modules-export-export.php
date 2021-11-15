d4bab6ccad607724d58224c81401dc44e73b3058
Ricardo Aragon <richard015ar@gmail.com>
2021-11-15 16:28:55 -0500

    fix test for export

--- tests/test-modules-export-export.php
+++ tests/test-modules-export-export.php
@@ -416,8 +416,8 @@ class Modules_Export_ExportTest extends \WP_UnitTestCase {
 			$this->assertContains( '<p><em>Ka kite ano!</em></p>', $xhtml_content );
 			$this->assertContains( 'https://github.com/pressbooks/pressbooks', $xhtml_content );
 			$this->assertContains( '</h2><h2 class="chapter-subtitle">Or, A Chapter to Test</h2></div>', $xhtml_content );
-			$this->assertContains( '<p>A YouTube element has been excluded from this version of the text.', $xhtml_content );
-			$this->assertRegExp( '~/?p=\d+#pb-interactive-content"~', $xhtml_content ); //  href="http://example.org/testpath26/?p=21#pb-interactive-content"
+			$this->assertContains( '<p>One or more interactive elements has been excluded from this version of the text.', $xhtml_content );
+			$this->assertContains( '#oembed-', $xhtml_content ); //  href="http://example.org/testpath26/?p=21#pb-interactive-content"
 		}
 
 		foreach ( $paths as $path ) {
