60b4db33554e9cceb07b5ff50e5176cb7a16574c
Ricardo Aragon <richard015ar@gmail.com>
2021-11-16 08:08:09 -0500

    improve phet class structure and add unit tests for this

--- tests/test-interactive-phet.php
+++ tests/test-interactive-phet.php
@@ -41,4 +41,44 @@ class Interactive_PhetTest extends \WP_UnitTestCase {
 		$this->assertNotEmpty( $wp_embed->handlers[10][ $this->phet::EMBED_ID ] );
 	}
 
+	/**
+	 * @group interactivecontent
+	 */
+	public function test_applyEmbedFilterForWeb() {
+		$iframe_html = $this->phet->applyEmbedFilterForWeb(
+			"<iframe src='https://pressbooks.com'></iframe>",
+			[],
+			'https://pressbooks.com',
+			''
+		);
+		$this->assertContains(
+			"<iframe id=\"iframe-phet-1\" src=\"https://phet.colorado.edu/sims/html/i\" width=\"800\" height=\"600\" scrolling=\"no\" allowfullscreen></iframe>",
+			$iframe_html
+		);
+	}
+
+	/**
+	 * @group interactivecontent
+	 */
+	public function test_applyEmbedFilterForExport() {
+		$iframe_html = $this->phet->applyEmbedFilterForExport(
+			"<iframe src='https://pressbooks.com'></iframe>",
+			[],
+			'https://pressbooks.com',
+			''
+		);
+		$this->assertContains(
+			'<div class="textbox interactive-content interactive-content--iframe">',
+			$iframe_html
+		);
+		$this->assertContains(
+			'One or more interactive elements has been excluded from this version of the text. You can view them online here',
+			$iframe_html
+		);
+		$this->assertContains(
+			'<a href="#iframe-phet-1" title="">#iframe-phet-1</a>',
+			$iframe_html
+		);
+	}
+
 }
