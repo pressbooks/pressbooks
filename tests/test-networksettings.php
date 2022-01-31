34225d69410a509536df6ac5768928683d385330
arzola <oscararzola@gmail.com>
2022-01-31 11:35:28 -0500

    fix: modify set_up method

--- tests/test-networksettings.php
+++ tests/test-networksettings.php
@@ -12,8 +12,8 @@ class NetworkSettingsTest extends \WP_UnitTestCase {
 	 */
 	private $networkSettings;
 
-	public function setUp() {
-		parent::setUp();
+	public function set_up() {
+		parent::set_up();
 		$this->networkSettings = new \Pressbooks\Admin\Network\NetworkSettings();
 	}
 
