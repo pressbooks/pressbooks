39d24d9972b2f9719ca5e19278e8eb95ffe51149
Felipe Dalcin <felipe.sdalcin@gmail.com>
2022-07-20 14:08:43 -0400

    chore: update rector config

--- rector.php
+++ rector.php
@@ -6,16 +6,23 @@ use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
-        __DIR__ . '/src'
-    ]);
+return static function ( RectorConfig $config ): void {
+	$config->paths( [
+		__DIR__ . '/bin',
+		__DIR__ . '/inc',
+		__DIR__ . '/symbionts',
+		__DIR__ . '/templates',
+		__DIR__ . '/tests',
+	] );
 
-    // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+	// Directories to skip, rules I don't agree with...
+	$config->skip( [
+		\Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector::class,
+		\Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector::class,
+	] );
 
-    // define sets of rules
-    //    $rectorConfig->sets([
-    //        LevelSetList::UP_TO_PHP_74
-    //    ]);
+	// define sets of rules
+	$config->sets( [
+		LevelSetList::UP_TO_PHP_80,
+	] );
 };
