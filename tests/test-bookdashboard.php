a62ae2907a17bb4d78273a3e57052faf65e6cec6
Felipe Dalcin <felipe.sdalcin@gmail.com>
2022-12-08 09:47:32 -0500

    feat: add additional tests

--- tests/test-bookdashboard.php
+++ tests/test-bookdashboard.php
@@ -1,6 +1,8 @@
 <?php
 
 use Pressbooks\Admin\Dashboard\BookDashboard;
+use Pressbooks\Metadata;
+use function Pressbooks\Admin\Metaboxes\upload_cover_image;
 
 /**
  * @group book-dashboard
@@ -84,5 +86,54 @@ class BookDashboardTest extends \WP_UnitTestCase {
 		$this->expectOutputRegex( '/<div class="pb-book-cover">/' );
 		$this->expectOutputRegex( '/<div class="pb-dashboard-action">/' );
 		$this->expectOutputRegex( '/<li id="delete">/' );
+	}
+
+	/**
+	 * @test
+	 */
+	public function it_retrieves_default_book_cover(): void {
+		$this->_book();
+
+		wp_set_current_user( $this->factory()->user->create( [ 'role' => 'administrator' ] ) );
+
+		ob_start();
+		$this->bookDashboard->renderBookDashboard();
+		$output = ob_get_clean();
+
+		$this->assertStringContainsString( 'images/default-book-cover-225x0@2x.jpg', $output );
+	}
+
+	/**
+	 * @test
+	 */
+	public function it_retrieves_book_cover(): void {
+		$this->_book();
+
+		wp_set_current_user( $this->factory()->user->create( [ 'role' => 'administrator' ] ) );
+
+		$_FILES = [
+			'pb_cover_image' => [
+				'name' => 'image.jpeg',
+			],
+		];
+
+		copy( __DIR__ . '/data/upload/image.jpeg', __DIR__ . '/data/upload/image_test.jpeg' );
+
+		$image = [
+			'file' => __DIR__ . '/data/upload/image_test.jpeg',
+			'url' => 'https://pressbooks.test/app/uploads/sites/4/2021/07/image.jpeg',
+			'type' => 'image/jpeg',
+		];
+
+		$post_id = ( new Metadata )->getMetaPostId();
+
+		upload_cover_image( $post_id, null, $image );
+
+		ob_start();
+		$this->bookDashboard->renderBookDashboard();
+		$output = ob_get_clean();
+
+		$this->assertStringNotContainsString( 'images/default-book-cover-225x0@2x.jpg', $output );
+		$this->assertStringContainsString( 'data/upload/image_test.jpeg', $output );
 	}
 }
