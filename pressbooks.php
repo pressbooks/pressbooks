d4cde98feb0da29ec542f780f5a6d8f7c8e64ca8
SteelWagstaff <steel@pressbooks.com>
2023-11-27 13:02:18 -0800

    chore: bump version

--- pressbooks.php
+++ pressbooks.php
@@ -5,7 +5,7 @@ Plugin URI:         https://pressbooks.org
 GitHub Plugin URI:  pressbooks/pressbooks
 Release Asset:      true
 Description:        Simple Book Production
-Version:            6.15.1
+Version:            6.15.2
 Requires at least:  6.3.1
 Requires PHP:       8.1
 Author:             Pressbooks (Book Oven Inc.)
