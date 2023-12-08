309c7523032b3b0706db51b697a14050eed95f1c
Ned Zimmerman <ned@bight.dev>
2023-12-07 22:55:28 -0400

    test: fix failing test

--- tests/test-admin-metaboxes.php
+++ tests/test-admin-metaboxes.php
@@ -75,9 +75,22 @@ class Admin_Metaboxes extends \WP_UnitTestCase {
 	}
 
 	public function test_save_metabox(): void {
-		global $post, $current_user;
+		global $post;
 
-		$default_user_id = $current_user->ID;
+		$metabox = new Pressbooks\Admin\Metaboxes\GeneralInformation();
+		$doc = new DOMDocument();
+		$doc->loadHTML( $metabox->nonce );
+		$nonce = $doc->getElementById( "{$metabox->slug}_nonce" )->getAttribute( 'value' );
+
+		// Nonce not set
+		$_POST[ 'pb_subtitle' ] = 'Or, the Whale';
+		$metabox->save( $post->ID );
+		$this->assertEquals( '', get_post_meta( $post->ID, 'pb_subtitle', true ) );
+
+		// Nonce set, user lacks permissions
+		$_POST[ 'pb_subtitle' ] = 'Or, the Whale';
+		$metabox->save( $post->ID );
+		$this->assertEquals( '', get_post_meta( $post->ID, 'pb_subtitle', true ) );
 
 		$user_id = wp_insert_user( [
 			'user_login' => 'administrator',
@@ -87,26 +100,9 @@ class Admin_Metaboxes extends \WP_UnitTestCase {
 		add_user_to_blog( get_current_blog_id(), $user_id, 'administrator' );
 		wp_set_current_user( $user_id, '' );
 
-		$metabox = new Pressbooks\Admin\Metaboxes\GeneralInformation();
-		$doc = new DOMDocument();
-		$doc->loadHTML( $metabox->nonce );
-		$nonce = $doc->getElementById( "{$metabox->slug}_nonce" )->getAttribute( 'value' );
-
-		$_POST[ "{$metabox->slug}_nonce" ] = $nonce;
-		$_POST[ 'pb_subtitle' ] = 'Or, the Whale';
-		$metabox->save( $post->ID );
-		$this->assertEquals( 'Or, the Whale', get_post_meta( $post->ID, 'pb_subtitle', true ) );
-
+		// Nonce set, user has permissions
 		$_POST[ "{$metabox->slug}_nonce" ] = 'bad nonce';
-		$_POST[ 'pb_subtitle' ] = 'An Arcane History';
-		$metabox->save( $post->ID );
-
-		$this->assertEquals( 'Or, the Whale', get_post_meta( $post->ID, 'pb_subtitle', true ) );
-
-		wp_set_current_user( $default_user_id, '' );
-
-		$_POST[ "{$metabox->slug}_nonce" ] = $nonce;
-		$_POST[ 'pb_subtitle' ] = 'An Arcane History';
+		$_POST[ 'pb_subtitle' ] = 'Or, the Whale';
 		$metabox->save( $post->ID );
 
 		$this->assertEquals( 'Or, the Whale', get_post_meta( $post->ID, 'pb_subtitle', true ) );
