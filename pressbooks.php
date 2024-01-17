e0e2893308dd94c5931237e31c308afa9b72635d
Oscar Arzola <oscararzola@gmail.com>
2024-01-17 11:48:04 -0500

    chore: bump version, upgrade packages

--- pressbooks.php
+++ pressbooks.php
@@ -5,7 +5,7 @@ Plugin URI:         https://pressbooks.org
 GitHub Plugin URI:  pressbooks/pressbooks
 Release Asset:      true
 Description:        Simple Book Production
-Version:            6.15.2
+Version:            6.16.0
 Requires at least:  6.4.2
 Requires PHP:       8.1
 Author:             Pressbooks (Book Oven Inc.)
