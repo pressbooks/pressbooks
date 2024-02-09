8793f5dd16dfd4d958282b4344bb794e171f9ea2
Ned Zimmerman <ned@bight.dev>
2024-02-08 21:46:52 -0400

    feat: localize field strings

--- templates/metaboxes/fields/reorderable-multiselect.blade.php
+++ templates/metaboxes/fields/reorderable-multiselect.blade.php
@@ -1,5 +1,14 @@
 <div class="form-field">
-	<pressbooks-reorderable-multiselect>
+	<pressbooks-reorderable-multiselect data-messages="{{ json_encode([
+		'Add' => __('Add', 'pressbooks'),
+		'Remove' => __('Remove', 'pressbooks'),
+		'Move Up' => __('Move Up', 'pressbooks'),
+		'Move Down' => __('Move Down', 'pressbooks'),
+		'Available Options' => __('Available options', 'pressbooks'),
+		'Moved to position $1' => __('Déplacé en position $1'),
+		'Removed $1 from selection' => __('Supprimer $1 de la sélection'),
+		'Added $1 to selection' => __('Ajouté $1 à la sélection')
+	]) }}">
 		<label>
 			{{ $field->label }}
 		</label>
