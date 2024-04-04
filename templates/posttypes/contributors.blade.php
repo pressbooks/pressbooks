efc562da1f955c1d43bc837994c60462ae1ed3f7
Felipe Dalcin <felipe@pressbooks.com>
2024-04-04 16:30:25 -0400

    fix: translate contributor type titles (#3671)
    * fix: translate contributor type titles
    
    * fix: remove unnecessary translation

--- templates/posttypes/contributors.blade.php
+++ templates/posttypes/contributors.blade.php
@@ -1,6 +1,6 @@
 <section class="contributors book-contributors">
 	@foreach($contributors as $contributor_type)
-		<h2 class="contributor__type">{{__( $contributor_type['title'], 'pressbooks' ) }}</h2>
+		<h2 class="contributor__type">{{ $contributor_type['title'] }}</h2>
 		@foreach($contributor_type['records'] as $contributor)
 			@include('posttypes.contributor',[ 'contributor' => $contributor, 'key' => str_random(), 'exporting' => $exporting ])
 		@endforeach
