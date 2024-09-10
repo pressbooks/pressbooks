b51303440641e1063c43263be5742495f486fd51
Oliver Tacke <o.tacke@posteo.de>
2024-09-10 19:34:26 +0200

    Remove obsolete div

--- templates/interactive/h5pextractor.blade.php
+++ templates/interactive/h5pextractor.blade.php
@@ -1,11 +1,9 @@
-<div>
-  <div style="border: 1px solid #eee;margin:0;padding:0;width: 100%;">
-   {!! $representation !!}
-  </div>
+<div style="border: 1px solid #eee;margin:0;padding:0;width: 100%;">
+  {!! $representation !!}
 </div>
 <div class="textbox interactive-content">
-	<span class="interactive-content__icon"></span>
-	<p>{{ __('The interactive version of this H5P content is available at:', 'pressbooks' ) }}
-		<a href="{{ $url.$id }}" title="{{ $title }}">{{ $url.$id }}</a>
-	</p>
+  <span class="interactive-content__icon"></span>
+  <p>{{ __('The interactive version of this H5P content is available at:', 'pressbooks' ) }}
+    <a href="{{ $url.$id }}" title="{{ $title }}">{{ $url.$id }}</a>
+  </p>
 </div>
