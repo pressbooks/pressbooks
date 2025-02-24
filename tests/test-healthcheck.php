ba6fa0f291882a623d834626efe06b862bfb1bab
Oscar Arzola <oscararzola@gmail.com>
2025-02-24 11:41:21 -0500

    remove unwanted backticks

--- tests/test-healthcheck.php
+++ tests/test-healthcheck.php
@@ -54,14 +54,14 @@ class HealthCheckTest extends \WP_UnitTestCase {
 		$request = new WP_REST_Request( 'GET', '/pressbooks/v2/health-check' );
 
 		$request->set_query_params( [
-			'_token' => env( '``PB_HEALTH_CHECK_TOKEN``' ),
+			'_token' => env( 'PB_HEALTH_CHECK_TOKEN' ),
 		] );
 
 		$response = $server->dispatch( $request );
 
 		/** @var \Illuminate\Support\Collection $data */
 		$data = $response->get_data();
-		
+
 		$this->assertEquals( 200, $response->status );
 
 		$this->assertEquals( [
