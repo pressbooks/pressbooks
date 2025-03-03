6b26afad5305439fdce6ad0e675c6ec851c68a75
Steel Wagstaff <steel@pressbooks.com>
2025-03-03 14:56:36 -0800

    fix: improve checks

--- hooks-admin.php
+++ hooks-admin.php
@@ -322,7 +322,11 @@ if ( $is_book ) {
 		}
 	);
 
-	add_action( 'admin_init', [ SideBar::class, 'handleH5pMenu' ] );
+	add_action( 'admin_init', function() {
+		if ( is_admin() ) {
+			SideBar::handleH5pMenu();
+		}
+} );
 
 	// Hide welcome screen
 	add_action(
