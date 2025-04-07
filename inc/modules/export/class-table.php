96315ba6f3ea987571c0e0f0ff0a8182963a9455
Felipe Dalcin <felipe@pressbooks.com>
2025-04-07 11:25:14 -0400

    chore: linting

--- inc/modules/export/class-table.php
+++ inc/modules/export/class-table.php
@@ -119,7 +119,7 @@ class Table extends \WP_List_Table {
 	public function column_pin( $item ) {
 		$format = $this->getTinyHash( $item['format'] );
 		$html = "<input type='checkbox' id='pin[{$item['ID']}]' name='pin[{$item['ID']}]' value='{$format}' " . checked( $item['pin'], true, false ) . '/>';
-		$html .= "<label for='pin[{$item['ID']}]'>" . __( sprintf( 'Pin %s', $item['file'] ) ) . "</label>";
+		$html .= "<label for='pin[{$item['ID']}]'>" . __( sprintf( 'Pin %s', $item['file'] ) ) . '</label>';
 		return $html;
 	}
 
