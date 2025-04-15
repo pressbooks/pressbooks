ef17a1a82431c747aec1849fd411f85814e04048
Ricardo Aragon <richard015ar@gmail.com>
2025-04-15 15:23:41 -0300

    fix: lint issues

--- assets/src/scripts/pressbooks-table.js
+++ assets/src/scripts/pressbooks-table.js
@@ -1,9 +1,9 @@
 // document ready
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.sorting-button').forEach(button => {
-        button.addEventListener('click', function (event) {
-            event.preventDefault();
-            window.location.href = this.dataset.href;
-        });
-    });
-});
+document.addEventListener( 'DOMContentLoaded', function () {
+	document.querySelectorAll( '.sorting-button' ).forEach( button => {
+		button.addEventListener( 'click', function ( event ) {
+			event.preventDefault();
+			window.location.href = this.dataset.href;
+		} );
+	} );
+} );
