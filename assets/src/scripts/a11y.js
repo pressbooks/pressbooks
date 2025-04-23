bf1dabea09ed318823e84514e511106fe4aec12f
Oscar Arzola <oscararzola@gmail.com>
2025-04-23 18:55:51 -0400

    fix: global date picker a11y

--- assets/src/scripts/a11y.js
+++ assets/src/scripts/a11y.js
@@ -69,4 +69,15 @@ document.addEventListener( 'DOMContentLoaded', function () {
 			irisPalette.setAttribute( 'aria-label', __( 'Select ' + color, 'pressbooks' ) );
 		}
 	} )();
+	// Add aria-describedby attribute to date picker inputs
+	const datePickers = document.querySelectorAll("duet-date-picker");
+	datePickers.forEach( ( datePicker ) => {
+		datePicker.addEventListener("duetFocus", () => {
+			const input = datePicker.querySelector("input.duet-date__input");
+			const ariaDescribedBy = datePicker.getAttribute("aria-describedby");
+			if (input) {
+				input.setAttribute("aria-describedby", ariaDescribedBy);
+			}
+		});
+	});
 } );
