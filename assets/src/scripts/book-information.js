6cc313fa16b7b38c2980b858b55c6532581ac240
Ricardo Aragon <richard015ar@gmail.com>
2025-04-24 10:32:52 -0300

    fix: scripts lint issues

--- assets/src/scripts/book-information.js
+++ assets/src/scripts/book-information.js
@@ -1,14 +1,14 @@
 jQuery( document ).ready( function ( $ ) {
 	// Set an initial focus to help users of assistive technology
 	$( '#pb_title' ).trigger( 'focus' );
-	const datePickers = document.querySelectorAll("duet-date-picker");
-	datePickers.forEach( ( datePicker ) => {
-		datePicker.addEventListener("duetFocus", () => {
-			const input = datePicker.querySelector("input.duet-date__input");
-			const ariaDescribedBy = datePicker.getAttribute("aria-describedby");
-			if (input) {
-				input.setAttribute("aria-describedby", ariaDescribedBy);
+	const datePickers = document.querySelectorAll( 'duet-date-picker' );
+	datePickers.forEach( datePicker => {
+		datePicker.addEventListener( 'duetFocus', () => {
+			const input = datePicker.querySelector( 'input.duet-date__input' );
+			const ariaDescribedBy = datePicker.getAttribute( 'aria-describedby' );
+			if ( input ) {
+				input.setAttribute( 'aria-describedby', ariaDescribedBy );
 			}
-		});
-	});
+		} );
+	} );
 } );
--- assets/src/scripts/contributors.js
+++ assets/src/scripts/contributors.js
@@ -267,18 +267,18 @@ jQuery( function ( $ ) {
 		updateRemoveButton();
 	};
 
-	const nameLinks = document.querySelectorAll('th#name a, tfoot th.column-name a');
-	nameLinks.forEach(link => {
+	const nameLinks = document.querySelectorAll( 'th#name a, tfoot th.column-name a' );
+	nameLinks.forEach( link => {
 	  const url = link.href;
 	  const content = link.innerHTML;
-	  const button = document.createElement('button');
+	  const button = document.createElement( 'button' );
 	  button.type = 'button';
 	  button.className = 'sorting-button';
 	  button.innerHTML = content;
-	  button.addEventListener('click', () => {
+	  button.addEventListener( 'click', () => {
 	    window.location.href = url;
-	  });
-	  link.parentNode.replaceChild(button, link);
-	});
+	  } );
+	  link.parentNode.replaceChild( button, link );
+	} );
 
 } );
