ed635fab622d75d13c145400377f71cfa8157956
Oscar Arzola <oscararzola@gmail.com>
2025-04-24 13:02:09 -0400

    fix: global date picker with a11y improvement (#4025)
    Co-authored-by: Ricardo Aragon <richard015ar@gmail.com>

--- assets/src/scripts/book-information.js
+++ assets/src/scripts/book-information.js
@@ -1,6 +1,7 @@
 jQuery( document ).ready( function ( $ ) {
 	// Set an initial focus to help users of assistive technology
 	$( '#pb_title' ).trigger( 'focus' );
+
 	const datePickers = document.querySelectorAll( 'duet-date-picker' );
 	datePickers.forEach( datePicker => {
 		datePicker.addEventListener( 'duetFocus', () => {
