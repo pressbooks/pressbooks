1007b7063a9b20cd51cb62b8428a2a2a9f9ea567
Oscar Arzola <oscararzola@gmail.com>
2025-05-01 15:54:33 -0400

    add test for more than 1 contributor

--- tests/test-exporthelpers.php
+++ tests/test-exporthelpers.php
@@ -215,4 +215,86 @@ class ExportHelpersTest extends \WP_UnitTestCase {
         // We can add a dummy assertion to prevent PHPUnit risky test warning
         $this->assertTrue( true );
     }
+
+    /**
+     * @group export_helpers
+     */
+    public function test_renderTitle_with_multiple_contributors() {
+        $this->_book();
+        $metadata = Book::getBookInformation( null, false, false );
+        // Add multiple contributor data to metadata as array of arrays with 'name' key
+        $metadata['pb_authors'] = [['name' => 'Test Author 1'], ['name' => 'Test Author 2']];
+        $metadata['pb_editors'] = [['name' => 'Test Editor 1'], ['name' => 'Test Editor 2']];
+        $metadata['pb_translators'] = [['name' => 'Test Translator 1'], ['name' => 'Test Translator 2']];
+        $metadata['pb_illustrators'] = [['name' => 'Test Illustrator 1'], ['name' => 'Test Illustrator 2']];
+        $metadata['pb_contributors'] = [['name' => 'Test Contributor 1'], ['name' => 'Test Contributor 2']];
+
+        $book_contents = Book::getBookContents();
+        $this->taxonomy = Taxonomy::init();
+        $this->contributors = new Contributors();
+
+        // --- Test XHTML11 renderTitle (Multiple Contributors) ---
+        $xhtml = new \Pressbooks\Modules\Export\Xhtml\Xhtml11( [] );
+        $xhtml_reflection_prop = new \ReflectionClass( $xhtml );
+        $xhtml_taxonomy_prop = $xhtml_reflection_prop->getProperty( 'taxonomy' );
+        $xhtml_taxonomy_prop->setAccessible( true );
+        $xhtml_taxonomy_prop->setValue( $xhtml, $this->taxonomy );
+
+        ob_start();
+        $xhtml_reflection = new \ReflectionClass( $xhtml );
+        $xhtml_method = $xhtml_reflection->getMethod( 'renderTitle' );
+        $xhtml_method->setAccessible( true );
+        $xhtml_method->invokeArgs( $xhtml, [ $book_contents, $metadata ] );
+        $xhtml_output = ob_get_clean();
+
+        // Assertions for XHTML output (Multiple Contributors) - Expecting ' and ' separation
+        $this->assertStringContainsString( '<div id="title-page"', $xhtml_output );
+        $this->assertStringContainsString( get_bloginfo( 'name' ), $xhtml_output );
+        $this->assertStringContainsString( 'Test Author 1 and Test Author 2', $xhtml_output );
+        $this->assertStringContainsString( __( 'Edited By ', 'pressbooks' ) . 'Test Editor 1 and Test Editor 2', $xhtml_output );
+        $this->assertStringContainsString( __( 'Translated By ', 'pressbooks' ) . 'Test Translator 1 and Test Translator 2', $xhtml_output );
+        $this->assertStringContainsString( __( 'Illustrated By ', 'pressbooks' ) . 'Test Illustrator 1 and Test Illustrator 2', $xhtml_output );
+        $this->assertStringContainsString( __( 'Contributors: ', 'pressbooks' ) . 'Test Contributor 1 and Test Contributor 2', $xhtml_output );
+
+        // --- Test Epub renderTitle (Multiple Contributors) ---
+        $epub_reflection = new \ReflectionClass( '\Pressbooks\Modules\Export\Epub\Epub' );
+        $epub_method = $epub_reflection->getMethod( 'renderTitle' );
+        $epub_method->setAccessible( true );
+        $epub_taxonomy_prop = $epub_reflection->getProperty( 'taxonomy' );
+        $epub_taxonomy_prop->setAccessible( true );
+
+        $mock_epub = $this->getMockBuilder( \Pressbooks\Modules\Export\Epub\Epub::class )
+            ->setConstructorArgs( [[]] )
+            ->onlyMethods( [ 'createEpubFile', 'updateManifest' ] )
+            ->getMock();
+
+        $mock_epub->expects( $this->once() )
+            ->method( 'createEpubFile' )
+            ->with(
+                $this->equalTo( 'title-page.xhtml' ),
+                // Check that the data array contains expected multiple contributor info (names separated by ' and ')
+                $this->callback(function($data) {
+                    $this->assertArrayHasKey('post_content', $data);
+                    $this->assertStringContainsString('Test Author 1 and Test Author 2', $data['post_content']);
+                    $this->assertStringContainsString(__( 'Edited By ', 'pressbooks' ) . 'Test Editor 1 and Test Editor 2', $data['post_content']);
+                    $this->assertStringContainsString(__( 'Translated By ', 'pressbooks' ) . 'Test Translator 1 and Test Translator 2', $data['post_content']);
+                    $this->assertStringContainsString(__( 'Illustrated By ', 'pressbooks' ) . 'Test Illustrator 1 and Test Illustrator 2', $data['post_content']);
+                    $this->assertStringContainsString(__( 'Contributors: ', 'pressbooks' ) . 'Test Contributor 1 and Test Contributor 2', $data['post_content']);
+                    return true; // Return true if all assertions pass
+                })
+            );
+
+        $mock_epub->expects( $this->once() )
+            ->method( 'updateManifest' )
+            ->with(
+                $this->equalTo( 'title-page' ),
+                $this->arrayHasKey( 'filename' )
+            );
+
+        $epub_taxonomy_prop->setValue( $mock_epub, $this->taxonomy );
+        $epub_method->invokeArgs( $mock_epub, [ $book_contents, $metadata ] );
+
+        // Add a final dummy assertion if needed to prevent risky test warnings, though the mock expectations should cover it.
+        $this->assertTrue( true );
+    }
 }
