87e16a2f0c41f20ad0617b4dd3fd55d3747886ce
Ned Zimmerman <ned@bight.dev>
2025-05-30 12:42:23 -0600

    test: fix test

--- tests/test-interactive-content.php
+++ tests/test-interactive-content.php
@@ -41,6 +41,8 @@ class Interactive_ContentTest extends \WP_UnitTestCase {
 		<iframe src="https://docs.google.com/garbage/d/e/xxx/viewform?embedded=true" width="640" height="398" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
 		Test Five
 		<iframe src="https://www.google.com/maps/d/embed?mid=xxx" width="640" height="480" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+		Test Six
+		<iframe title="YouTube video player" src="https://www.youtube.com/embed/0guNyC6cEJE?si=qvxZsBmo1hXLUdzF" width="560" height="315" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
 		';
 		$result = $this->content->deleteIframesNotOnWhitelist( $raw, [ 'post' ] );
 		$this->assertEquals( 2, substr_count( $result, '<iframe' ) );
@@ -49,6 +51,7 @@ class Interactive_ContentTest extends \WP_UnitTestCase {
 		$this->assertStringContainsString( '<iframe src="https://docs.google.com/forms/d/e/xxx/viewform?embedded=true', $result );
 		$this->assertStringContainsString( '[embed]https://docs.google.com/garbage/d/e/xxx/viewform?embedded=true[/embed]', $result );
 		$this->assertStringContainsString( '<iframe src="https://www.google.com/maps/d/embed?mid=xxx', $result );
+		$this->assertStringContainsString( '<iframe title="YouTube video player" src="https://www.youtube.com/embed/0guNyC6cEJE?si=qvxZsBmo1hXLUdzF" width="560" height="315" frameborder="0" allowfullscreen="allowfullscreen"></iframe>', $result );
 		$this->assertStringNotContainsString( '<p>', $result );
 	}
 
@@ -67,16 +70,6 @@ class Interactive_ContentTest extends \WP_UnitTestCase {
 		$this->assertStringContainsString( '<div ', $result );
 		$this->assertStringContainsString( '<p>Test</p>', $result );
 		$this->assertStringContainsString( 'excluded from this version of the text', $result );
-
-		$html = '
-		<p>Test</p>
-		<iframe title="YouTube video player" src="https://www.youtube.com/embed/0guNyC6cEJE?si=qvxZsBmo1hXLUdzF" width="560" height="315" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
-		';
-
-		$result = $this->content->replaceIframes( $html );
-
-		$this->assertStringContainsString( '<iframe', $result );
-		$this->assertStringContainsString( 'allowfullscreen', $result );
 	}
 
 	/**
