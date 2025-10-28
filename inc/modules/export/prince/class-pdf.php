ad5724fc82d096787715caa40e3bad70bb239608
Alex Dryden <adryden3@illinois.edu>
2025-10-28 13:38:48 -0400

    fix: pass input type to Prince
    This addresses #4263 by adding parsing for xml:lang properties in
    Prince pdf exports. Because Prince can't infer the file type by the
    extension, it needs to be told explicitly that this is xml,
    otherwise it won't parse xml:* attributes.

--- inc/modules/export/prince/class-pdf.php
+++ inc/modules/export/prince/class-pdf.php
@@ -254,6 +254,7 @@ class Pdf extends Export {
 		$prince->setHTML( true );
 		$prince->setCompress( true );
 		$prince->setHttpTimeout( defined( 'WP_TESTS_MULTISITE' ) ? 5 : 600 ); // 5 seconds for tests, 10 minutes for production
+		$prince->setInputType('xml');
 		if ( defined( 'WP_ENV' ) && ( WP_ENV === 'development' ) ) {
 			$prince->setInsecure( true );
 		}
@@ -265,6 +266,7 @@ class Pdf extends Export {
 			$prince->setPDFOutputIntent( $this->pdfOutputIntent );
 		} elseif ( stripos( get_class( $this ), 'print' ) === false && empty( $this->pdfProfile ) ) {
 			$prince->setPDFProfile( 'PDF/UA-1' );
+			
 		}
 
 		yield 60 => __( 'Adding stylesheets and scripts...', 'pressbooks' );
