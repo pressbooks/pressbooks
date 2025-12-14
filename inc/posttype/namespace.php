53b77959644130b2f093807476aa7adf840e43a2
Steel Wagstaff <steel@pressbooks.com>
2025-12-14 15:17:39 -0800

    feat: allow subscript and superscript in glossary terms

--- inc/posttype/namespace.php
+++ inc/posttype/namespace.php
@@ -268,7 +268,7 @@ function wp_editor_settings( $settings ) {
 		$settings['wpautop'] = true;
 		$settings['media_buttons'] = false;
 		$settings['tinymce'] = [
-			'toolbar1' => 'bold,italic,|,link,unlink,|,undo,redo',
+			'toolbar1' => 'bold,italic,superscript,subscript,|,link,unlink,|,undo,redo',
 			'toolbar2' => '',
 			'toolbar3' => '',
 		];
