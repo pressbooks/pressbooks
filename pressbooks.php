63a83201fd50b01427becaec334cb3f9cc4dec36
pressbooks-ops <66340483+pressbooks-ops@users.noreply.github.com>
2026-07-23 13:08:25 -0400

    chore(dev): release 6.43.0 (#4524)

--- pressbooks.php
+++ pressbooks.php
@@ -6,7 +6,7 @@
  * Release Asset: true
  * Description: Simple Book Production
  * x-release-please-start-version
- * Version: 6.42.3
+ * Version: 6.43.0
  * x-release-please-end
  * Requires at least: WordPress 6.8.3
  * Requires PHP: 8.3
