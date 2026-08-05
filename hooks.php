616ef3e1e0d3bd546ac6d0410138a7ea700ee374
Christopher Murtagh <christopher@pressbooks.com>
2026-08-05 09:11:52 -0600

    fix: use correct sitemap url (#4540)
    * fix: using home_url() because siteurl includes /wp
    
    * fix: removing wp sitemap function

--- hooks.php
+++ hooks.php
@@ -249,6 +249,7 @@ add_filter( 'login_redirect', '\Pressbooks\Redirect\handle_dashboard_redirect', 
 // Sitemap
 // -------------------------------------------------------------------------------------------------------------------
 
+add_filter( 'wp_sitemaps_enabled', '__return_false' );
 add_action( 'init', '\Pressbooks\Redirect\rewrite_rules_for_sitemap', 1 );
 add_action( 'do_robotstxt', '\Pressbooks\Utility\add_sitemap_to_robots_txt' );
 add_filter( 'wp_robots', '\Pressbooks\Utility\handle_book_indexing' );
